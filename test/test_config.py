5294ac3512b97796754f1276a14718d432e2cc2d
ONUR <atadogan06@gmail.com>
2021-03-24 17:22:56 +0300

    added unittest for config system

---
+++ test/test_config.py
@@ -0,0 +1,15 @@
+import unittest
+
+
+class Test_Config(unittest.TestCase):
+
+    def test_getting_and_saving_config(self):
+        temp_config = get_config()
+        self.assertIsNotNone(temp_config.main_folder, "A problem on the config.")
+
+if __name__ == '__main__':
+    import os
+    import sys
+    sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
+    from config import get_config
+    unittest.main()
