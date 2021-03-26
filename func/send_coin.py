fe5c51064a06b5275e0b41d8d7f4702d9bb1c783
cpyberry <cpyberry222@gmail.com>
2021-03-26 19:55:36 +0900

    fixed a type checking bug

--- func/send_coin.py
+++ func/send_coin.py
@@ -5,11 +5,14 @@ from wallet.wallet import Wallet_Import
 def send_coin(coin_amount,to_user):
   my_public_key = Wallet_Import(0,0)
   my_private_key = Wallet_Import(0,1)
+
+  if not (isinstance(coin_amount, int) or isinstance(coin_amount, float)):
+    print("This is not integer coin amount.")
+    return None
+
   if coin_amount < 0:
     print("This is negative coin amount.")
     return None
-  if isinstance(int(coin_amount), int) or isinstance(float(coin_amount), float):
+
    print("sendcoin"+str(coin_amount))
    send(my_public_key=my_public_key,my_private_key=my_private_key,to_user=to_user, amount = coin_amount)
-  else:
-    print("This is not integer coin amount.")
