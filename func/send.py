1a330b8580d671af0ef05c04baf5e00712f7fc35
onuratakan <atadogan06@gmail.com>
2021-04-28 19:41:22 +0000

    Fix pack for consensus

--- func/send.py
+++ func/send.py
@@ -16,4 +16,4 @@ def send(my_public_key, my_private_key, to_user, data = None, amount = None):
     sequance_number = system.getSequanceNumber(user = my_public_key) + 1
     transaction_fee = 0.02
 
-    system.createTrans(sequance_number = sequance_number, signature = Ecdsa.sign(str(sequance_number)+str(my_public_key)+str(to_user)+str(data)+str(amount)+str(transaction_fee), PrivateKey.fromPem(my_private_key)).toBase64(), fromUser = str(my_public_key), toUser = str(to_user), data = data, amount = amount, transaction_fee = transaction_fee, transaction_sender = None, my_tx = True)
+    system.createTrans(sequance_number = sequance_number, signature = Ecdsa.sign(str(sequance_number)+str(my_public_key)+str(to_user)+str(data)+str(amount)+str(transaction_fee), PrivateKey.fromPem(my_private_key)).toBase64(), fromUser = str(my_public_key), toUser = str(to_user), data = data, amount = amount, transaction_fee = transaction_fee, transaction_sender = None)
