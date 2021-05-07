0e774ca835caea479439f6c1ef5d9b2ebc00f8bc
Onur Atakan ULUSOY <41792982+onuratakan@users.noreply.github.com>
2021-05-07 16:18:50 +0300

    Update tabnavigation.py

--- gui_lib/libs/baseclass/tabnavigation.py
+++ gui_lib/libs/baseclass/tabnavigation.py
@@ -1,8 +1,9 @@
-"""
-    This module had taken in
-        https://github.com/kivymd-extensions/akivymd
-"""
-
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
 from kivy.animation import Animation
 from kivy.clock import Clock
