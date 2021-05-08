404c8ac0df45c27d8eece869fc1bdc778c4d87be
Onur Atakan ULUSOY <41792982+onuratakan@users.noreply.github.com>
2021-05-08 12:16:27 +0300

    Update config.py

--- src/config.py
+++ src/config.py
@@ -5,19 +5,19 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
-CONFIG_PATH = 'db/config.json'
-SETTING_PATH = 'db/settings.json'
+CONFIG_PATH = 'src/db/config.json'
+SETTING_PATH = 'src/db/settings.json'
 
-CONNECTED_NODE_PATH = 'db/connected_node.json'
-UNL_NODES_PATH = 'db/unl_nodes.json'
+CONNECTED_NODE_PATH = 'src/db/connected_node.json'
+UNL_NODES_PATH = 'src/db/unl_nodes.json'
 
-TEMP_BLOCK_PATH = 'db/temp_block.decentra_network'
+TEMP_BLOCK_PATH = 'src/db/temp_block.decentra_network'
 
-TEMP_CANDIDATE_BLOCKS_PATH = 'db/temp_candidate_blocks.decentra_network'
+TEMP_CANDIDATE_BLOCKS_PATH = 'src/db/temp_candidate_blocks.decentra_network'
 
-LOADING_BLOCK_PATH = 'db/loading_block.decentra_network'
+LOADING_BLOCK_PATH = 'src/db/loading_block.decentra_network'
 
-BLOCKCHAIN_PATH = 'db/blockchain.decentra_network'
+BLOCKCHAIN_PATH = 'src/db/blockchain.decentra_network'
 
 
-WALLETS_PATH = 'db/wallet_list.json'
+WALLETS_PATH = 'src/db/wallet_list.json'
