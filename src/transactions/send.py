5bfebd96a18b255176c9f67859bfd5f91950a040
cpyberry <cpyberry222@gmail.com>
2021-05-22 23:07:18 +0900

    added a mechanism to change transaction fee

--- src/transactions/send.py
+++ src/transactions/send.py
@@ -12,6 +12,15 @@ from accounts.get_sequance_number import GetSequanceNumber
 from blockchain.block.get_block import GetBlock
 
 
+class TransactionConfig:
+    """transaction related settings"""
+    transaction_fee = 0.02
+
+    @classmethod
+    def change_transaction_fee(cls, transaction_fee: float) -> None:
+        cls.transaction_fee = transaction_fee
+
+
 def send(my_public_key, my_private_key, to_user, data = None, amount = None):
     my_public_key = "".join([
             l.strip() for l in my_public_key.splitlines()
@@ -20,6 +29,6 @@ def send(my_public_key, my_private_key, to_user, data = None, amount = None):
 
     system = GetBlock()
     sequance_number = GetSequanceNumber(my_public_key, system) + 1
-    transaction_fee = 0.02
+    transaction_fee = TransactionConfig.transaction_fee
 
     system.createTrans(sequance_number = sequance_number, signature = Ecdsa.sign(str(sequance_number)+str(my_public_key)+str(to_user)+str(data)+str(amount)+str(transaction_fee), PrivateKey.fromPem(my_private_key)).toBase64(), fromUser = str(my_public_key), toUser = str(to_user), data = data, amount = amount, transaction_fee = transaction_fee, transaction_sender = None)
