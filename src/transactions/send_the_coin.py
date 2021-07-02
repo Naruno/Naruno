e1c4c2b5ae321ce389488f76f6a79b56885d1729
ONUR <atadogan06@gmail.com>
2021-07-02 17:02:09 +0300

    cli: Added some argument based controls

---
+++ src/transactions/send_the_coin.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+
+from getpass import getpass
+from hashlib import sha256
+
+from wallet.wallet import Wallet_Import
+
+from transactions.send_coin import send_coin
+
+from blockchain.block.get_block import GetBlock
+
+from lib.settings_system import the_settings
+
+
+def send_the_coin(receiver, temp_coin_amount):
+            type_control = False
+            try:
+                float(temp_coin_amount)
+                type_control = True
+            except:
+                print("This is not float coin amount.")
+
+
+            if type_control and not float(temp_coin_amount) < GetBlock().minumum_transfer_amount:
+                password = getpass("Password: ")
+                if Wallet_Import(int(the_settings()["wallet"]),2) == sha256(password.encode("utf-8")).hexdigest():
+                    print(password)
+                    send_coin(float(temp_coin_amount), receiver, password)
+                else:
+                    print("Password is not correct")
+                del password
