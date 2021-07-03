e29403758ca4b88be404bfb85c70ef286755289d
Onur Atakan ULUSOY <41792982+onuratakan@users.noreply.github.com>
2021-07-03 20:26:45 +0300

    Update api.py

--- src/api.py
+++ src/api.py
@@ -132,6 +132,6 @@ if __name__ == '__main__':
 
 
     if len(sys.argv) < 2:
-        app.run(port=8000)
+        app.run(host='0.0.0.0', port=8000)
     else:
-        app.run(port=args.port)
+        app.run(host='0.0.0.0', port=args.port)
