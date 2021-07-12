b2005492bb0e2375e3ddf695190ebebb18493932
ONUR <atadogan06@gmail.com>
2021-07-12 23:58:45 +0300

    transaction: Fix for E501 line too long

--- src/transactions/send_coin.py
+++ src/transactions/send_coin.py
@@ -5,6 +5,7 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
+
 from transactions.send import send
 
 from wallet.wallet import Wallet_Import
@@ -33,6 +34,12 @@ def send_coin(coin_amount, to_user, password):
         print("This is negative coin amount.")
         return None
 
-    send(my_public_key = my_public_key, my_private_key = my_private_key, to_user = to_user, password = password, amount = coin_amount)
+    send(
+        my_public_key=my_public_key,
+        my_private_key=my_private_key,
+        to_user=to_user,
+        password=password,
+        amount=coin_amount,
+    )
 
     del my_private_key
