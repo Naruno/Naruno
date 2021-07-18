8a405e05c28f748fadeff2afaa3d63b83ccf4d5e
onuratakan <>
2021-07-18 10:24:22 +0000

    dev commit will use after a bug fixes on kivymd

--- src/gui_lib/libs/baseclass/operations_screen.py
+++ src/gui_lib/libs/baseclass/operations_screen.py
@@ -9,14 +9,18 @@
 import os
 from hashlib import sha256
 
+from kivy.metrics import dp
+
 from kivymd.uix.gridlayout import MDGridLayout
 from kivymd.uix.screen import MDScreen
 from kivymd.uix.button import MDFlatButton
+from kivymd.uix.datatables import MDDataTable
 from kivymd_extensions.sweetalert import SweetAlert
 
 from kivy.core.clipboard import Clipboard
 
 from transactions.send_coin import send_coin
+from transactions.save_to_my_transaction import GetMyTransaction
 
 from blockchain.block.get_block import GetBlock
 
@@ -33,10 +37,16 @@ class OperationScreen(MDScreen):
 class Send_Coin_Box(MDGridLayout):
     cols = 2
 
+
+class Transaction_History_Box(MDGridLayout):
+    cols = 1
+
+
 class OperationBox(MDGridLayout):
     cols = 2
     send_coin_dialog = None
     export_transaction_csv_dialog = None
+    transaction_history_dialog = None
     FONT_PATH = f"{os.environ['DECENTRA_ROOT']}/gui_lib/fonts/"
 
 
@@ -106,7 +116,8 @@ class OperationBox(MDGridLayout):
 
 
     def export_transaction_csv(self):
-        if export_the_transactions():
+        transactions = GetMyTransaction()
+        if not len(transactions) == 0:
             Clipboard.copy(MY_TRANSACTION_EXPORT_PATH)
             SweetAlert().fire(
                 f"CSV file created in {MY_TRANSACTION_EXPORT_PATH} directory, The directory has been copied to your clipboard.",
@@ -116,5 +127,31 @@ class OperationBox(MDGridLayout):
             SweetAlert().fire(
                 "You have not a transaction",
                 type='failure',
-            )                       
+            )
+
+    def dismiss_transaction_history_dialog(self,widget):
+        self.transaction_history_dialog.dismiss()
+
+    def transaction_history(self):
+        if export_the_transactions():
+            self.transaction_history_dialog = SweetAlert(
+                  title="Send Coin",
+                  type="custom",
+                  auto_dismiss=False,
+                  content_cls=Transaction_History_Box(),
+                  buttons=[
+                      MDFlatButton(
+                          text="OK",
+                          on_press=self.dismiss_transaction_history_dialog,
+                          font_size= "18sp",
+                          font_name= self.FONT_PATH + "RobotoCondensed-Bold",
+                      ),
+                  ],
+              )
+            self.transaction_history_dialog.open()
+        else:
+            SweetAlert().fire(
+                "You have not a transaction",
+                type='failure',
+            )                     
         
