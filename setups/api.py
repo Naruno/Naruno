d0ad7f944e62ff257a5694d8bd78052aa95bc95c
Onur Atakan ULUSOY <41792982+onuratakan@users.noreply.github.com>
2021-08-28 00:52:04 +0300

    Merge #208 setup: Some fixes for library based installation
    Some fixes for library based installation

--- setups/api.py
+++ setups/api.py
@@ -1,12 +1,13 @@
 from setuptools import setup
 
 setup(name='decentra_network_api',
-version='0.8.0',
+version='0.8.1',
 description="""This is API mode installer for Decentra Network Core""",
 url='https://decentra-network.github.io/Decentra-Network/',
 author='Decentra Network Developers',
 author_email='atadogan06@gmail.com',
 license='MPL-2.0',
 install_requires=open("requirements/api.txt", "r").readlines(),
+package_dir={'':'.'},
 python_requires=">=3.6",
 zip_safe=False)
