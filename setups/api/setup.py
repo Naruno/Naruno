aee1e880b3916a8df5fb0f8f0d52347884884ab8
Onur Atakan ULUSOY <atadogan06@gmail.com>
2021-08-27 23:02:47 +0000

    version number fix

--- setups/api/setup.py
+++ setups/api/setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='decentra_network_api',
-version='0.1.1',
+version='0.8.3',
 description="""This is API mode installer for Decentra Network Core""",
 url='https://decentra-network.github.io/Decentra-Network/',
 author='Decentra Network Developers',
