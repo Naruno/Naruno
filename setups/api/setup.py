1e7744a47012256d1aac559be0a0c504173e4ed8
Onur Atakan ULUSOY <atadogan06@gmail.com>
2021-08-27 23:01:57 +0000

    fixed api and gui setups

--- setups/api/setup.py
+++ setups/api/setup.py
@@ -1,13 +1,12 @@
 from setuptools import setup
 
-setup(name='decentra_network_gui',
-version='0.8.2',
-description="""This is GUI mode installer for Decentra Network Core""",
+setup(name='decentra_network_api',
+version='0.1.1',
+description="""This is API mode installer for Decentra Network Core""",
 url='https://decentra-network.github.io/Decentra-Network/',
 author='Decentra Network Developers',
 author_email='atadogan06@gmail.com',
 license='MPL-2.0',
-install_requires=open("requirements/gui.txt", "r").readlines(),
-package_dir={'':'.'},
+install_requires="flask==2.0.0",
 python_requires=">=3.6",
 zip_safe=False)
--- setups/gui/setup.py
+++ setups/gui/setup.py
@@ -1,13 +1,16 @@
 from setuptools import setup
 
-setup(name='decentra_network_api',
-version='0.8.2',
-description="""This is API mode installer for Decentra Network Core""",
+setup(name='decentra_network_gui',
+version='0.8.3',
+description="""This is GUI mode installer for Decentra Network Core""",
 url='https://decentra-network.github.io/Decentra-Network/',
 author='Decentra Network Developers',
 author_email='atadogan06@gmail.com',
 license='MPL-2.0',
-install_requires=open("requirements/api.txt", "r").readlines(),
-package_dir={'':'.'},
+install_requires="""
+Kivy==2.0.0
+kivymd==0.104.1
+kivymd_extensions.sweetalert==0.1.5
+""",
 python_requires=">=3.6",
 zip_safe=False)
