63e9f9e616ff91de9afe5fbc80ed138a8515a7eb
Onur Atakan ULUSOY <atadogan06@gmail.com>
2021-08-30 14:07:50 +0000

    fix the requirements getting on the setups

--- setups/gui/setup.py
+++ setups/gui/setup.py
@@ -1,15 +1,17 @@
 from setuptools import setup
-import os
-import pathlib
 
 
 setup(name='decentra_network_gui',
-version='0.9.0',
+version='0.9.1',
 description="""This is GUI mode installer for Decentra Network Core""",
 url='https://decentra-network.github.io/Decentra-Network/',
 author='Decentra Network Developers',
 author_email='atadogan06@gmail.com',
 license='MPL-2.0',
-install_requires=open(os.path.join((pathlib.Path(__file__) / ".." / ".." / "..").resolve(), 'requirements/gui.txt' ), "r").read(),
+install_requires="""
+Kivy==2.0.0
+kivymd==0.104.1
+kivymd_extensions.sweetalert==0.1.5
+""",
 python_requires=">=3.6",
 zip_safe=False)
