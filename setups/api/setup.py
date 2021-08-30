d6f3b296e0679ef58659e38610fedb7cc1d3721c
Onur Atakan ULUSOY <41792982+onuratakan@users.noreply.github.com>
2021-08-30 12:32:27 +0300

    setup: Added the get requirements from requirements in api setup

--- setups/api/setup.py
+++ setups/api/setup.py
@@ -1,4 +1,6 @@
 from setuptools import setup
+import os
+import pathlib
 
 setup(name='decentra_network_api',
 version='0.8.4',
@@ -7,6 +9,6 @@ url='https://decentra-network.github.io/Decentra-Network/',
 author='Decentra Network Developers',
 author_email='atadogan06@gmail.com',
 license='MPL-2.0',
-install_requires="flask==2.0.0",
+install_requires=open(os.path.join((pathlib.Path(__file__) / ".." / ".." / "..").resolve(), 'requirements/api.txt' ), "r").read(),
 python_requires=">=3.6",
 zip_safe=False)
