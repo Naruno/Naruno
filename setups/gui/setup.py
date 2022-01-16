c4ebec10e8a47278667de4bc0e2a93dd8ff85828
Onur Atakan ULUSOY <atadogan06@gmail.com>
2022-01-16 22:05:33 +0000

    Added 0.11.2 release

--- setups/gui/setup.py
+++ setups/gui/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 
 setup(name='decentra_network_gui',
-version='0.11.1',
+version='0.11.2',
 description="""This is GUI mode installer for Decentra Network Core""",
 url='https://decentra-network.github.io/Decentra-Network/',
 author='Decentra Network Developers',
