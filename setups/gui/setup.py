99a8d521139f27b20e14f66d0badcc3795524b5c
Onur Atakan ULUSOY <atadogan06@gmail.com>
2022-01-27 18:17:19 +0000

    Added version 0.12.0

--- setups/gui/setup.py
+++ setups/gui/setup.py
@@ -10,14 +10,14 @@ from setuptools import setup
 
 
 setup(name='decentra_network_gui',
-version='0.11.3',
+version='0.12.0',
 description="""This is GUI mode installer for Decentra Network Core""",
 url='https://decentra-network.github.io/Decentra-Network/',
 author='Decentra Network Developers',
 author_email='atadogan06@gmail.com',
 license='MPL-2.0',
 install_requires="""
-decentra-network==0.11.3
+decentra-network==0.12.0
 Kivy==2.0.0
 kivymd==0.104.1
 kivymd_extensions.sweetalert==0.1.5
