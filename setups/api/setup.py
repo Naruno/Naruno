4ef34309427ab213424377d8cb059309a04d8eb3
Restyled.io <commits@restyled.io>
2022-01-30 13:19:35 +0000

    Restyled by black

--- setups/api/setup.py
+++ setups/api/setup.py
@@ -9,17 +9,19 @@
 from setuptools import setup
 
 
-setup(name='decentra_network_api',
-      version='0.13.0',
-      description="""This is API mode installer for Decentra Network Core""",
-      url='https://decentra-network.github.io/Decentra-Network/',
-      author='Decentra Network Developers',
-      author_email='atadogan06@gmail.com',
-      license='MPL-2.0',
-      install_requires="""
+setup(
+    name="decentra_network_api",
+    version="0.13.0",
+    description="""This is API mode installer for Decentra Network Core""",
+    url="https://decentra-network.github.io/Decentra-Network/",
+    author="Decentra Network Developers",
+    author_email="atadogan06@gmail.com",
+    license="MPL-2.0",
+    install_requires="""
 decentra-network==0.13.0
 flask==2.0.0
 waitress==2.0.0
 """,
-      python_requires=">=3.6",
-      zip_safe=False)
+    python_requires=">=3.6",
+    zip_safe=False,
+)
