ecbb7d2ca7cf707944f99c2936ca544c6e8e3be1
Onur Atakan ULUSOY <atadogan06@gmail.com>
2022-01-30 13:17:25 +0000

    Added version 0.13.0

--- setups/api/setup.py
+++ setups/api/setup.py
@@ -10,14 +10,14 @@ from setuptools import setup
 
 
 setup(name='decentra_network_api',
-version='0.12.0',
+version='0.13.0',
 description="""This is API mode installer for Decentra Network Core""",
 url='https://decentra-network.github.io/Decentra-Network/',
 author='Decentra Network Developers',
 author_email='atadogan06@gmail.com',
 license='MPL-2.0',
 install_requires="""
-decentra-network==0.12.0
+decentra-network==0.13.0
 flask==2.0.0
 waitress==2.0.0
 """,
