992fcf11bd80beac9cdc89d18dad4dc408a572a9
Restyled.io <commits@restyled.io>
2022-01-31 20:42:22 +0000

    Restyled by autopep8

--- functional_test/docker/test_decentra_network_docker.py
+++ functional_test/docker/test_decentra_network_docker.py
@@ -6,6 +6,12 @@
 # file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
 
+import json
+import urllib.request
+import os
+from auto_builders.docker import Decentra_Network_Docker
+import time
+import sys
 import unittest
 
 
@@ -24,19 +30,23 @@ class Test_Decentra_Network_Docker(unittest.TestCase):
             temp_environment.run()
             temp_environment.start()
 
-            wallet_2_json = json.loads(urllib.request.urlopen("http://localhost:8010/wallet/create/123").read().decode())
-            wallet_2_address = wallet_2_json[0].replace("0) ", "").replace(" - CURRENTLY USED\n", "")
-            urllib.request.urlopen(f"http://localhost:8000/send/coin/{wallet_2_address}/5000/123")
+            wallet_2_json = json.loads(urllib.request.urlopen(
+                "http://localhost:8010/wallet/create/123").read().decode())
+            wallet_2_address = wallet_2_json[0].replace(
+                "0) ", "").replace(" - CURRENTLY USED\n", "")
+            urllib.request.urlopen(
+                f"http://localhost:8000/send/coin/{wallet_2_address}/5000/123")
 
             time.sleep(10)
 
-
-            balance_wallet_1 = json.loads(urllib.request.urlopen("http://localhost:8010/wallet/balance").read().decode())
+            balance_wallet_1 = json.loads(urllib.request.urlopen(
+                "http://localhost:8010/wallet/balance").read().decode())
 
             if balance_wallet_1 == 4000.0:
                 success = True
 
-        self.assertEqual(success,True,"A problem in different network one transaction.")
+        self.assertEqual(
+            success, True, "A problem in different network one transaction.")
 
     def test_2_different_network_multi_transacton(self):
         """
@@ -50,25 +60,24 @@ class Test_Decentra_Network_Docker(unittest.TestCase):
             temp_environment.install()
             temp_environment.run()
             temp_environment.start()
-            wallet_2_json = json.loads(urllib.request.urlopen("http://localhost:8010/wallet/print").read().decode())
-            wallet_2_address = wallet_2_json[0].replace("0) ", "").replace(" - CURRENTLY USED\n", "")
-
-
+            wallet_2_json = json.loads(urllib.request.urlopen(
+                "http://localhost:8010/wallet/print").read().decode())
+            wallet_2_address = wallet_2_json[0].replace(
+                "0) ", "").replace(" - CURRENTLY USED\n", "")
 
             for i in range(4):
-                urllib.request.urlopen(f"http://localhost:8000/send/coin/{wallet_2_address}/5000/123")
+                urllib.request.urlopen(
+                    f"http://localhost:8000/send/coin/{wallet_2_address}/5000/123")
                 time.sleep(10)
 
-
-
-            balance_wallet_1 = json.loads(urllib.request.urlopen("http://localhost:8010/wallet/balance").read().decode())
+            balance_wallet_1 = json.loads(urllib.request.urlopen(
+                "http://localhost:8010/wallet/balance").read().decode())
 
             if balance_wallet_1 == 19000.0:
                 success = True
 
-        self.assertEqual(success,True,"A problem in different network multi transaction.")
-
-
+        self.assertEqual(
+            success, True, "A problem in different network multi transaction.")
 
     def test_3_same_network__one_and_multi_transacton(self):
         """
@@ -81,26 +90,28 @@ class Test_Decentra_Network_Docker(unittest.TestCase):
             temp_environment.install()
             temp_environment.run()
             temp_environment.start()
-            wallet_2_json = json.loads(urllib.request.urlopen("http://localhost:8010/wallet/create/123").read().decode())
-            wallet_2_address = wallet_2_json[0].replace("0) ", "").replace(" - CURRENTLY USED\n", "")
-            urllib.request.urlopen(f"http://localhost:8000/send/coin/{wallet_2_address}/5000/123")
+            wallet_2_json = json.loads(urllib.request.urlopen(
+                "http://localhost:8010/wallet/create/123").read().decode())
+            wallet_2_address = wallet_2_json[0].replace(
+                "0) ", "").replace(" - CURRENTLY USED\n", "")
+            urllib.request.urlopen(
+                f"http://localhost:8000/send/coin/{wallet_2_address}/5000/123")
 
             time.sleep(10)
 
-
             for i in range(4):
-                urllib.request.urlopen(f"http://localhost:8000/send/coin/{wallet_2_address}/5000/123")
+                urllib.request.urlopen(
+                    f"http://localhost:8000/send/coin/{wallet_2_address}/5000/123")
                 time.sleep(10)
 
-
-            balance_wallet_1 = json.loads(urllib.request.urlopen("http://localhost:8010/wallet/balance").read().decode())
+            balance_wallet_1 = json.loads(urllib.request.urlopen(
+                "http://localhost:8010/wallet/balance").read().decode())
 
             if balance_wallet_1 == 24000.0:
                 success = True
 
-
-        self.assertEqual(success,True,"A problem in same network one and multi transaction -multi.")
-
+        self.assertEqual(
+            success, True, "A problem in same network one and multi transaction -multi.")
 
     def test_4_same_network_long_term_multi_transacton(self):
         """
@@ -114,30 +125,31 @@ class Test_Decentra_Network_Docker(unittest.TestCase):
             temp_environment.install()
             temp_environment.run()
             temp_environment.start()
-            wallet_2_json = json.loads(urllib.request.urlopen("http://localhost:8010/wallet/create/123").read().decode())
-            wallet_2_address = wallet_2_json[0].replace("0) ", "").replace(" - CURRENTLY USED\n", "")
+            wallet_2_json = json.loads(urllib.request.urlopen(
+                "http://localhost:8010/wallet/create/123").read().decode())
+            wallet_2_address = wallet_2_json[0].replace(
+                "0) ", "").replace(" - CURRENTLY USED\n", "")
 
-
-
-            urllib.request.urlopen(f"http://localhost:8000/send/coin/{wallet_2_address}/5000/123")
+            urllib.request.urlopen(
+                f"http://localhost:8000/send/coin/{wallet_2_address}/5000/123")
             time.sleep(10)
 
-            urllib.request.urlopen(f"http://localhost:8000/send/coin/{wallet_2_address}/5000/123")
+            urllib.request.urlopen(
+                f"http://localhost:8000/send/coin/{wallet_2_address}/5000/123")
             time.sleep(10)
 
-            urllib.request.urlopen(f"http://localhost:8000/send/coin/{wallet_2_address}/5000/123")
+            urllib.request.urlopen(
+                f"http://localhost:8000/send/coin/{wallet_2_address}/5000/123")
             time.sleep(10)
-            balance_wallet_1 = json.loads(urllib.request.urlopen("http://localhost:8010/wallet/balance").read().decode())
-            
+            balance_wallet_1 = json.loads(urllib.request.urlopen(
+                "http://localhost:8010/wallet/balance").read().decode())
+
             if balance_wallet_1 == 14000.0:
                 success = True
-        
-        self.assertEqual(success,True,"A problem in same network one transaction -3.")
 
-import os
-import sys
-sys.path.append(os.path.join(os.path.dirname(__file__), "..",".."))
-import urllib.request, json
-import time
-from auto_builders.docker import Decentra_Network_Docker
+        self.assertEqual(
+            success, True, "A problem in same network one transaction -3.")
+
+
+sys.path.append(os.path.join(os.path.dirname(__file__), "..", ".."))
 unittest.main(exit=False)
