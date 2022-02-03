378a4f19038d36d2dfa143d09ebd2afa34f1aab2
Restyled.io <commits@restyled.io>
2022-02-03 12:36:33 +0000

    Restyled by black

--- src/consensus/consensus_second_round.py
+++ src/consensus/consensus_second_round.py
@@ -38,8 +38,7 @@ def consensus_round_2(block):
 
     candidate_class = GetCandidateBlocks()
     dprint("Raund 2 Conditions")
-    dprint(len(candidate_class.candidate_block_hashes)
-           > ((len(unl_nodes) * 80) / 100))
+    dprint(len(candidate_class.candidate_block_hashes) > ((len(unl_nodes) * 80) / 100))
     dprint((int(time.time()) - block.raund_2_starting_time) < block.raund_2_time)
 
     if len(candidate_class.candidate_block_hashes) > ((len(unl_nodes) * 80) / 100):
@@ -69,8 +68,7 @@ def consensus_round_2(block):
                     else:
                         print("Raund 2: my block is not valid")
                         node = mynode.main_node
-                        unl_list = get_as_node_type(
-                            [candidate_block["sender"]])
+                        unl_list = get_as_node_type([candidate_block["sender"]])
                         node.send_data_to_node(unl_list[0], "sendmefullblock")
                         block.dowload_true_block = candidate_block["sender"]
                     block.save_block()
