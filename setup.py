3bb961c3904f5338ef99b334e7fe2cd93b763a92
Onur Atakan ULUSOY <atadogan06@gmail.com>
2022-02-12 18:34:45 +0000

    Changed the version number

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from setuptools import setup
 
 setup(
     name="decentra_network",
-    version="0.13.0",
+    version="0.14.0",
     description=
     """This is an open source decentralized application network. In this network, you can develop and publish decentralized applications.""",
     long_description=open("README.md", "r").read(),
--- setups/gui/setup.py
+++ setups/gui/setup.py
@@ -8,14 +8,14 @@ from setuptools import setup
 
 setup(
     name="decentra_network_gui",
-    version="0.13.0",
+    version="0.14.0",
     description="""This is GUI mode installer for Decentra Network Core""",
     url="https://decentra-network.github.io/Decentra-Network/",
     author="Decentra Network Developers",
     author_email="atadogan06@gmail.com",
     license="MPL-2.0",
     install_requires="""
-decentra-network==0.13.0
+decentra-network==0.14.0
 Kivy==2.0.0
 kivymd==0.104.1
 kivymd_extensions.sweetalert==0.1.5
