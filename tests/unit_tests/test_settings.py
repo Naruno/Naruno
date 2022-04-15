3af846e3d5cbd3089ce3ac55d8456eb038ace8a3
Restyled.io <commits@restyled.io>
2022-04-15 13:30:13 +0000

    Restyled by reorder-python-imports

--- tests/unit_tests/test_settings.py
+++ tests/unit_tests/test_settings.py
@@ -4,12 +4,12 @@
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at https://mozilla.org/MPL/2.0/.
-
 import os
 import sys
 import unittest
 
-from lib.settings_system import save_settings, the_settings
+from lib.settings_system import save_settings
+from lib.settings_system import the_settings
 
 
 class Test_Settings(unittest.TestCase):
