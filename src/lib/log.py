ec02f382b54a7ff6f9e05212cfc585381bfdd746
Onur Atakan ULUSOY <ataogan06@gmail.com>
2022-04-22 18:37:26 +0000

    logging: Added name variable for get_logger

--- src/lib/log.py
+++ src/lib/log.py
@@ -10,15 +10,15 @@ import sys
 import time
 
 
-def get_logger():
-    logger = logging.getLogger("Decentra Network")
+def get_logger(name):
+    logger = logging.getLogger(name)
     logger.setLevel(logging.DEBUG)
     # create console handler and set level to debug
     ch = logging.StreamHandler()
     ch.setLevel(logging.DEBUG)
     # create formatter
     formatter = logging.Formatter(
-        "%(asctime)s - %(levelname)s - %(message)s")
+        "%(asctime)s - %(name)s - %(levelname)s - %(message)s")
     # add formatter to ch
     ch.setFormatter(formatter)
     # add ch to logger
