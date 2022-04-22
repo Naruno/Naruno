d27fdc67ad97e65a50d32318c3092a56276b29a5
Onur Atakan ULUSOY <ataogan06@gmail.com>
2022-04-22 18:35:44 +0000

    logging: Added log.py

---
+++ src/lib/log.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at https://mozilla.org/MPL/2.0/.
+import logging
+import os
+import sys
+import time
+
+
+def get_logger():
+    logger = logging.getLogger("Decentra Network")
+    logger.setLevel(logging.DEBUG)
+    # create console handler and set level to debug
+    ch = logging.StreamHandler()
+    ch.setLevel(logging.DEBUG)
+    # create formatter
+    formatter = logging.Formatter(
+        "%(asctime)s - %(levelname)s - %(message)s")
+    # add formatter to ch
+    ch.setFormatter(formatter)
+    # add ch to logger
+    logger.addHandler(ch)
+    return logger
