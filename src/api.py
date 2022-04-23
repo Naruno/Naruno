e31d24cd2893d131216dbf70c0d50fd1c9115c0d
Onur Atakan ULUSOY <41792982+onuratakan@users.noreply.github.com>
2022-04-23 22:32:04 +0300

    Removed unused imports

--- src/api.py
+++ src/api.py
@@ -5,8 +5,6 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at https://mozilla.org/MPL/2.0/.
 import argparse
-import sys
-from cmath import log
 
 import flask
 from blockchain.block.create_block import CreateBlock
