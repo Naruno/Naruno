519a45375e8ae944941b551a6bb04244782584d5
Onur Atakan ULUSOY <41792982+onuratakan@users.noreply.github.com>
2022-04-25 21:21:21 +0300

    Merge #377 status: Lots of status information added

--- src/lib/status.py
+++ src/lib/status.py
@@ -4,11 +4,11 @@
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at https://mozilla.org/MPL/2.0/.
-
-
 import time
 
 from blockchain.block.get_block import GetBlock
+from node.unl import Unl
+from transactions.get_my_transaction import GetMyTransaction
 
 
 def Status():
@@ -22,16 +22,39 @@ def Status():
         time.sleep(5)
         new_time = time.time()
         new_block = GetBlock()
-        difference = int(new_time - start_time) 
-        if not (first_block.sequance_number + first_block.empty_block_number) == (new_block.sequance_number + new_block.empty_block_number):
+        difference = int(new_time - start_time)
+
+        status_json = {
+            "status":
+            "",
+            "first_block":
+            str(first_block.__dict__),
+            "new_block":
+            str(new_block.__dict__),
+            "last_transaction_of_block":
+            str(new_block.validating_list[-1])
+            if len(new_block.validating_list) > 0 else "",
+            "last_transaction_of_us":
+            str([i.__dict__ for i in GetMyTransaction()]),
+            "connected_nodes": [
+                str(the_connections) for the_connections in
+                Unl.get_as_node_type(Unl.get_unl_nodes())
+            ],
+        }
+
+        if not (first_block.sequance_number +
+                first_block.empty_block_number) == (
+                    new_block.sequance_number + new_block.empty_block_number):
             if difference <= 6:
-                return "Good"
+                status_json["status"] = "Good"
             elif difference <= 10:
-                return "Not bad"
+                status_json["status"] = "Not bad"
             elif difference <= 20:
-                return "Bad"
+                status_json["status"] = "Bad"
             elif difference >= 20:
-                return "Very bad"
+                status_json["status"] = "Very bad"
         else:
             if difference >= 20:
-                return "Not work"
+                status_json["status"] = "Not work"
+
+        return status_json
