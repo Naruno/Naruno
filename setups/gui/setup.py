5aa63d97c185ce59d125c3b61ff4f51a4dce835a
Onur Atakan ULUSOY <atadogan06@gmail.com>
2022-05-30 22:41:50 +0300

    setup-setups: Changed the version number

--- setups/gui/setup.py
+++ setups/gui/setup.py
@@ -8,7 +8,7 @@ from setuptools import setup
 
 setup(
     name="decentra_network_gui",
-    version="0.15.2",
+    version="0.16.0",
     description="""This is GUI mode installer for Decentra Network Core""",
     url="https://decentra-network.github.io/Decentra-Network/",
     author="Decentra Network Developers",
