af0b1a9d46d1ebfb545615961f17b3f9d5d1f179
Restyled.io <commits@restyled.io>
2022-06-17 23:04:16 +0000

    Restyled by black

--- tests/stability_tests/local/test_decentra_network_local_7_1.py
+++ tests/stability_tests/local/test_decentra_network_local_7_1.py
@@ -16,34 +16,47 @@ import unittest
 
 
 class Test_Decentra_Network_Local(unittest.TestCase):
-
     def test_4_same_network_long_term_multi_transacton(self):
         """
         Send coin to 2.wallet from 1.wallet
         """
 
-        wallet_2_json = json.loads(urllib.request.urlopen(
-            "http://localhost:8101/wallet/create/123").read().decode())
-        wallet_2_address = wallet_2_json[0].replace(
-            "0) ", "").replace(" - CURRENTLY USED\n", "")
+        wallet_2_json = json.loads(
+            urllib.request.urlopen("http://localhost:8101/wallet/create/123")
+            .read()
+            .decode()
+        )
+        wallet_2_address = (
+            wallet_2_json[0].replace("0) ", "").replace(" - CURRENTLY USED\n", "")
+        )
 
         urllib.request.urlopen(
-            f"http://localhost:8000/send/coin/{wallet_2_address}/5000/123")
+            f"http://localhost:8000/send/coin/{wallet_2_address}/5000/123"
+        )
         time.sleep(25)
-        balance_wallet_1 = json.loads(urllib.request.urlopen(
-            "http://localhost:8101/wallet/balance").read().decode())
-        self.assertEqual(balance_wallet_1, 4000.0,
-                         "A problem in same network one transaction -1.")
+        balance_wallet_1 = json.loads(
+            urllib.request.urlopen("http://localhost:8101/wallet/balance")
+            .read()
+            .decode()
+        )
+        self.assertEqual(
+            balance_wallet_1, 4000.0, "A problem in same network one transaction -1."
+        )
 
         time.sleep(45)
 
         urllib.request.urlopen(
-            f"http://localhost:8000/send/coin/{wallet_2_address}/5000/123")
+            f"http://localhost:8000/send/coin/{wallet_2_address}/5000/123"
+        )
         time.sleep(25)
-        balance_wallet_1 = json.loads(urllib.request.urlopen(
-            "http://localhost:8101/wallet/balance").read().decode())
-        self.assertEqual(balance_wallet_1, 9000.0,
-                         "A problem in same network one transaction -3.")
+        balance_wallet_1 = json.loads(
+            urllib.request.urlopen("http://localhost:8101/wallet/balance")
+            .read()
+            .decode()
+        )
+        self.assertEqual(
+            balance_wallet_1, 9000.0, "A problem in same network one transaction -3."
+        )
 
 
 sys.path.append(os.path.join(os.path.dirname(__file__), "..", "..", ".."))
