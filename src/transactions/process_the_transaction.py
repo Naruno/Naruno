b3cec2251001b8afa6aaee025425960d8ebd228a
Onur Atakan ULUSOY <atadogan06@gmail.com>
2022-06-17 19:16:50 +0300

    transaction: Added a new list to proccess the transaction function for sacalability in big lists (#439)
    * transaction: Added a new list to proccess the transaction function for sacalability in big lists
    
    * Restyle transaction: Added a new list to proccess the transaction function for sacalability in big lists (#440)
    
    * Restyled by autopep8
    
    * Restyled by black
    
    * Restyled by isort
    
    * Restyled by reorder-python-imports
    
    * Restyled by yapf
    
    Co-authored-by: Restyled.io <commits@restyled.io>
    
    Co-authored-by: restyled-io[bot] <32688539+restyled-io[bot]@users.noreply.github.com>
    Co-authored-by: Restyled.io <commits@restyled.io>

--- src/transactions/process_the_transaction.py
+++ src/transactions/process_the_transaction.py
@@ -22,6 +22,7 @@ def ProccesstheTransaction(block):
     temp_validating_list = block.validating_list
 
     temp_accounts = GetAccounts()
+    new_added_accounts_list = []
 
     for trans in block.validating_list:
         touser_inlist = False
@@ -42,13 +43,15 @@ def ProccesstheTransaction(block):
 
         # If not included in the temp_accounts, add.
         if not touser_inlist:
-            temp_accounts.append(Account(trans.toUser, float(trans.amount)))
+            new_added_accounts_list.append(
+                Account(trans.toUser, float(trans.amount)))
 
     # Syncs new sorted list to block.validating_list
 
     block.validating_list = sorted(temp_validating_list,
                                    key=lambda x: x.fromUser)
 
-    new_accounts_list = sorted(temp_accounts, key=lambda x: x.Address)
+    new_accounts_list = temp_accounts + sorted(new_added_accounts_list,
+                                               key=lambda x: x.Address)
 
     save_accounts(new_accounts_list)
