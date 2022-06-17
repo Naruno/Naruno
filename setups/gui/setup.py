c32ee4e7026a69c509bc40e155f4aef0e3b68b95
Onur Atakan ULUSOY <atadogan06@gmail.com>
2022-06-17 17:14:43 +0000

    Added needed for 0.19.0-alpha version

--- setups/gui/setup.py
+++ setups/gui/setup.py
@@ -8,7 +8,7 @@ from setuptools import setup
 
 setup(
     name="decentra_network_gui",
-    version="0.18.3",
+    version="0.19.0",
     description="""This is GUI mode installer for Decentra Network Core""",
     url="https://decentra-network.github.io/Decentra-Network/",
     author="Decentra Network Developers",
