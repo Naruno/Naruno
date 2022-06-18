4e7c0bd186c64d35b6dc7f6d9157e3ff757a7ba9
Onur Atakan ULUSOY <atadogan06@gmail.com>
2022-06-19 00:41:56 +0300

    Update test_decentra_network_local.py

--- tests/functional_tests/local/test_decentra_network_local.py
+++ tests/functional_tests/local/test_decentra_network_local.py
@@ -39,7 +39,7 @@ class Test_Decentra_Network_Local(unittest.TestCase):
                 urllib.request.urlopen(
                     f"http://localhost:8000/send/coin/{wallet_2_address}/5000/123")
 
-                time.sleep(25)
+                time.sleep(30)
 
             balance_wallet_1 = json.loads(
                 urllib.request.urlopen(
