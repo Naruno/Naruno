18cf77237240252e2aa1fc1955658629454102d4
Restyled.io <commits@restyled.io>
2022-06-18 19:05:48 +0000

    Restyled by autopep8

--- tests/functional_tests/local/test_decentra_network_local.py
+++ tests/functional_tests/local/test_decentra_network_local.py
@@ -5,13 +5,14 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
+import json
+import urllib.request
+from auto_builders.local import Decentra_Network_Local
+import time
 import unittest
 import os
 import sys
-sys.path.append(os.path.join(os.path.dirname(__file__), "..","..",".."))
-import urllib.request, json
-import time
-from auto_builders.local import Decentra_Network_Local
+sys.path.append(os.path.join(os.path.dirname(__file__), "..", "..", ".."))
 
 
 class Test_Decentra_Network_Local(unittest.TestCase):
@@ -120,7 +121,6 @@ class Test_Decentra_Network_Local(unittest.TestCase):
         self.assertEqual(
             success, True,
             "A problem in same network one and multi transaction -multi.")
-
 
 
 unittest.main(exit=False)
