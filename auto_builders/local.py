702c4050c8c368ce73e8d2ea60d186e560cd315b
Restyled.io <commits@restyled.io>
2022-06-19 09:39:11 +0000

    Restyled by black

--- auto_builders/local.py
+++ auto_builders/local.py
@@ -15,23 +15,25 @@ import random
 
 
 class Decentra_Network_Local:
-
     def __init__(self, number_of_nodes=3, number_of_security_circle=1):
         self.number_of_nodes = number_of_nodes - 1
         self.number_of_security_circle = number_of_security_circle
         nodes_list = list(range(self.number_of_nodes))
         self.circles = [
-            nodes_list[x:x + ((self.number_of_nodes + 1) //
-                              self.number_of_security_circle)]
+            nodes_list[
+                x : x + ((self.number_of_nodes + 1) // self.number_of_security_circle)
+            ]
             for x in range(
                 0,
                 len(nodes_list),
-                ((self.number_of_nodes + 1) //
-                 self.number_of_security_circle),
+                ((self.number_of_nodes + 1) // self.number_of_security_circle),
             )
         ]
-        random_amount = int(10 * (self.number_of_security_circle / self.number_of_nodes)
-                            ) if not self.number_of_security_circle == 1 else 0
+        random_amount = (
+            int(10 * (self.number_of_security_circle / self.number_of_nodes))
+            if not self.number_of_security_circle == 1
+            else 0
+        )
         for i in range(random_amount):
             random_circle = random.randint(0, len(self.circles) - 1)
             random_node = random.randint(0, self.number_of_nodes - 1)
@@ -64,14 +66,13 @@ class Decentra_Network_Local:
                 os.kill(int(fields[0]), signal.SIGKILL)
         os.system("rm -r -f Decentra-Network-0.out")
         for i in range(self.number_of_nodes):
-            os.system(
-                f"rm -r -f Decentra-Network-{i + 1}.out"
-            )
+            os.system(f"rm -r -f Decentra-Network-{i + 1}.out")
 
     def run(self):
         time.sleep(5 * self.number_of_nodes)
         os.system(
-            "nohup python3 Decentra-Network-0/src/api.py >> Decentra-Network-0.out &")
+            "nohup python3 Decentra-Network-0/src/api.py >> Decentra-Network-0.out &"
+        )
         for i in range(self.number_of_nodes):
             os.system(
                 f"nohup python3 Decentra-Network-{i+1}/src/api.py -p {8100 + i + 1} >> Decentra-Network-{i + 1}.out &"
@@ -82,15 +83,13 @@ class Decentra_Network_Local:
         urllib.request.urlopen("http://localhost:8000/settings/test/on")
         urllib.request.urlopen("http://localhost:8000/settings/debug/on")
         for i in range(self.number_of_nodes):
-            urllib.request.urlopen(
-                f"http://localhost:{8100 + i + 1}/settings/debug/on")
+            urllib.request.urlopen(f"http://localhost:{8100 + i + 1}/settings/debug/on")
 
     def creating_the_wallets(self):
         time.sleep(1 * self.number_of_nodes)
         urllib.request.urlopen("http://localhost:8000/wallet/create/123")
         for i in range(self.number_of_nodes):
-            urllib.request.urlopen(
-                f"http://localhost:{8100 + i + 1}/wallet/create/123")
+            urllib.request.urlopen(f"http://localhost:{8100 + i + 1}/wallet/create/123")
 
     def starting_the_nodest(self):
         time.sleep(1 * self.number_of_nodes)
@@ -103,20 +102,23 @@ class Decentra_Network_Local:
     def unl_nodes_settting(self):
         time.sleep(1 * self.number_of_nodes)
         node_id_1 = json.loads(
-            urllib.request.urlopen(
-                "http://localhost:8000/node/id").read().decode())
+            urllib.request.urlopen("http://localhost:8000/node/id").read().decode()
+        )
         for i in range(self.number_of_nodes):
             urllib.request.urlopen(
-                f"http://localhost:{8100 + i + 1}/node/newunl/?{node_id_1}")
+                f"http://localhost:{8100 + i + 1}/node/newunl/?{node_id_1}"
+            )
 
         if self.number_of_security_circle == 1:
             for i in range(self.number_of_nodes):
                 node_id_2 = json.loads(
-                    urllib.request.urlopen(
-                        f"http://localhost:{8100 + i + 1}/node/id").read().
-                    decode())
+                    urllib.request.urlopen(f"http://localhost:{8100 + i + 1}/node/id")
+                    .read()
+                    .decode()
+                )
                 urllib.request.urlopen(
-                    f"http://localhost:8000/node/newunl/?{node_id_2}")
+                    f"http://localhost:8000/node/newunl/?{node_id_2}"
+                )
                 for i_n in range(self.number_of_nodes):
                     if not i == i_n:
                         urllib.request.urlopen(
@@ -127,10 +129,14 @@ class Decentra_Network_Local:
                 for i in circle:
                     node_id_2 = json.loads(
                         urllib.request.urlopen(
-                            f"http://localhost:{8100 + i + 1}/node/id").read().
-                        decode())
+                            f"http://localhost:{8100 + i + 1}/node/id"
+                        )
+                        .read()
+                        .decode()
+                    )
                     urllib.request.urlopen(
-                        f"http://localhost:8000/node/newunl/?{node_id_2}")
+                        f"http://localhost:8000/node/newunl/?{node_id_2}"
+                    )
                     for i_n in circle:
                         if not i == i_n:
                             urllib.request.urlopen(
@@ -141,7 +147,8 @@ class Decentra_Network_Local:
         time.sleep(1 * self.number_of_nodes)
         for i in range(self.number_of_nodes):
             urllib.request.urlopen(
-                f"http://localhost:8000/node/connect/0.0.0.0/{8010 + i + 1}")
+                f"http://localhost:8000/node/connect/0.0.0.0/{8010 + i + 1}"
+            )
 
         if self.number_of_security_circle == 1:
             for i in range(self.number_of_nodes):
@@ -174,10 +181,9 @@ if __name__ == "__main__":
 
     parser.add_argument("-nn", "--nodenumber", type=int, help="Node Number")
 
-    parser.add_argument("-scn",
-                        "--securitycirclenumber",
-                        type=int,
-                        help="Security Circle Number")
+    parser.add_argument(
+        "-scn", "--securitycirclenumber", type=int, help="Security Circle Number"
+    )
 
     parser.add_argument("-i", "--install", action="store_true", help="Install")
 
@@ -193,8 +199,9 @@ if __name__ == "__main__":
         parser.print_help()
 
     if args.securitycirclenumber is not None:
-        temp_environment = Decentra_Network_Local(args.nodenumber,
-                                                  args.securitycirclenumber)
+        temp_environment = Decentra_Network_Local(
+            args.nodenumber, args.securitycirclenumber
+        )
     else:
         temp_environment = Decentra_Network_Local(args.nodenumber)
 
