e54cc63c3487893dd02a81f97b27626af8bbfa35
Onur Atakan ULUSOY <atadogan06@gmail.com>
2022-06-23 13:52:25 +0000

    gui: Added argparse to gui

--- src/gui.py
+++ src/gui.py
@@ -4,7 +4,10 @@
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+
 import os
+import argparse
 
 from kivy import Config
 from kivy.lang import Builder
