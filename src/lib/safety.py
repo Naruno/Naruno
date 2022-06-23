83f2e87b6f4d24c6e2a8d742be5398b9a8118810
Restyled.io <commits@restyled.io>
2022-06-23 14:40:25 +0000

    Restyled by yapf

--- src/lib/safety.py
+++ src/lib/safety.py
@@ -29,7 +29,8 @@ def safety_check(interface=None, timeout=None):
                 logger.info("Safe")
         else:
             if the_settings()["debug_mode"]:
-                logger.info("Control check is none but passing because of debug mode")
+                logger.info(
+                    "Control check is none but passing because of debug mode")
             else:
                 logger.info("NOT Safe (Control check is None)")
                 exit()
