4060a1f7d3c6f82423bbc8ea16e6b60de70986ae
Onur Atakan ULUSOY <atadogan06@gmail.com>
2022-06-25 01:49:31 +0300

    Update block_main.py

--- src/blockchain/block/block_main.py
+++ src/blockchain/block/block_main.py
@@ -49,7 +49,7 @@ class Block:
     ):
         self.genesis_time = int(time.time())
         self.start_time = int(time.time())
-        self.block_time = 10
+        self.block_time = 11
 
         self.newly = False
 
@@ -80,11 +80,11 @@ class Block:
         self.minumum_transfer_amount = 1000
 
         self.raund_1_starting_time = None
-        self.raund_1_time = 5
+        self.raund_1_time = 4
         self.raund_1 = False
 
         self.raund_2_starting_time = None
-        self.raund_2_time = 5
+        self.raund_2_time = 4
         self.raund_2 = False
 
         self.consensus_timer = 0.50
