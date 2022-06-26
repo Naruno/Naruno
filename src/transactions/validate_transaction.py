c09324a7265a6f98c8de4f7310160473bf701966
Restyled.io <commits@restyled.io>
2022-06-26 00:35:18 +0000

    Restyled by reorder-python-imports

--- src/transactions/validate_transaction.py
+++ src/transactions/validate_transaction.py
@@ -4,13 +4,11 @@
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at https://mozilla.org/MPL/2.0/.
-
-
 import os
 import pickle
-from distutils.log import info
 
 from config import MY_TRANSACTION_PATH
+from distutils.log import info
 from lib.config_system import get_config
 from transactions.get_my_transaction import GetMyTransaction
 from transactions.save_my_transaction import SaveMyTransaction
