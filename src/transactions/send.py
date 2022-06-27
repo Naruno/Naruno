de490e8b3462b94f07dd744f159ee93d8cdc156c
Onur Atakan ULUSOY <atadogan06@gmail.com>
2022-06-27 18:16:05 +0000

    transaction: Limit set for len of data

--- src/transactions/send.py
+++ src/transactions/send.py
@@ -38,13 +38,17 @@ def send(password, to_user, amount, data=None):
 
     if not isinstance(amount, float):
         print("This is not int or float coin amount.")
-        return None
+        return False
 
     if amount < 0:
         print("This is negative coin amount.")
-        return None
+        return False
 
     block = GetBlock()
+
+    if not (1000000 / block.max_tx_number) >= len(data):
+        print("The data is too long.")
+        return False
 
     decimal_amount = len(str(block.transaction_fee).split(".")[1])
     if len(str(amount).split(".")[1]) > decimal_amount:
