d0ab5916dff225072f943ab170bf5041b0a144c5
Restyled.io <commits@restyled.io>
2022-06-30 18:29:58 +0000

    Restyled by yapf

--- src/wallet/wallet_import.py
+++ src/wallet/wallet_import.py
@@ -81,7 +81,8 @@ def wallet_import(wallet, mode, password=None):
         if not password is None:
             if not list(temp_saved_wallet).index(account) == 0:
 
-                return decrypt(temp_saved_wallet[account]["privatekey"], password)
+                return decrypt(temp_saved_wallet[account]["privatekey"],
+                               password)
             else:
                 if wallet == -1:
                     my_private_key = temp_saved_wallet[account]["privatekey"]
@@ -100,13 +101,10 @@ def wallet_import(wallet, mode, password=None):
 
     elif mode == 3:
         my_address = temp_saved_wallet[account]["publickey"]
-        my_address = "".join(
-            [
-                l.strip()
-                for l in my_address.splitlines()
-                if l and not l.startswith("-----")
-            ]
-        )
+        my_address = "".join([
+            l.strip() for l in my_address.splitlines()
+            if l and not l.startswith("-----")
+        ])
         my_address = Address(my_address)
         return my_address
     else:
@@ -114,9 +112,10 @@ def wallet_import(wallet, mode, password=None):
 
 
 def Address(publickey):
-    the_public_key = "".join(
-        [l.strip() for l in publickey.splitlines() if l and not l.startswith("-----")]
-    )
+    the_public_key = "".join([
+        l.strip() for l in publickey.splitlines()
+        if l and not l.startswith("-----")
+    ])
     return sha256(
-        sha256(the_public_key.encode("utf-8")).hexdigest().encode("utf-8")
-    ).hexdigest()[-40:]
+        sha256(the_public_key.encode("utf-8")).hexdigest().encode(
+            "utf-8")).hexdigest()[-40:]
