39c09b0e94131d651a5d154a55077a8c33c894dc
Restyled.io <commits@restyled.io>
2022-06-30 13:40:27 +0000

    Restyled by isort

--- src/cli.py
+++ src/cli.py
@@ -15,15 +15,10 @@ from blockchain.block.get_block import GetBlock, GetBlockFromOtherNode
 from config import MY_TRANSACTION_EXPORT_PATH
 from lib.export import export_the_transactions
 from lib.log import get_logger
-from lib.mixlib import banner_maker
-from lib.mixlib import menu_maker
-from lib.mixlib import menu_space
-from lib.mixlib import question_maker
-from lib.mixlib import quit_menu_maker
+from lib.mixlib import (banner_maker, menu_maker, menu_space, question_maker,
+                        quit_menu_maker)
 from lib.safety import safety_check
-from lib.settings_system import debug_mode
-from lib.settings_system import test_mode
-from lib.settings_system import the_settings
+from lib.settings_system import debug_mode, test_mode, the_settings
 from lib.status import Status
 from node.node import Node
 from node.node_connection import Node_Connection
@@ -31,9 +26,9 @@ from node.unl import Unl
 from transactions.print_transactions import PrintTransactions
 from transactions.send import send
 from wallet.delete_current_wallet import delete_current_wallet
-from wallet.wallet_import import wallet_import
 from wallet.print_wallets import print_wallets
 from wallet.wallet_create import wallet_create
+from wallet.wallet_import import wallet_import
 from wallet.wallet_selector import wallet_selector
 
 logger = get_logger("CLI")
