b53ed6cfeb4dfc9d2840561f135ddce7f1f7bcf0
Onur Atakan ULUSOY <atadogan06@gmail.com>
2022-06-30 09:54:32 +0000

    tests: Adding unit test for my transaction functions

---
+++ tests/unit_tests/test_transactions.py
@@ -0,0 +1,72 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+import unittest
+
+
+class Test_Settings(unittest.TestCase):
+
+    def test_1_get_my_transaction_non(self):
+        backup = GetMyTransaction()
+        SaveMyTransaction([])
+
+        result = GetMyTransaction()
+
+        SaveMyTransaction(backup)
+        self.assertEqual(result, [])
+
+    def test_2_get_my_transaction_not_validated(self):
+        backup = GetMyTransaction()
+        new_transaction = Transaction(1, "", "", "", "", 1, 1, 1)
+        SavetoMyTransaction(new_transaction)
+        
+        result = GetMyTransaction()
+
+        SaveMyTransaction(backup)
+        self.assertEqual(result[0][0].signature, new_transaction.signature)
+        self.assertEqual(result[0][1], False)
+
+    def test_3_get_my_transaction_validated(self):
+        backup = GetMyTransaction()
+        new_transaction = Transaction(1, "", "", "", "", 1, 1, 1)
+        SavetoMyTransaction(new_transaction, validated=True)
+        
+        result = GetMyTransaction()
+
+        SaveMyTransaction(backup)
+        self.assertEqual(result[0][0].signature, new_transaction.signature)
+        self.assertEqual(result[0][1], True)
+
+    def test_4_validate_my_transaction(self):
+        backup = GetMyTransaction()
+        new_transaction = Transaction(1, "", "", "", "", 1, 1, 1)
+        SavetoMyTransaction(new_transaction)
+        
+        result = GetMyTransaction()
+
+        ValidateTransaction(new_transaction)
+
+        result_2 = GetMyTransaction()
+
+
+        SaveMyTransaction(backup)
+        self.assertEqual(result[0][0].signature, new_transaction.signature)
+        self.assertEqual(result[0][1], False)
+
+        self.assertEqual(result_2[0][0].signature, new_transaction.signature)
+        self.assertEqual(result_2[0][1], True)
+
+
+import os
+import sys
+sys.path.append(os.path.join(os.path.dirname(__file__), "..","..","src"))
+from transactions.get_my_transaction import GetMyTransaction
+from transactions.save_my_transaction import SaveMyTransaction
+from transactions.save_to_my_transaction import SavetoMyTransaction
+from transactions.validate_transaction import ValidateTransaction
+from transactions.transaction import Transaction
+unittest.main(exit=False)
