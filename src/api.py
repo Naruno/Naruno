b27e46681703ccf42b64dd0af44a677cd188dc78
Onur Atakan ULUSOY <atadogan06@gmail.com>
2022-06-30 13:48:05 +0000

    Removed importing mistakes

--- src/api.py
+++ src/api.py
@@ -5,7 +5,6 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at https://mozilla.org/MPL/2.0/.
 import argparse
-from inspect import getblock
 
 import flask
 from accounts.get_balance import GetBalance
