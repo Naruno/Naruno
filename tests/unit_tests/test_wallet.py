798d65e0c000da6f9778ab1f2be7d7e6385a7740
Restyled.io <commits@restyled.io>
2022-06-30 14:27:30 +0000

    Restyled by yapf

--- tests/unit_tests/test_wallet.py
+++ tests/unit_tests/test_wallet.py
@@ -25,6 +25,7 @@ from wallet.wallet_selector import wallet_selector
 
 
 class Test_Wallet(unittest.TestCase):
+
     def test_1_wallet_by_creating_saving_importing_and_deleting_a_wallet(self):
 
         password = "123"
@@ -36,21 +37,22 @@ class Test_Wallet(unittest.TestCase):
         result = False
         for each_wallet in saved_wallets:
             if temp_private_key == (saved_wallets[each_wallet]["privatekey"]):
-                if temp_private_key == (wallet_import(each_wallet, 1, password)):
+                if temp_private_key == (wallet_import(each_wallet, 1,
+                                                      password)):
 
                     wallet_delete(each_wallet)
-                    result = True if each_wallet not in get_saved_wallet() else False
+                    result = True if each_wallet not in get_saved_wallet(
+                    ) else False
                     break
-                elif decrypt(temp_private_key, password) == (
-                    wallet_import(each_wallet, 1, password)
-                ):
+                elif decrypt(temp_private_key, password) == (wallet_import(
+                        each_wallet, 1, password)):
                     wallet_delete(each_wallet)
-                    result = True if each_wallet not in get_saved_wallet() else False
+                    result = True if each_wallet not in get_saved_wallet(
+                    ) else False
                     break
 
-        self.assertEqual(
-            result, True, "A problem on the saving and importing the wallet."
-        )
+        self.assertEqual(result, True,
+                         "A problem on the saving and importing the wallet.")
 
     def test_2_wallet_by_private_pem_conversion(self):
 
