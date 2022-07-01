f875361f1b81a2321640fdcbcd6b110a9546168e
Onur Atakan ULUSOY <atadogan06@gmail.com>
2022-07-01 20:37:53 +0000

    tests: Removed test number from functional and stability tests

--- tests/functional_tests/local/test_decentra_network_local.py
+++ tests/functional_tests/local/test_decentra_network_local.py
@@ -18,7 +18,7 @@ from auto_builders.local import Decentra_Network_Local
 
 class Test_Decentra_Network_Local(unittest.TestCase):
 
-    def test_1_multiple_transaction_with_time_difference(self):
+    def test_multiple_transaction_with_time_difference(self):
         """
         Send coin to 2.wallet from 1.wallet
         """
