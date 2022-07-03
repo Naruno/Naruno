342e45d40456b283d791e13a5c764accbd27d88a
Restyled.io <commits@restyled.io>
2022-07-03 20:42:03 +0000

    Restyled by reorder-python-imports

--- src/blockchain/block/save_block_to_blockchain_db.py
+++ src/blockchain/block/save_block_to_blockchain_db.py
@@ -4,12 +4,11 @@
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at https://mozilla.org/MPL/2.0/.
-
-
 import pickle
 
 from accounts.get_accounts import GetAccounts
-from blockchain.block.blocks_hash import GetBlockshash, GetBlockshash_part
+from blockchain.block.blocks_hash import GetBlockshash
+from blockchain.block.blocks_hash import GetBlockshash_part
 from config import BLOCKS_PATH
 from wallet.wallet_import import wallet_import
 
