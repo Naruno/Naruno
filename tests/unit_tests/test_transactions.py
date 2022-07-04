3f25b28e9ae80f70b7e79725152ea07d9849bffa
Restyled.io <commits@restyled.io>
2022-07-04 21:27:40 +0000

    Restyled by isort

--- tests/unit_tests/test_transactions.py
+++ tests/unit_tests/test_transactions.py
@@ -4,27 +4,30 @@
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at https://mozilla.org/MPL/2.0/.
-from transactions.transaction import Transaction
-from transactions.send import send
-from transactions.pending_to_validating import PendingtoValidating
-from transactions.my_transactions.validate_transaction import ValidateTransaction
-from transactions.my_transactions.save_to_my_transaction import SavetoMyTransaction
-from transactions.my_transactions.save_my_transaction import SaveMyTransaction
-from transactions.my_transactions.get_my_transaction import GetMyTransaction
-from transactions.check.type.check_type import Check_Type
-from transactions.check.len.check_len import Check_Len
-from transactions.check.datas.check_datas import Check_Datas
-from transactions.get_transaction import GetTransaction
-from transactions.check.check_transaction import CheckTransaction
-from transactions.process_the_transaction import ProccesstheTransaction
-from blockchain.block.change_transaction_fee import ChangeTransactionFee
-from blockchain.block.block_main import Block
-from accounts.account import Account
 import copy
-import time
-import unittest
 import os
 import sys
+import time
+import unittest
+
+from accounts.account import Account
+from blockchain.block.block_main import Block
+from blockchain.block.change_transaction_fee import ChangeTransactionFee
+from transactions.check.check_transaction import CheckTransaction
+from transactions.check.datas.check_datas import Check_Datas
+from transactions.check.len.check_len import Check_Len
+from transactions.check.type.check_type import Check_Type
+from transactions.get_transaction import GetTransaction
+from transactions.my_transactions.get_my_transaction import GetMyTransaction
+from transactions.my_transactions.save_my_transaction import SaveMyTransaction
+from transactions.my_transactions.save_to_my_transaction import \
+    SavetoMyTransaction
+from transactions.my_transactions.validate_transaction import \
+    ValidateTransaction
+from transactions.pending_to_validating import PendingtoValidating
+from transactions.process_the_transaction import ProccesstheTransaction
+from transactions.send import send
+from transactions.transaction import Transaction
 
 sys.path.append(os.path.join(os.path.dirname(__file__), "..", "..", "src"))
 
