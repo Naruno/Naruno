418a48fa5ba2a495506a1bf1d9ce13e69981a8dd
Restyled.io <commits@restyled.io>
2022-07-04 14:13:59 +0000

    Restyled by reorder-python-imports

--- src/node/node.py
+++ src/node/node.py
@@ -13,8 +13,11 @@ import time
 
 from blockchain.block.change_transaction_fee import ChangeTransactionFee
 from blockchain.block.get_block import GetBlock
-from config import (CONNECTED_NODE_PATH, LOADING_BLOCK_PATH,
-                    TEMP_ACCOUNTS_PATH, TEMP_BLOCK_PATH, TEMP_BLOCKSHASH_PATH)
+from config import CONNECTED_NODE_PATH
+from config import LOADING_BLOCK_PATH
+from config import TEMP_ACCOUNTS_PATH
+from config import TEMP_BLOCK_PATH
+from config import TEMP_BLOCKSHASH_PATH
 from lib.log import get_logger
 from lib.merkle_root import MerkleTree
 from node.node import *
