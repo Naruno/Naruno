b7aea88a2c74d72564cf1df9af9c10ed9959abe6
Onur Atakan ULUSOY <atadogan06@gmail.com>
2022-07-08 22:26:09 +0300

    Update test_blockchain.py

--- tests/unit_tests/test_blockchain.py
+++ tests/unit_tests/test_blockchain.py
@@ -7,6 +7,7 @@
 import copy
 import os
 import sys
+sys.path.append(os.path.join(os.path.dirname(__file__), "..", "..", "src"))
 import time
 import unittest
 
@@ -28,7 +29,6 @@ from node.node_connection import Node_Connection
 from node.unl import Unl
 from transactions.transaction import Transaction
 
-sys.path.append(os.path.join(os.path.dirname(__file__), "..", "..", "src"))
 
 
 class Test_Blockchain(unittest.TestCase):
