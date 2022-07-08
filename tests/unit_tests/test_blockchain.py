b185827bbd997f4d903fc56eea42f037af7d3504
Restyled.io <commits@restyled.io>
2022-07-08 17:09:57 +0000

    Restyled by autopep8

--- tests/unit_tests/test_blockchain.py
+++ tests/unit_tests/test_blockchain.py
@@ -4,26 +4,24 @@
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at https://mozilla.org/MPL/2.0/.
+from blockchain.block.blocks_hash import GetBlockshash, GetBlockshash_part, SaveBlockshash, SaveBlockshash_part
+from transactions.transaction import Transaction
+from node.unl import Unl
+from node.node_connection import Node_Connection
+from blockchain.block.save_block import SaveBlock
+from blockchain.block.hash.tx_hash import TransactionsHash
+from blockchain.block.hash.calculate_hash import CalculateHash
+from blockchain.block.hash.blocks_hash import BlocksHash
+from blockchain.block.hash.accounts_hash import AccountsHash
+from blockchain.block.get_block import GetBlock
+from blockchain.block.block_main import Block
+from accounts.get_accounts import GetAccounts
+from accounts.account import Account
+import unittest
+import time
 import os
 import sys
 sys.path.append(os.path.join(os.path.dirname(__file__), "..", "..", "src"))
-import time
-import unittest
-
-from accounts.account import Account
-from accounts.get_accounts import GetAccounts
-from blockchain.block.block_main import Block
-from blockchain.block.blocks_hash import GetBlockshash, GetBlockshash_part, SaveBlockshash, SaveBlockshash_part 
-from blockchain.block.get_block import GetBlock
-from blockchain.block.hash.accounts_hash import AccountsHash
-from blockchain.block.hash.blocks_hash import BlocksHash
-from blockchain.block.hash.calculate_hash import CalculateHash
-from blockchain.block.hash.tx_hash import TransactionsHash
-from blockchain.block.save_block import SaveBlock
-from node.node_connection import Node_Connection
-from node.unl import Unl
-from transactions.transaction import Transaction
-
 
 
 class Test_Blockchain(unittest.TestCase):
@@ -317,27 +315,33 @@ class Test_Blockchain(unittest.TestCase):
 
         custom_TEMP_BLOCKSHASH_PATH = "db/test_SaveBlockshash_TEMP_BLOCKSHASH_PATH"
         the_list = ["onur"]
-        SaveBlockshash(the_list, custom_TEMP_BLOCKSHASH_PATH=custom_TEMP_BLOCKSHASH_PATH)
-        the_list_2 = GetBlockshash(custom_TEMP_BLOCKSHASH_PATH=custom_TEMP_BLOCKSHASH_PATH)
+        SaveBlockshash(
+            the_list, custom_TEMP_BLOCKSHASH_PATH=custom_TEMP_BLOCKSHASH_PATH)
+        the_list_2 = GetBlockshash(
+            custom_TEMP_BLOCKSHASH_PATH=custom_TEMP_BLOCKSHASH_PATH)
         self.assertEqual(the_list, the_list_2)
 
     def test_SaveBlockshash_part(self):
 
         custom_TEMP_BLOCKSHASH_PART_PATH = "db/test_SaveBlockshash_part_TEMP_BLOCKSHASH_PART_PATH"
         the_list = ["onur"]
-        SaveBlockshash_part(the_list, custom_TEMP_BLOCKSHASH_PART_PATH=custom_TEMP_BLOCKSHASH_PART_PATH)
-        the_list_2 = GetBlockshash_part(custom_TEMP_BLOCKSHASH_PART_PATH=custom_TEMP_BLOCKSHASH_PART_PATH)
+        SaveBlockshash_part(
+            the_list, custom_TEMP_BLOCKSHASH_PART_PATH=custom_TEMP_BLOCKSHASH_PART_PATH)
+        the_list_2 = GetBlockshash_part(
+            custom_TEMP_BLOCKSHASH_PART_PATH=custom_TEMP_BLOCKSHASH_PART_PATH)
         self.assertEqual(the_list, the_list_2)
-
 
     def test_GetBlockshash_non(self):
         custom_TEMP_BLOCKSHASH_PATH = f"db/test_GetBlockshash_non_TEMP_BLOCKSHASH_PATH"
-        the_list = GetBlockshash(custom_TEMP_BLOCKSHASH_PATH=custom_TEMP_BLOCKSHASH_PATH)
+        the_list = GetBlockshash(
+            custom_TEMP_BLOCKSHASH_PATH=custom_TEMP_BLOCKSHASH_PATH)
         self.assertEqual(the_list, [])
 
     def test_GetBlockshash_part_non(self):
         custom_TEMP_BLOCKSHASH_PART_PATH = f"db/test_GetBlockshash_part_non_TEMP_BLOCKSHASH_PART_PATH"
-        the_list = GetBlockshash_part(custom_TEMP_BLOCKSHASH_PART_PATH=custom_TEMP_BLOCKSHASH_PART_PATH)
+        the_list = GetBlockshash_part(
+            custom_TEMP_BLOCKSHASH_PART_PATH=custom_TEMP_BLOCKSHASH_PART_PATH)
         self.assertEqual(the_list, [])
+
 
 unittest.main(exit=False)
