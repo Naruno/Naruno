f2feeed71e92c28e2eec3e95dd95a5c573300d0c
Onur Atakan ULUSOY <atadogan06@gmail.com>
2022-07-08 16:49:42 +0300

    tests: Added unit tests for GetSequanceNumber function (#717)
    * tests: Added unit tests for GetSequanceNumber function
    
    * Restyle tests: Added unit tests for GetSequanceNumber function (#718)
    
    * Restyled by autopep8
    
    * Restyled by black
    
    * Restyled by isort
    
    * Restyled by reorder-python-imports
    
    * Restyled by yapf
    
    * Update test_accounts.py
    
    Co-authored-by: Restyled.io <commits@restyled.io>
    Co-authored-by: Onur Atakan ULUSOY <atadogan06@gmail.com>
    
    Co-authored-by: restyled-io[bot] <32688539+restyled-io[bot]@users.noreply.github.com>
    Co-authored-by: Restyled.io <commits@restyled.io>

--- src/accounts/get_sequance_number.py
+++ src/accounts/get_sequance_number.py
@@ -4,17 +4,15 @@
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at https://mozilla.org/MPL/2.0/.
-
-
+from accounts.get_accounts import GetAccounts
 from wallet.wallet_import import Address
 
-from accounts.get_accounts import GetAccounts
 
-
-def GetSequanceNumber(user):
+def GetSequanceNumber(user, account_list=None):
     user = Address(user)
     sequance_number = 0
-    for Accounts in GetAccounts():
+    the_account_list = GetAccounts() if account_list == None else account_list
+    for Accounts in the_account_list:
 
         if Accounts.Address == user:
 
