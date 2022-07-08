00a53d97c064d10567b2c8e0d8e68f62c34d350b
Restyled.io <commits@restyled.io>
2022-07-08 12:30:38 +0000

    Restyled by black

--- tests/unit_tests/test_accounts.py
+++ tests/unit_tests/test_accounts.py
@@ -10,11 +10,11 @@ from blockchain.block.block_main import Block
 import unittest
 import os
 import sys
+
 sys.path.append(os.path.join(os.path.dirname(__file__), "..", "..", "src"))
 
 
 class Test_Accounts(unittest.TestCase):
-
     def test_dumb_account(self):
 
         new_account = Account("test_account", 1, 1)
@@ -65,12 +65,9 @@ class Test_Accounts(unittest.TestCase):
 
     def test_get_balance_not_list_account(self):
 
-        the_account = Account(
-            "dbd811a12104827240153c8fd2f25a294a851ec8", 10, 1)
-        the_account_2 = Account(
-            "15562b06dc6b1acd6e8c86031e564e0c451c7a73", 15, 1)
-        the_account_3 = Account(
-            "7340ac0cdf3f7b59cba4ec6348ee8e41d0c24ef1", 20, 1)
+        the_account = Account("dbd811a12104827240153c8fd2f25a294a851ec8", 10, 1)
+        the_account_2 = Account("15562b06dc6b1acd6e8c86031e564e0c451c7a73", 15, 1)
+        the_account_3 = Account("7340ac0cdf3f7b59cba4ec6348ee8e41d0c24ef1", 20, 1)
 
         account_list = [the_account, the_account_2, the_account_3]
 
@@ -83,12 +80,9 @@ class Test_Accounts(unittest.TestCase):
 
     def test_GetBalance(self):
 
-        the_account = Account(
-            "dbd811a12104827240153c8fd2f25a294a851ec8", 10, 1)
-        the_account_2 = Account(
-            "15562b06dc6b1acd6e8c86031e564e0c451c7a73", 15, 1)
-        the_account_3 = Account(
-            "7340ac0cdf3f7b59cba4ec6348ee8e41d0c24ef1", 20, 1)
+        the_account = Account("dbd811a12104827240153c8fd2f25a294a851ec8", 10, 1)
+        the_account_2 = Account("15562b06dc6b1acd6e8c86031e564e0c451c7a73", 15, 1)
+        the_account_3 = Account("7340ac0cdf3f7b59cba4ec6348ee8e41d0c24ef1", 20, 1)
 
         account_list = [the_account, the_account_2, the_account_3]
 
@@ -97,11 +91,9 @@ class Test_Accounts(unittest.TestCase):
 
         result = GetBalance(block, "test_account", account_list=account_list)
         self.assertEqual(result, 5)
-        result_2 = GetBalance(block, "test_account_2",
-                              account_list=account_list)
+        result_2 = GetBalance(block, "test_account_2", account_list=account_list)
         self.assertEqual(result_2, 10)
-        result_3 = GetBalance(block, "test_account_3",
-                              account_list=account_list)
+        result_3 = GetBalance(block, "test_account_3", account_list=account_list)
         self.assertEqual(result_3, 15)
 
 
