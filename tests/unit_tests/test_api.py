ce84f32e5f45f561c22756ff1803c580d9068ca1
Onur Atakan ULUSOY <atadogan06@gmail.com>
2022-07-09 16:12:19 +0300

    Restore test_api.py

--- tests/unit_tests/test_api.py
+++ tests/unit_tests/test_api.py
@@ -6,11 +6,11 @@
 # file, You can obtain one at https://mozilla.org/MPL/2.0/.
 import os
 import sys
+sys.path.append(os.path.join(os.path.dirname(__file__), "..", "..", "src"))
 import unittest
 
 from api import app
 
-sys.path.append(os.path.join(os.path.dirname(__file__), "..", "..", "src"))
 
 
 class Test_API(unittest.TestCase):
