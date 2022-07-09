91e5150320955c775c6610bf965860dd2c6c8925
Onur Atakan ULUSOY <atadogan06@gmail.com>
2022-07-09 13:05:28 +0000

    tests: Added unit tests for app class and AppsTrigger function

---
+++ tests/unit_tests/test_apps.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at https://mozilla.org/MPL/2.0/.
+import os
+import sys
+import time
+
+from numpy import block
+sys.path.append(os.path.join(os.path.dirname(__file__), "..", "..", "src"))
+import unittest
+
+from blockchain.block.block_main import Block
+from transactions.transaction import Transaction
+from apps.apps_trigger import AppsTrigger
+
+from lib.config_system import get_config
+
+class Test_apps(unittest.TestCase):
+
+    def test_AppsTrigger_App(self):
+        block = Block("onur")
+        the_transaction= Transaction(1,1,1,1,1,1,1,1)
+        the_transaction.transaction_time = time.time()
+        block.validating_list.append(the_transaction)
+        AppsTrigger(block)
+        time.sleep(2)
+        os.chdir(get_config()["main_folder"])
+        self.assertTrue(os.path.isfile(f"apps/testing_app/{block.validating_list[0].transaction_time}.tx"))
+
+
+
+unittest.main(exit=False)
