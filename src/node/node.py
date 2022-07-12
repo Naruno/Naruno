a6ee914e458f68c951e0ccb2f947ae97457d13ea
Restyled.io <commits@restyled.io>
2022-07-12 00:28:40 +0000

    Restyled by isort

--- src/node/node.py
+++ src/node/node.py
@@ -14,15 +14,12 @@ import time
 from blockchain.block.change_transaction_fee import ChangeTransactionFee
 from blockchain.block.get_block import GetBlock
 from blockchain.block.save_block import SaveBlock
-from config import CONNECTED_NODE_PATH
-from config import LOADING_BLOCK_PATH
-from config import TEMP_ACCOUNTS_PATH
-from config import TEMP_BLOCK_PATH
-from config import TEMP_BLOCKSHASH_PATH
+from config import (CONNECTED_NODE_PATH, LOADING_BLOCK_PATH,
+                    TEMP_ACCOUNTS_PATH, TEMP_BLOCK_PATH, TEMP_BLOCKSHASH_PATH)
 from lib.log import get_logger
 from lib.merkle_root import MerkleTree
-from node.node import *
 from node.connection import Connection
+from node.node import *
 from node.unl import Unl
 from transactions.check.check_transaction import CheckTransaction
 from transactions.get_transaction import GetTransaction
