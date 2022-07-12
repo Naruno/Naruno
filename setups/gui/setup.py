c958228d5153c5c57e0ca03d990d3a9887ed9adc
Onur Atakan ULUSOY <atadogan06@gmail.com>
2022-07-12 23:31:27 +0300

    Restore setups/gui/setup.py

---
+++ setups/gui/setup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at https://mozilla.org/MPL/2.0/.
+from setuptools import setup
+
+setup(
+    name="decentra_network_gui",
+    version="0.25.0",
+    description="""This is GUI mode installer for Decentra Network Core""",
+    url="https://decentra-network.github.io/Decentra-Network/",
+    author="Decentra Network Developers",
+    author_email="atadogan06@gmail.com",
+    license="MPL-2.0",
+    install_requires="""
+Kivy==2.0.0
+kivymd==0.104.1
+kivymd_extensions.sweetalert==0.1.5
+""",
+    python_requires=">=3.8",
+    zip_safe=False,
+)
