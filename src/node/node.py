4b9dde6cb067869df2fe3f768c53b8840d9db93f
Restyled.io <commits@restyled.io>
2022-07-12 00:28:39 +0000

    Restyled by black

--- src/node/node.py
+++ src/node/node.py
@@ -123,8 +123,7 @@ class Node(threading.Thread):
 
         for n in self.nodes:
             if n in exclude:
-                logger.info(
-                    "Node System: Node send_data_to_nodes: Node is excluded")
+                logger.info("Node System: Node send_data_to_nodes: Node is excluded")
             else:
                 self.send_data_to_node(n, data)
 
@@ -140,8 +139,7 @@ class Node(threading.Thread):
                     "Node System: Node send_data_to_node: Could not send data to node"
                 )
         else:
-            logger.warning(
-                "Node System: Node send_data_to_node: Node is not connected")
+            logger.warning("Node System: Node send_data_to_node: Node is not connected")
 
     def connect_to_node(self, host, port, save_messages=False):
 
@@ -168,8 +166,7 @@ class Node(threading.Thread):
 
         if Unl.node_is_unl(connected_node_id):
             thread_client = Connection(
-                self, sock, connected_node_id, host, port,
-                save_messages=save_messages
+                self, sock, connected_node_id, host, port, save_messages=save_messages
             )
             thread_client.start()
 
@@ -190,8 +187,7 @@ class Node(threading.Thread):
             del self.nodes[self.nodes.index(node)]
 
         else:
-            logger.info(
-                "Node System: Node disconnect_to_node: Node is not connected")
+            logger.info("Node System: Node disconnect_to_node: Node is not connected")
 
     def stop(self):
         self.terminate_flag.set()
@@ -223,8 +219,7 @@ class Node(threading.Thread):
         node_list = Node.get_connected_node()
 
         already_in_list = any(
-            (node_list[element]["host"] ==
-             host and node_list[element]["port"] == port)
+            (node_list[element]["host"] == host and node_list[element]["port"] == port)
             for element in node_list
         )
 
@@ -361,8 +356,7 @@ class Node(threading.Thread):
             "transaction": new_list,
             "sequance_number": system.sequance_number,
             "signature": Ecdsa.sign(
-                (f"myblock{Merkle_signature_list}" +
-                 str(system.sequance_number)),
+                (f"myblock{Merkle_signature_list}" + str(system.sequance_number)),
                 PrivateKey.fromPem(wallet_import(0, 1)),
             ).toBase64(),
         }
@@ -395,15 +389,13 @@ class Node(threading.Thread):
             or GetBlock().sequance_number != data["sequance_number"]
         ):
             return
-        signature_list = [element["signature"]
-                          for element in data["transaction"]]
+        signature_list = [element["signature"] for element in data["transaction"]]
         merkle_root_of_signature_list = (
             MerkleTree(signature_list).getRootHash() if signature_list else "0"
         )
 
         if Ecdsa.verify(
-            (f"myblock{merkle_root_of_signature_list}" +
-             str(data["sequance_number"])),
+            (f"myblock{merkle_root_of_signature_list}" + str(data["sequance_number"])),
             Signature.fromBase64(data["signature"]),
             PublicKey.fromPem(node.id),
         ):
@@ -441,8 +433,7 @@ class Node(threading.Thread):
                 "fullblock": 1,
                 "byte": (SendData.decode(encoding="iso-8859-1")),
                 "signature": Ecdsa.sign(
-                    "fullblock" +
-                    str((SendData.decode(encoding="iso-8859-1"))),
+                    "fullblock" + str((SendData.decode(encoding="iso-8859-1"))),
                     PrivateKey.fromPem(wallet_import(0, 1)),
                 ).toBase64(),
             }
@@ -458,8 +449,7 @@ class Node(threading.Thread):
                     "fullblock": 1,
                     "byte": "end",
                     "signature": Ecdsa.sign(
-                        "fullblock" +
-                            "end", PrivateKey.fromPem(wallet_import(0, 1))
+                        "fullblock" + "end", PrivateKey.fromPem(wallet_import(0, 1))
                     ).toBase64(),
                 }
                 if node is not None:
@@ -476,8 +466,7 @@ class Node(threading.Thread):
                 "fullaccounts": 1,
                 "byte": (SendData.decode(encoding="iso-8859-1")),
                 "signature": Ecdsa.sign(
-                    "fullaccounts" +
-                    str((SendData.decode(encoding="iso-8859-1"))),
+                    "fullaccounts" + str((SendData.decode(encoding="iso-8859-1"))),
                     PrivateKey.fromPem(wallet_import(0, 1)),
                 ).toBase64(),
             }
@@ -493,8 +482,7 @@ class Node(threading.Thread):
                     "fullaccounts": 1,
                     "byte": "end",
                     "signature": Ecdsa.sign(
-                        "fullaccounts" +
-                            "end", PrivateKey.fromPem(wallet_import(0, 1))
+                        "fullaccounts" + "end", PrivateKey.fromPem(wallet_import(0, 1))
                     ).toBase64(),
                 }
                 if node is not None:
@@ -511,8 +499,7 @@ class Node(threading.Thread):
                 "fullblockshash": 1,
                 "byte": (SendData.decode(encoding="iso-8859-1")),
                 "signature": Ecdsa.sign(
-                    "fullblockshash" +
-                    str((SendData.decode(encoding="iso-8859-1"))),
+                    "fullblockshash" + str((SendData.decode(encoding="iso-8859-1"))),
                     PrivateKey.fromPem(wallet_import(0, 1)),
                 ).toBase64(),
             }
@@ -563,8 +550,7 @@ class Node(threading.Thread):
                 ChangeTransactionFee(system)
 
                 system.exclude_validators = []
-                perpetualTimer(system.consensus_timer,
-                               consensus_trigger).start()
+                perpetualTimer(system.consensus_timer, consensus_trigger).start()
                 SaveBlock(system)
 
             else:
