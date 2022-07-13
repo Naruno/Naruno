e876b63edf5d783c8eaf5968cbf702d4eebe94f0
Onur Atakan ULUSOY <atadogan06@gmail.com>
2022-07-13 15:16:44 +0300

    tests: Merged all node unit tests that start and close Node and Connection in one test (#812)

--- tests/unit_tests/test_node.py
+++ tests/unit_tests/test_node.py
@@ -23,23 +23,34 @@ from decentra_network.wallet.wallet_delete import wallet_delete
 
 class Test_Node(unittest.TestCase):
 
+    def setUp(self):
+        self.node_1 = Node("127.0.0.1", 10001)
+        self.node_2 = Node("127.0.0.1", 10002)
+
+    def tearDown(self):
+        self.node_2.stop()
+        self.node_2.join()
+        self.node_1.stop()
+        self.node_1.join()
+
+    def reset_node_connections(self):
+        for node_1_node in self.node_1.nodes:
+            self.node_1.disconnect_to_node(node_1_node)
+        self.node_1.delete_closed_connections()
+        for node_2_node in self.node_2.nodes:
+            self.node_2.disconnect_to_node(node_2_node)
+        self.node_2.delete_closed_connections()  
+        time.sleep(2)    
+
     def test_node_by_connection_saving_and_unl_nodes_system(self):
 
-        password = "123"
+        Node.id = "id"
+        Unl.save_new_unl_node(Node.id )
+        Node.id = "id2"
+        Unl.save_new_unl_node(Node.id )
 
-        temp_private_key = wallet_create(password)
-
-        node_1 = Node("127.0.0.1", 10001)
-
-        temp_private_key2 = wallet_create(password)
-
-        node_2 = Node("127.0.0.1", 10002)
-
-        Unl.save_new_unl_node(node_1.id)
-        Unl.save_new_unl_node(node_2.id)
-
-        connection = node_2.connect_to_node("127.0.0.1", 10001)
-
+        self.node_2.connect_to_node("127.0.0.1", 10001)
+        time.sleep(2)
         connection_closing_deleting = True
         finded_node = False
         in_unl_list = False
@@ -48,41 +59,30 @@ class Test_Node(unittest.TestCase):
         nodes_list = Node.get_connected_node()
 
         for element in nodes_list:
-            if element == node_1.id or element == node_2.id:
+            if element == self.node_1.id or element == self.node_2.id:
                 finded_node = True
 
                 temp_unl_node_list = Unl.get_unl_nodes()
                 temp_get_as_node_type = Unl.get_as_node_type(
                     temp_unl_node_list)
                 for unl_element in temp_unl_node_list:
-                    if unl_element == node_1.id or unl_element == node_2.id:
+                    if unl_element == self.node_1.id or unl_element == self.node_2.id:
                         for node_element_of_unl in temp_get_as_node_type:
-                            if (node_1.host == node_element_of_unl.host
-                                    or node_2 == node_element_of_unl.host):
-                                if (node_1.port == node_element_of_unl.port
-                                        or node_2 == node_element_of_unl.port):
+                            if (self.node_1.host == node_element_of_unl.host
+                                    or self.node_2 == node_element_of_unl.host):
+                                if (self.node_1.port == node_element_of_unl.port
+                                        or self.node_2 == node_element_of_unl.port):
                                     get_as_node = True
                         in_unl_list = True
                         Unl.unl_node_delete(unl_element)
                 Node.connected_node_delete(element)
 
 
-        
 
-        saved_wallets = get_saved_wallet()
 
-        for each_wallet in saved_wallets:
-            if (temp_private_key == saved_wallets[each_wallet]["privatekey"]
-                    or temp_private_key2
-                    == saved_wallets[each_wallet]["privatekey"]):
-                wallet_delete(each_wallet)
 
-        node_2.stop()
-        node_2.join()
-        node_1.stop()
-        node_1.join()
-
-        connection_closing_deleting = any(element.id == Node.id for element in node_2.nodes)
+        self.reset_node_connections()
+        connection_closing_deleting = any(element.id == Node.id for element in self.node_2.nodes)
 
         self.assertEqual(connection_closing_deleting, False,
                         "Connection closing deleting")
@@ -92,7 +92,7 @@ class Test_Node(unittest.TestCase):
                          "Problem on UNL node saving system.")
         self.assertEqual(get_as_node, True,
                          "Problem on UNL get as node system.")
-        time.sleep(10)
+
 
     def test_GetCandidateBlocks(self):
 
@@ -124,72 +124,44 @@ class Test_Node(unittest.TestCase):
         
     def test_send_data_to_nodes(self):
       
-        password = "123"
 
-        temp_private_key = wallet_create(password)
+        Unl.save_new_unl_node(self.node_1.id)
+        Unl.save_new_unl_node(self.node_2.id)
 
-        node_1 = Node("127.0.0.1", 10001)
-
-        temp_private_key2 = wallet_create(password)
-
-        node_2 = Node("127.0.0.1", 10002)
-
-        Unl.save_new_unl_node(node_1.id)
-        Unl.save_new_unl_node(node_2.id)
-
-        connection = node_2.connect_to_node("127.0.0.1", 10001, save_messages=True)
+        self.node_2.connect_to_node("127.0.0.1", 10001, save_messages=True)
         time.sleep(2)
-        connection_2 = node_1.nodes[0]
+        connection_2 = self.node_1.nodes[0]
         connection_2.save_messages = True
 
-        node_2.send_data_to_nodes(1)
-        node_2.send_data_to_nodes("test")
+        self.node_2.send_data_to_nodes(1)
+        self.node_2.send_data_to_nodes("test")
         time.sleep(2)
-        node_2.send_data_to_nodes({"test": b'b'})
-        node_2.send_data_to_nodes({"test": "test"})
+        self.node_2.send_data_to_nodes({"test": b'b'})
+        self.node_2.send_data_to_nodes({"test": "test"})
         time.sleep(2)
-        node_2.send_data_to_nodes(b"test")
+        self.node_2.send_data_to_nodes(b"test")
 
 
-        saved_wallets = get_saved_wallet()
 
-        for each_wallet in saved_wallets:
-            if (temp_private_key == saved_wallets[each_wallet]["privatekey"]
-                    or temp_private_key2
-                    == saved_wallets[each_wallet]["privatekey"]):
-                wallet_delete(each_wallet)
-
-
-  
-        node_2.stop()
-        node_2.join()
-        node_1.stop()
-        node_1.join()
+        self.reset_node_connections()
         self.assertEqual(connection_2.messages[0], "test")
         self.assertEqual(connection_2.messages[1], {"test": "test"})
         self.assertEqual(connection_2.messages[2], "test")
-        time.sleep(10)
 
     def test_connection_not_unl(self):
 
         default_id = copy.copy(Node.id)
-        Node.id = "id"
-        node_1 = Node("127.0.0.1", 10001)
-        node_2 = Node("127.0.0.1", 10002)
-        connection = node_2.connect_to_node("127.0.0.1", 10001)
+        Node.id = "id3"
+        connection = self.node_2.connect_to_node("127.0.0.1", 10001)
         time.sleep(2)
 
 
 
         Node.id = default_id
-
-        node_2.stop()
-        node_2.join()
-        node_1.stop()
-        node_1.join()
-        self.assertEqual(node_1.nodes, [])
-        self.assertEqual(node_2.nodes, [])
+        self.reset_node_connections()
+        self.assertEqual(self.node_1.nodes, [])
+        self.assertEqual(self.node_2.nodes, [])
         self.assertEqual(connection, None)
-        time.sleep(10)  
+
 
 unittest.main(exit=False)
