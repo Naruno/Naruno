23987ffc6cb609b5785e5cb263b4a95827d594c0
Restyled.io <commits@restyled.io>
2022-07-16 16:24:51 +0000

    Restyled by yapf

--- decentra_network/node/server/server.py
+++ decentra_network/node/server/server.py
@@ -75,31 +75,21 @@ class server(Thread):
         self.messages = []
         self.save_messages = save_messages
 
-        self.TEMP_BLOCK_PATH = (
-            TEMP_BLOCK_PATH
-            if custom_TEMP_BLOCK_PATH is None
-            else custom_TEMP_BLOCK_PATH
-        )
-        self.TEMP_ACCOUNTS_PATH = (
-            TEMP_ACCOUNTS_PATH
-            if custom_TEMP_ACCOUNTS_PATH is None
-            else TEMP_ACCOUNTS_PATH
-        )
-        self.TEMP_BLOCKSHASH_PATH = (
-            TEMP_BLOCKSHASH_PATH
-            if custom_TEMP_BLOCKSHASH_PATH is None
-            else TEMP_BLOCKSHASH_PATH
-        )
-        self.TEMP_BLOCKSHASH_PART_PATH = (
-            TEMP_BLOCKSHASH_PART_PATH
-            if custom_TEMP_BLOCKSHASH_PART_PATH is None
-            else TEMP_BLOCKSHASH_PART_PATH
-        )
-        self.LOADING_BLOCK_PATH = (
-            LOADING_BLOCK_PATH
-            if custom_LOADING_BLOCK_PATH is None
-            else custom_LOADING_BLOCK_PATH
-        )
+        self.TEMP_BLOCK_PATH = (TEMP_BLOCK_PATH
+                                if custom_TEMP_BLOCK_PATH is None else
+                                custom_TEMP_BLOCK_PATH)
+        self.TEMP_ACCOUNTS_PATH = (TEMP_ACCOUNTS_PATH
+                                   if custom_TEMP_ACCOUNTS_PATH is None else
+                                   TEMP_ACCOUNTS_PATH)
+        self.TEMP_BLOCKSHASH_PATH = (TEMP_BLOCKSHASH_PATH
+                                     if custom_TEMP_BLOCKSHASH_PATH is None
+                                     else TEMP_BLOCKSHASH_PATH)
+        self.TEMP_BLOCKSHASH_PART_PATH = (TEMP_BLOCKSHASH_PART_PATH if
+                                          custom_TEMP_BLOCKSHASH_PART_PATH is
+                                          None else TEMP_BLOCKSHASH_PART_PATH)
+        self.LOADING_BLOCK_PATH = (LOADING_BLOCK_PATH
+                                   if custom_LOADING_BLOCK_PATH is None else
+                                   custom_LOADING_BLOCK_PATH)
 
         self.start()
 
@@ -116,12 +106,14 @@ class server(Thread):
                 conn, addr = self.sock.accept()
                 connected = self.check_connected(host=addr[0], port=addr[1])
                 if not connected:
-                    logger.info(f"NODE:{self.host}:{self.port} New connection: {addr}")
+                    logger.info(
+                        f"NODE:{self.host}:{self.port} New connection: {addr}")
                     data = conn.recv(1024)
                     conn.send(server.id.encode("utf-8"))
                     client_id = data.decode("utf-8")
                     if Unl.node_is_unl(client_id):
-                        self.clients.append(client(conn, addr, client_id, self))
+                        self.clients.append(client(conn, addr, client_id,
+                                                   self))
                         server.save_connected_node(addr[0], addr[1], client_id)
                 else:
                     logger.info(
@@ -133,8 +125,7 @@ class server(Thread):
     def stop(self):
         self.running = False
         socket.socket(socket.AF_INET, socket.SOCK_STREAM).connect(
-            (self.host, self.port)
-        )
+            (self.host, self.port))
         for c in self.clients:
             c.stop()
         time.sleep(1)
@@ -176,7 +167,8 @@ class server(Thread):
                 self.messages.append(data)
             self.direct_message(client, data)
         else:
-            logger.info(f"NODE:{self.host}:{self.port} Message not valid: {data}")
+            logger.info(
+                f"NODE:{self.host}:{self.port} Message not valid: {data}")
 
     def check_message(self, data):
         # remove sign from data
@@ -204,7 +196,8 @@ class server(Thread):
                     self.clients.append(client(conn, addr, client_id, self))
                     return True
             except socket.timeout:
-                logger.info(f"NODE:{self.host}:{self.port} Connection timeout: {addr}")
+                logger.info(
+                    f"NODE:{self.host}:{self.port} Connection timeout: {addr}")
                 conn.close()
 
     @staticmethod
@@ -237,7 +230,8 @@ class server(Thread):
         node_list["host"] = host
         node_list["port"] = port
 
-        node_id = sha256((node_id + host + str(port)).encode("utf-8")).hexdigest()
+        node_id = sha256(
+            (node_id + host + str(port)).encode("utf-8")).hexdigest()
         file_name = CONNECTED_NODES_PATH + f"{node_id}.json"
         os.chdir(get_config()["main_folder"])
         with open(file_name, "w") as connected_node_file:
@@ -252,9 +246,8 @@ class server(Thread):
         node_list = server.Server.get_connected_nodes()
 
         for element in node_list:
-            server.Server.connect(
-                node_list[element]["host"], node_list[element]["port"]
-            )
+            server.Server.connect(node_list[element]["host"],
+                                  node_list[element]["port"])
 
     @staticmethod
     def connected_node_delete(node):
@@ -263,9 +256,8 @@ class server(Thread):
         """
         print(node)
         os.chdir(get_config()["main_folder"])
-        node_id = sha256(
-            (node["id"] + node["host"] + str(node["port"])).encode("utf-8")
-        ).hexdigest()
+        node_id = sha256((node["id"] + node["host"] +
+                          str(node["port"])).encode("utf-8")).hexdigest()
         for entry in os.scandir(CONNECTED_NODES_PATH):
             if entry.name == f"{node_id}.json":
                 os.remove(entry.path)
@@ -328,7 +320,8 @@ class server(Thread):
             self.send(data)
 
     def get_candidate_block(self, data, node):
-        logger.info("Getting candidate block: {}".format(data["sequance_number"]))
+        logger.info("Getting candidate block: {}".format(
+            data["sequance_number"]))
         if GetBlock().sequance_number != data["sequance_number"]:
             logger.info("Candidate block sequance number is not correct")
             return False
@@ -342,11 +335,8 @@ class server(Thread):
             node.candidate_block_hash = data
 
     def send_full_chain(self, node=None):
-        log_text = (
-            "Sending full chain"
-            if node is None
-            else f"Sending full chain to {node.id}:{node.host}:{node.port}"
-        )
+        log_text = ("Sending full chain" if node is None else
+                    f"Sending full chain to {node.id}:{node.host}:{node.port}")
         logger.info(log_text)
         file = open(self.TEMP_BLOCK_PATH, "rb")
         SendData = file.read(1024)
@@ -475,7 +465,8 @@ class server(Thread):
                     custom_TEMP_BLOCK_PATH=self.TEMP_BLOCK_PATH,
                     custom_TEMP_ACCOUNTS_PATH=self.TEMP_ACCOUNTS_PATH,
                     custom_TEMP_BLOCKSHASH_PATH=self.TEMP_BLOCKSHASH_PATH,
-                    custom_TEMP_BLOCKSHASH_PART_PATH=self.TEMP_BLOCKSHASH_PART_PATH,
+                    custom_TEMP_BLOCKSHASH_PART_PATH=self.
+                    TEMP_BLOCKSHASH_PART_PATH,
                 )
 
             else:
@@ -516,7 +507,8 @@ class server(Thread):
 
         if get_ok:
             if str(data["byte"]) == "end":
-                os.rename(LOADING_BLOCKSHASH_PART_PATH, TEMP_BLOCKSHASH_PART_PATH)
+                os.rename(LOADING_BLOCKSHASH_PART_PATH,
+                          TEMP_BLOCKSHASH_PART_PATH)
             else:
                 file = open(LOADING_BLOCKSHASH_PART_PATH, "ab")
                 file.write((data["byte"].encode(encoding="iso-8859-1")))
