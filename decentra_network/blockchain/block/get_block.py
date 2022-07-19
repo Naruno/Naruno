e1aa8b6f45941bd839977670c0bb073706dbf024
Onur Atakan ULUSOY <atadogan06@gmail.com>
2022-07-19 23:28:32 +0000

    fix

--- decentra_network/blockchain/block/get_block.py
+++ decentra_network/blockchain/block/get_block.py
@@ -25,7 +25,7 @@ def GetBlock(custom_TEMP_BLOCK_PATH=None):
     os.chdir(get_config()["main_folder"])
     with open(the_TEMP_BLOCK_PATH, "r") as block_file:
         try:
-            the_block_json = json.load(block_file.read())
+            the_block_json = json.load(block_file)
         except json.JSONDecodeError:
-            logger.exception(f"Error while loading block file. {block_file.read()}")
+            logger.exception(f"Error while loading block file. {block_file}")
     return Block.load_json(the_block_json)
