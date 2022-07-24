c59e68d8fbcb2737f33fba7c6415d1782001af6c
Onur Atakan ULUSOY <atadogan06@gmail.com>
2022-07-24 18:43:03 +0300

    tests: Decreased time sleeps for functional and stability-tests (#866)
    * tests: Removed tims.sleep after validating balance because the block already change
    
    * consensus: Changed pending to validating approach
    
    * Restyled by autopep8
    
    * Restyled by black
    
    * Restyled by isort
    
    * Restyled by reorder-python-imports
    
    * Restyled by yapf
    
    * Restore consensus_main.py
    
    * Restore get_transaction.py
    
    * Restore pending_to_validating.py
    
    Co-authored-by: Restyled.io <commits@restyled.io>

--- tests/stability_tests/local/test_decentra_network_local_5_2.py
+++ tests/stability_tests/local/test_decentra_network_local_5_2.py
@@ -7,13 +7,13 @@
 import json
 import os
 import sys
+
 sys.path.append(os.path.join(os.path.dirname(__file__), "..", "..", ".."))
 import time
 import unittest
 import urllib.request
 
 from auto_builders.local import Decentra_Network_Local
-
 
 temp_environment = Decentra_Network_Local(5, 2)
 temp_environment.delete()
@@ -29,23 +29,29 @@ class Test_Decentra_Network_Local(unittest.TestCase):
         Send coin to 2.wallet from 1.wallet
         """
 
+        wallet_2_json = json.loads(
+            urllib.request.urlopen(
+                "http://localhost:8101/wallet/create/123").read().decode())
+        wallet_2_address = (wallet_2_json[0].replace("0) ", "").replace(
+            " - CURRENTLY USED\n", ""))
 
-        wallet_2_json = json.loads(urllib.request.urlopen("http://localhost:8101/wallet/create/123").read().decode())
-        wallet_2_address = wallet_2_json[0].replace("0) ", "").replace(" - CURRENTLY USED\n", "")
-
-        urllib.request.urlopen(f"http://localhost:8000/send/coin/{wallet_2_address}/5000/123")
+        urllib.request.urlopen(
+            f"http://localhost:8000/send/coin/{wallet_2_address}/5000/123")
         time.sleep(25)
-        balance_wallet_1 = json.loads(urllib.request.urlopen("http://localhost:8101/wallet/balance").read().decode())
-        self.assertEqual(balance_wallet_1,4000.0,"A problem in same network one transaction -1.")
+        balance_wallet_1 = json.loads(
+            urllib.request.urlopen(
+                "http://localhost:8101/wallet/balance").read().decode())
+        self.assertEqual(balance_wallet_1, 4000.0,
+                         "A problem in same network one transaction -1.")
 
-
-        time.sleep(45)
-
-
-        urllib.request.urlopen(f"http://localhost:8000/send/coin/{wallet_2_address}/5000/123")
+        urllib.request.urlopen(
+            f"http://localhost:8000/send/coin/{wallet_2_address}/5000/123")
         time.sleep(25)
-        balance_wallet_1 = json.loads(urllib.request.urlopen("http://localhost:8101/wallet/balance").read().decode())
-        self.assertEqual(balance_wallet_1,9000.0,"A problem in same network one transaction -3.")
+        balance_wallet_1 = json.loads(
+            urllib.request.urlopen(
+                "http://localhost:8101/wallet/balance").read().decode())
+        self.assertEqual(balance_wallet_1, 9000.0,
+                         "A problem in same network one transaction -3.")
 
 
 unittest.main(exit=False)
