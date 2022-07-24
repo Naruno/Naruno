0f42840d3d4f4addde27660e137b09466bbcbf78
Onur Atakan ULUSOY <atadogan06@gmail.com>
2022-07-24 18:32:32 +0300

    Restore pending_to_validating.py

--- decentra_network/transactions/pending_to_validating.py
+++ decentra_network/transactions/pending_to_validating.py
@@ -4,10 +4,9 @@
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at https://mozilla.org/MPL/2.0/.
-import time
 
-from decentra_network.transactions.pending.delete_pending import DeletePending
 from decentra_network.transactions.pending.get_pending import GetPending
+from decentra_network.transactions.pending.delete_pending import DeletePending
 
 
 def PendingtoValidating(block):
@@ -16,11 +15,11 @@ def PendingtoValidating(block):
     if there are suitable conditions.
     """
 
-    if len(block.validating_list) < block.max_tx_number:
-        if block.raund_2_starting_time is None and not (
-                time.time() -
-                block.raund_1_starting_time) > (block.raund_1_time / 2):
-            for tx in GetPending():
-                if len(block.validating_list) < block.max_tx_number:
-                    block.validating_list.append(tx)
-                    DeletePending(tx)
+    if (
+        len(block.validating_list) < block.max_tx_number
+        and block.raund_1_starting_time is None
+    ):
+        for tx in GetPending():
+            if len(block.validating_list) < block.max_tx_number:
+                block.validating_list.append(tx)
+                DeletePending(tx)
