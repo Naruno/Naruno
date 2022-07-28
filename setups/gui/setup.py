f7cbeb36d2fcc52345ea3a9243a741f62e028341
Onur Atakan ULUSOY <atadogan06@gmail.com>
2022-07-28 10:06:41 +0000

    Fix

--- setups/gui/setup.py
+++ setups/gui/setup.py
@@ -15,7 +15,7 @@ setup(
     author_email="atadogan06@gmail.com",
     license="MPL-2.0",
     install_requires="""
-Kivy==2.0.0
+Kivy==2.1.0
 kivymd==0.104.2
 kivymd_extensions.sweetalert==0.1.5
 """,
