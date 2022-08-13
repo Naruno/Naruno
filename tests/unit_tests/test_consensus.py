d681855edc8dd4de0be848e9645b84c356e18d38
Onur Atakan ULUSOY <atadogan06@gmail.com>
2022-08-14 02:39:15 +0300

    tests: Added unit test for transactions_main function (#972)
    * tests: Adding unit test for transactions_main function
    
    * Restyled by autopep8
    
    * Restyled by black
    
    * Restyled by isort
    
    * Restyled by yapf
    
    * fix
    
    * Restyled by autopep8
    
    * Restyled by black
    
    * Restyled by isort
    
    * Restyled by yapf
    
    Co-authored-by: Restyled.io <commits@restyled.io>

--- tests/unit_tests/test_consensus.py
+++ tests/unit_tests/test_consensus.py
@@ -8,12 +8,25 @@ import os
 import sys
 
 sys.path.append(os.path.join(os.path.dirname(__file__), "..", ".."))
+import copy
 import time
 import unittest
 
 from decentra_network.blockchain.block.block_main import Block
+from decentra_network.consensus.finished.transactions.transactions_main import \
+    transactions_main
 from decentra_network.consensus.time.true_time.true_time_main import true_time
 from decentra_network.lib.clean_up import CleanUp_tests
+from decentra_network.transactions.my_transactions.get_my_transaction import \
+    GetMyTransaction
+from decentra_network.transactions.my_transactions.save_my_transaction import \
+    SaveMyTransaction
+from decentra_network.transactions.my_transactions.save_to_my_transaction import \
+    SavetoMyTransaction
+from decentra_network.transactions.my_transactions.validate_transaction import \
+    ValidateTransaction
+from decentra_network.transactions.transaction import Transaction
+from decentra_network.wallet.ellipticcurve.wallet_import import wallet_import
 
 
 class Test_Consensus(unittest.TestCase):
@@ -44,6 +57,50 @@ class Test_Consensus(unittest.TestCase):
         block.empty_block_number = 0
         time.sleep(1)
         self.assertTrue(true_time(block=block))
+
+    def test_transactions_main(self):
+        backup = GetMyTransaction()
+        block = Block("Onur")
+        the_transaction_json = {
+            "sequance_number": 1,
+            "signature":
+            "MEUCIHABt7ypkpvFlpqL4SuogwVuzMu2gGynVkrSw6ohZ/GyAiEAg2O3iOei1Ft/vQRpboX7Sm1OOey8a3a67wPJaH/FmVE=",
+            "fromUser":
+            "MFYwEAYHKoZIzj0CAQYFK4EEAAoDQgAE0AYA7B+neqfUA17wKh3OxC67K8UlIskMm9T2qAR+pl+kKX1SleqqvLPM5bGykZ8tqq4RGtAcGtrtvEBrB9DTPg==",
+            "toUser": "onur",
+            "data": "blockchain-lab",
+            "amount": 5000.0,
+            "transaction_fee": 0.02,
+            "transaction_time": 1656764224,
+        }
+        the_transaction = Transaction.load_json(the_transaction_json)
+        the_transaction.fromUser = wallet_import(-1, 0)
+        block.validating_list.append(the_transaction)
+
+        the_transaction_2 = copy.copy(the_transaction)
+        the_transaction_2.signature = "ulusoy"
+        the_transaction_2.fromUser = "onuratakan"
+        the_transaction_2.toUser = wallet_import(-1, 3)
+
+        block.validating_list.append(the_transaction_2)
+
+        SavetoMyTransaction(the_transaction)
+
+        transactions_main(block=block)
+
+        result = GetMyTransaction()
+
+        for each_result in result:
+            result[result.index(each_result)][0] = result[result.index(
+                each_result)][0].dump_json()
+        SaveMyTransaction(backup)
+        self.assertEqual(
+            result,
+            [
+                [the_transaction.dump_json(), True],
+                [the_transaction_2.dump_json(), True],
+            ],
+        )
 
 
 unittest.main(exit=False)
