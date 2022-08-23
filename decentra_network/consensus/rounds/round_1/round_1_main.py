da26277b6c88a0a24c7469852afbe9b071cf4ba4
Restyled.io <commits@restyled.io>
2022-08-23 16:01:24 +0000

    Restyled by isort

--- decentra_network/consensus/rounds/round_1/round_1_main.py
+++ decentra_network/consensus/rounds/round_1/round_1_main.py
@@ -5,16 +5,16 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at https://mozilla.org/MPL/2.0/.
 from decentra_network.blockchain.block.block_main import Block
-from decentra_network.consensus.rounds.round_1.checks.checks_main import round_check
-from decentra_network.consensus.rounds.round_1.process.process_main import round_process
+from decentra_network.blockchain.candidate_block.candidate_block_main import \
+    candidate_block
+from decentra_network.consensus.rounds.round_1.checks.checks_main import \
+    round_check
+from decentra_network.consensus.rounds.round_1.process.process_main import \
+    round_process
 from decentra_network.lib.log import get_logger
 from decentra_network.node.get_candidate_blocks import GetCandidateBlocks
 from decentra_network.node.server.server import server
 from decentra_network.node.unl import Unl
-
-from decentra_network.blockchain.candidate_block.candidate_block_main import (
-    candidate_block,
-)
 
 logger = get_logger("CONSENSUS_FIRST_ROUND")
 
