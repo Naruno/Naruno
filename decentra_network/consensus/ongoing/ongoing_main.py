050c2b5509519e8de8d1e80cabc072682ba3d990
Onur Atakan ULUSOY <atadogan06@gmail.com>
2022-08-25 23:17:24 +0300

    tests: Added unit test for ongoing_main function (#1001)
    * tests: Added unit test for ongoing_main function
    
    * Restyled by autopep8
    
    * Restyled by black
    
    * Restyled by isort
    
    * Restyled by yapf
    
    Co-authored-by: Restyled.io <commits@restyled.io>

--- decentra_network/consensus/ongoing/ongoing_main.py
+++ decentra_network/consensus/ongoing/ongoing_main.py
@@ -5,22 +5,58 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at https://mozilla.org/MPL/2.0/.
 from decentra_network.blockchain.block.block_main import Block
+from decentra_network.blockchain.candidate_block.candidate_block_main import \
+    candidate_block
 from decentra_network.consensus.rounds.round_1.round_1_main import \
     consensus_round_1
 from decentra_network.consensus.rounds.round_2.round_2_main import \
     consensus_round_2
 from decentra_network.lib.log import get_logger
+from decentra_network.node.client.client import client
+from decentra_network.node.server.server import server
 
 logger = get_logger("CONSENSUS")
 
 
-def ongoing_main(block: Block) -> None:
+def ongoing_main(
+    block: Block,
+    custom_candidate_class: candidate_block = None,
+    custom_unl_nodes: dict = None,
+    custom_UNL_NODES_PATH: str = None,
+    custom_server: server = None,
+    custom_unl: client = None,
+    custom_TEMP_ACCOUNTS_PATH: str = None,
+    custom_TEMP_BLOCK_PATH: str = None,
+    custom_TEMP_BLOCKSHASH_PATH: str = None,
+    custom_TEMP_BLOCKSHASH_PART_PATH: str = None,
+) -> Block:
 
     if not block.round_1:
         logger.info("First round is starting")
-        consensus_round_1(block)
+        consensus_round_1(
+            block,
+            custom_candidate_class=custom_candidate_class,
+            custom_unl_nodes=custom_unl_nodes,
+            custom_UNL_NODES_PATH=custom_UNL_NODES_PATH,
+            custom_server=custom_server,
+            custom_TEMP_ACCOUNTS_PATH=custom_TEMP_ACCOUNTS_PATH,
+            custom_TEMP_BLOCK_PATH=custom_TEMP_BLOCK_PATH,
+            custom_TEMP_BLOCKSHASH_PATH=custom_TEMP_BLOCKSHASH_PATH,
+            custom_TEMP_BLOCKSHASH_PART_PATH=custom_TEMP_BLOCKSHASH_PART_PATH,
+        )
         logger.info("First round is done")
     elif not block.round_2:
         logger.info("Second round is starting")
-        consensus_round_2(block)
+        consensus_round_2(
+            block,
+            candidate_class=custom_candidate_class,
+            unl_nodes=custom_unl_nodes,
+            custom_server=custom_server,
+            custom_unl=custom_unl,
+            custom_TEMP_BLOCK_PATH=custom_TEMP_BLOCK_PATH,
+            custom_TEMP_ACCOUNTS_PATH=custom_TEMP_ACCOUNTS_PATH,
+            custom_TEMP_BLOCKSHASH_PATH=custom_TEMP_BLOCKSHASH_PATH,
+            custom_TEMP_BLOCKSHASH_PART_PATH=custom_TEMP_BLOCKSHASH_PART_PATH,
+        )
         logger.info("Second round is done")
+    return block
