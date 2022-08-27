959a872f3e61da372f712a912cd1f7f91a1bf56e
Restyled.io <commits@restyled.io>
2022-08-27 20:30:05 +0000

    Restyled by black

--- decentra_network/api/main.py
+++ decentra_network/api/main.py
@@ -13,6 +13,7 @@ from flask import jsonify
 from flask import request
 from waitress import serve
 from waitress.server import create_server
+
 sys.path.append(os.path.join(os.path.dirname(__file__), "..", ".."))
 
 from decentra_network.accounts.get_balance import GetBalance
@@ -24,16 +25,20 @@ from decentra_network.lib.export import export_the_transactions
 from decentra_network.lib.log import get_logger
 from decentra_network.lib.perpetualtimer import perpetualTimer
 from decentra_network.lib.safety import safety_check
-from decentra_network.lib.settings_system import (d_mode_settings,
-                                                  t_mode_settings,
-                                                  the_settings)
+from decentra_network.lib.settings_system import (
+    d_mode_settings,
+    t_mode_settings,
+    the_settings,
+)
 from decentra_network.lib.status import Status
 from decentra_network.node.server.server import server
 from decentra_network.node.unl import Unl
-from decentra_network.transactions.my_transactions.get_my_transaction import \
-    GetMyTransaction
-from decentra_network.transactions.my_transactions.save_to_my_transaction import \
-    SavetoMyTransaction
+from decentra_network.transactions.my_transactions.get_my_transaction import (
+    GetMyTransaction,
+)
+from decentra_network.transactions.my_transactions.save_to_my_transaction import (
+    SavetoMyTransaction,
+)
 from decentra_network.transactions.send import send
 from decentra_network.wallet.delete_current_wallet import delete_current_wallet
 from decentra_network.wallet.ellipticcurve.wallet_create import wallet_create
@@ -52,49 +57,46 @@ custom_balance = None
 custom_server = None
 
 custom_TEMP_BLOCK_PATH = None
-custom_TEMP_ACCOUNTS_PATH = None,
-custom_TEMP_BLOCKSHASH_PATH = None,
+custom_TEMP_ACCOUNTS_PATH = (None,)
+custom_TEMP_BLOCKSHASH_PATH = (None,)
 custom_TEMP_BLOCKSHASH_PART_PATH = None
 
 
 @app.route("/wallet/print", methods=["GET"])
 def print_wallets_page():
-    logger.info(
-        f"{request.remote_addr} {request.method} {request.url} {request.data}")
+    logger.info(f"{request.remote_addr} {request.method} {request.url} {request.data}")
     return jsonify(print_wallets())
 
 
 @app.route("/wallet/change/<number>", methods=["GET"])
 def wallet_change_page(number):
-    logger.info(
-        f"{request.remote_addr} {request.method} {request.url} {request.data}")
+    logger.info(f"{request.remote_addr} {request.method} {request.url} {request.data}")
     wallet_selector(number)
     return jsonify(print_wallets())
 
 
 @app.route("/wallet/create/<password>", methods=["GET"])
 def create_wallet_page(password):
-    logger.info(
-        f"{request.remote_addr} {request.method} {request.url} {request.data}")
+    logger.info(f"{request.remote_addr} {request.method} {request.url} {request.data}")
     wallet_create(password)
     return jsonify(print_wallets())
 
 
 @app.route("/wallet/delete", methods=["GET"])
 def delete_wallets_page():
-    logger.info(
-        f"{request.remote_addr} {request.method} {request.url} {request.data}")
+    logger.info(f"{request.remote_addr} {request.method} {request.url} {request.data}")
     delete_current_wallet()
     return jsonify(print_wallets())
 
 
 @app.route("/send/coin/<address>/<amount>/<password>", methods=["GET"])
 def send_coin_page(address, amount, password):
-    logger.info(
-        f"{request.remote_addr} {request.method} {request.url} {request.data}")
-    block = GetBlock(
-        custom_TEMP_BLOCK_PATH=custom_TEMP_BLOCK_PATH
-    ) if custom_block is None else custom_block
+    logger.info(f"{request.remote_addr} {request.method} {request.url} {request.data}")
+    block = (
+        GetBlock(custom_TEMP_BLOCK_PATH=custom_TEMP_BLOCK_PATH)
+        if custom_block is None
+        else custom_block
+    )
     send_tx = send(
         block,
         password,
@@ -102,7 +104,7 @@ def send_coin_page(address, amount, password):
         amount,
         custom_current_time=custom_current_time,
         custom_sequence_number=custom_sequence_number,
-        custom_balance=custom_balance
+        custom_balance=custom_balance,
     )
     if send_tx != False:
         SavetoMyTransaction(send_tx)
@@ -111,27 +113,27 @@ def send_coin_page(address, amount, password):
             custom_current_time=custom_current_time,
             custom_sequence_number=custom_sequence_number,
             custom_balance=custom_balance,
-            custom_server=custom_server
+            custom_server=custom_server,
         )
         SaveBlock(
             block,
             custom_TEMP_BLOCK_PATH=custom_TEMP_BLOCK_PATH,
             custom_TEMP_ACCOUNTS_PATH=custom_TEMP_ACCOUNTS_PATH,
             custom_TEMP_BLOCKSHASH_PATH=custom_TEMP_BLOCKSHASH_PATH,
-            custom_TEMP_BLOCKSHASH_PART_PATH=custom_TEMP_BLOCKSHASH_PART_PATH
+            custom_TEMP_BLOCKSHASH_PART_PATH=custom_TEMP_BLOCKSHASH_PART_PATH,
         )
     result = send_tx.dump_json() if send_tx != False else False
     return jsonify(result)
 
 
-@app.route("/send/coin-data/<address>/<amount>/<data>/<password>",
-           methods=["GET"])
+@app.route("/send/coin-data/<address>/<amount>/<data>/<password>", methods=["GET"])
 def send_coin_data_page(address, amount, data, password):
-    logger.info(
-        f"{request.remote_addr} {request.method} {request.url} {request.data}")
-    block = GetBlock(
-        custom_TEMP_BLOCK_PATH=custom_TEMP_BLOCK_PATH
-    ) if custom_block is None else custom_block
+    logger.info(f"{request.remote_addr} {request.method} {request.url} {request.data}")
+    block = (
+        GetBlock(custom_TEMP_BLOCK_PATH=custom_TEMP_BLOCK_PATH)
+        if custom_block is None
+        else custom_block
+    )
     send_tx = send(
         block,
         password,
@@ -140,7 +142,7 @@ def send_coin_data_page(address, amount, data, password):
         data=data,
         custom_current_time=custom_current_time,
         custom_sequence_number=custom_sequence_number,
-        custom_balance=custom_balance
+        custom_balance=custom_balance,
     )
     if send_tx != False:
         SavetoMyTransaction(send_tx)
@@ -149,14 +151,14 @@ def send_coin_data_page(address, amount, data, password):
             custom_current_time=custom_current_time,
             custom_sequence_number=custom_sequence_number,
             custom_balance=custom_balance,
-            custom_server=custom_server
+            custom_server=custom_server,
         )
         SaveBlock(
             block,
             custom_TEMP_BLOCK_PATH=custom_TEMP_BLOCK_PATH,
             custom_TEMP_ACCOUNTS_PATH=custom_TEMP_ACCOUNTS_PATH,
             custom_TEMP_BLOCKSHASH_PATH=custom_TEMP_BLOCKSHASH_PATH,
-            custom_TEMP_BLOCKSHASH_PART_PATH=custom_TEMP_BLOCKSHASH_PART_PATH
+            custom_TEMP_BLOCKSHASH_PART_PATH=custom_TEMP_BLOCKSHASH_PART_PATH,
         )
     result = send_tx.dump_json() if send_tx != False else False
     return jsonify(result)
@@ -164,39 +166,34 @@ def send_coin_data_page(address, amount, data, password):
 
 @app.route("/wallet/balance", methods=["GET"])
 def balance_wallets_page():
-    logger.info(
-        f"{request.remote_addr} {request.method} {request.url} {request.data}")
+    logger.info(f"{request.remote_addr} {request.method} {request.url} {request.data}")
     return jsonify(GetBalance(GetBlock(), wallet_import(-1, 0)))
 
 
 @app.route("/node/start/<ip>/<port>", methods=["GET"])
 def node_start_page(ip, port):
-    logger.info(
-        f"{request.remote_addr} {request.method} {request.url} {request.data}")
+    logger.info(f"{request.remote_addr} {request.method} {request.url} {request.data}")
     server(str(ip), int(port))
     return jsonify("OK")
 
 
 @app.route("/node/stop", methods=["GET"])
 def node_stop_page():
-    logger.info(
-        f"{request.remote_addr} {request.method} {request.url} {request.data}")
+    logger.info(f"{request.remote_addr} {request.method} {request.url} {request.data}")
     server.Server.stop()
     return jsonify("OK")
 
 
 @app.route("/node/connect/<ip>/<port>", methods=["GET"])
 def node_connect_page(ip, port):
-    logger.info(
-        f"{request.remote_addr} {request.method} {request.url} {request.data}")
+    logger.info(f"{request.remote_addr} {request.method} {request.url} {request.data}")
     server.Server.connect(str(ip), int(port))
     return jsonify("OK")
 
 
 @app.route("/node/connectmixdb", methods=["GET"])
 def node_connectmixdb_page():
-    logger.info(
-        f"{request.remote_addr} {request.method} {request.url} {request.data}")
+    logger.info(f"{request.remote_addr} {request.method} {request.url} {request.data}")
     server.connectionfrommixdb()
     return jsonify("OK")
 
@@ -204,39 +201,34 @@ def node_connectmixdb_page():
 # /node/newunl/?MFYw......
 @app.route("/node/newunl/", methods=["GET"])
 def node_newunl_page():
-    logger.info(
-        f"{request.remote_addr} {request.method} {request.url} {request.data}")
+    logger.info(f"{request.remote_addr} {request.method} {request.url} {request.data}")
     Unl.save_new_unl_node(request.query_string.decode("utf-8"))
     return jsonify("OK")
 
 
 @app.route("/node/id", methods=["GET"])
 def node_id_page():
-    logger.info(
-        f"{request.remote_addr} {request.method} {request.url} {request.data}")
+    logger.info(f"{request.remote_addr} {request.method} {request.url} {request.data}")
     return jsonify(server.id)
 
 
 @app.route("/settings/test/on", methods=["GET"])
 def settings_test_on_page():
-    logger.info(
-        f"{request.remote_addr} {request.method} {request.url} {request.data}")
+    logger.info(f"{request.remote_addr} {request.method} {request.url} {request.data}")
     t_mode_settings(True)
     return jsonify("OK")
 
 
 @app.route("/settings/test/off", methods=["GET"])
 def settings_test_off_page():
-    logger.info(
-        f"{request.remote_addr} {request.method} {request.url} {request.data}")
+    logger.info(f"{request.remote_addr} {request.method} {request.url} {request.data}")
     t_mode_settings(False)
     return jsonify("OK")
 
 
 @app.route("/settings/debug/on", methods=["GET"])
 def settings_debug_on_page():
-    logger.info(
-        f"{request.remote_addr} {request.method} {request.url} {request.data}")
+    logger.info(f"{request.remote_addr} {request.method} {request.url} {request.data}")
     app.config["DEBUG"] = True
     d_mode_settings(True)
     return jsonify("OK")
@@ -244,8 +236,7 @@ def settings_debug_on_page():
 
 @app.route("/settings/debug/off", methods=["GET"])
 def settings_debug_off_page():
-    logger.info(
-        f"{request.remote_addr} {request.method} {request.url} {request.data}")
+    logger.info(f"{request.remote_addr} {request.method} {request.url} {request.data}")
     app.config["DEBUG"] = False
     d_mode_settings(False)
     return jsonify("OK")
@@ -253,8 +244,7 @@ def settings_debug_off_page():
 
 @app.route("/block/get", methods=["GET"])
 def block_get_page():
-    logger.info(
-        f"{request.remote_addr} {request.method} {request.url} {request.data}")
+    logger.info(f"{request.remote_addr} {request.method} {request.url} {request.data}")
     if the_settings()["test_mode"]:
         the_block = CreateBlock()
         SaveBlock(the_block)
@@ -268,8 +258,7 @@ def block_get_page():
 
 @app.route("/export/transactions/csv", methods=["GET"])
 def export_transaction_csv_page():
-    logger.info(
-        f"{request.remote_addr} {request.method} {request.url} {request.data}")
+    logger.info(f"{request.remote_addr} {request.method} {request.url} {request.data}")
     if export_the_transactions():
         return jsonify("OK")
     else:
@@ -278,16 +267,13 @@ def export_transaction_csv_page():
 
 @app.route("/export/transactions/json", methods=["GET"])
 def export_transaction_json_page():
-    logger.info(
-        f"{request.remote_addr} {request.method} {request.url} {request.data}")
-    return jsonify(
-        [f"{str(i[0].__dict__)} | {str(i[1])}" for i in GetMyTransaction()])
+    logger.info(f"{request.remote_addr} {request.method} {request.url} {request.data}")
+    return jsonify([f"{str(i[0].__dict__)} | {str(i[1])}" for i in GetMyTransaction()])
 
 
 @app.route("/status", methods=["GET"])
 def status_page():
-    logger.info(
-        f"{request.remote_addr} {request.method} {request.url} {request.data}")
+    logger.info(f"{request.remote_addr} {request.method} {request.url} {request.data}")
     return jsonify(Status())
 
 
@@ -300,11 +286,7 @@ def start(port=None, test=False):
         description="This is an open source decentralized application network. In this network, you can develop and publish decentralized applications."
     )
 
-    parser.add_argument("-p",
-                        "--port",
-                        default=8000,
-                        type=int,
-                        help="Add new UNL node")
+    parser.add_argument("-p", "--port", default=8000, type=int, help="Add new UNL node")
 
     parser.add_argument(
         "-i",
@@ -327,8 +309,11 @@ def start(port=None, test=False):
     safety_check(args.interface, args.timeout)
 
     logger.info(f"Starting API on port {args.port}")
-    result = serve(app, host="0.0.0.0", port=args.port) if test is False else create_server(
-        app, host="0.0.0.0", port=args.port)
+    result = (
+        serve(app, host="0.0.0.0", port=args.port)
+        if test is False
+        else create_server(app, host="0.0.0.0", port=args.port)
+    )
     return result
 
 
