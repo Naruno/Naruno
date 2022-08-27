b26298eb1585c9692cabfc2477a627d02d44ab3e
Onur Atakan ULUSOY <atadogan06@gmail.com>
2022-08-27 16:19:55 +0300

    tests: Added unit test for wallet_change_page api (#1040)
    * tests: Added unit test for wallet_change_page api
    
    * fix try 1
    
    * Restyled by autopep8
    
    * Restyled by black
    
    * Restyled by isort
    
    * Restyled by reorder-python-imports
    
    * Restyled by yapf
    
    * Restore test_wallet.py
    
    * Restore main.py
    
    * fix
    
    * Restyled by autopep8
    
    * Restyled by black
    
    * Restyled by isort
    
    * Restyled by yapf
    
    * fix try 2
    
    * Restyled by autopep8
    
    * Restyled by black
    
    * Restyled by isort
    
    * Restyled by yapf
    
    * fix try: 3
    
    * Restyled by autopep8
    
    * Restyled by black
    
    * Restyled by isort
    
    * Restyled by reorder-python-imports
    
    * Restyled by yapf
    
    Co-authored-by: Restyled.io <commits@restyled.io>

--- tests/unit_tests/test_api.py
+++ tests/unit_tests/test_api.py
@@ -8,16 +8,25 @@ import json
 import os
 import sys
 import time
-from decentra_network.wallet.print_wallets import print_wallets
-sys.path.append(os.path.join(os.path.dirname(__file__), "..", ".."))
-import unittest
 
+from decentra_network.lib.settings_system import save_settings
+from decentra_network.lib.settings_system import the_settings
+
+sys.path.append(os.path.join(os.path.dirname(__file__), "..", ".."))
+
+import threading
+import unittest
 import urllib
 
 from decentra_network.api.main import start
 from decentra_network.lib.clean_up import CleanUp_tests
+from decentra_network.wallet.ellipticcurve.get_saved_wallet import \
+    get_saved_wallet
+from decentra_network.wallet.ellipticcurve.save_wallet_list import \
+    save_wallet_list
+from decentra_network.wallet.ellipticcurve.wallet_create import wallet_create
+from decentra_network.wallet.print_wallets import print_wallets
 
-import threading
 
 class Test_API(unittest.TestCase):
 
@@ -26,22 +35,52 @@ class Test_API(unittest.TestCase):
         CleanUp_tests()
         backup = sys.argv
         sys.argv = [sys.argv[0]]
+
         cls.result = start(port=7777, test=True)
         cls.proc = threading.Thread(target=cls.result.run)
         cls.proc.start()
+
         sys.argv = backup
         time.sleep(2)
-    
+
     @classmethod
     def tearDownClass(cls):
         cls.result.close()
 
-    def test_api_debug_by_response_status_code(self):
+    def test_print_wallets_page(self):
         response = urllib.request.urlopen("http://localhost:7777/wallet/print")
         result = str(json.loads(response.read())).replace("'", """\"""")
+
         data = str(json.dumps(print_wallets()))
+
         self.assertEqual(result, data)
 
+    def test_change_wallet_page(self):
+        backup_settings = the_settings()
+
+        original_saved_wallets = get_saved_wallet()
+        save_wallet_list({})
+
+        password = "123"
+
+        temp_private_key = wallet_create(password)
+        temp_private_key_2 = wallet_create(password)
+
+        response = urllib.request.urlopen(
+            "http://localhost:7777/wallet/change/1")
+        result = str(json.loads(response.read())).replace("'", """\"""")
+
+        data = str(json.dumps(print_wallets()))
+
+        self.assertEqual(result, data)
+
+        control = False
+        if "CURRENTLY USED" in print_wallets()[1]:
+            control = True
+
+        self.assertTrue(control)
+        save_settings(backup_settings)
+        save_wallet_list(original_saved_wallets)
 
 
 unittest.main(exit=False)
