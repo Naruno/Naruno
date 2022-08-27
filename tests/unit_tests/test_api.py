0b6836df89a40832909e99b75b6d89c787e79b9e
Restyled.io <commits@restyled.io>
2022-08-27 15:11:33 +0000

    Restyled by isort

--- tests/unit_tests/test_api.py
+++ tests/unit_tests/test_api.py
@@ -9,19 +9,19 @@ import os
 import sys
 import time
 
-
 sys.path.append(os.path.join(os.path.dirname(__file__), "..", ".."))
 
 import threading
 import unittest
 import urllib
 
-from decentra_network.lib.settings_system import save_settings
-from decentra_network.lib.settings_system import the_settings
 from decentra_network.api.main import start
 from decentra_network.lib.clean_up import CleanUp_tests
-from decentra_network.wallet.ellipticcurve.get_saved_wallet import get_saved_wallet
-from decentra_network.wallet.ellipticcurve.save_wallet_list import save_wallet_list
+from decentra_network.lib.settings_system import save_settings, the_settings
+from decentra_network.wallet.ellipticcurve.get_saved_wallet import \
+    get_saved_wallet
+from decentra_network.wallet.ellipticcurve.save_wallet_list import \
+    save_wallet_list
 from decentra_network.wallet.ellipticcurve.wallet_create import wallet_create
 from decentra_network.wallet.print_wallets import print_wallets
 
