0a749ef9416753afad12a2cc807d86ba16618be6
Onur Atakan ULUSOY <atadogan06@gmail.com>
2022-08-28 19:23:16 +0300

    tests: Adding unit test for settings_test_on_page api and settings_te… (#1052)
    * tests: Adding unit test for settings_test_on_page api and settings_test_off_page api
    
    * fix
    
    * Restyled by autopep8
    
    * Restyled by black
    
    * Restyled by isort
    
    * Restyled by yapf
    
    Co-authored-by: Restyled.io <commits@restyled.io>

--- tests/unit_tests/test_api.py
+++ tests/unit_tests/test_api.py
@@ -491,5 +491,21 @@ class Test_API(unittest.TestCase):
             server.id,
         )
 
+    def test_settings_test_on_off_page(self):
+        temp_settings = the_settings()
+        changed_value = "on" if temp_settings["test_mode"] is False else "off"
+        response = urllib.request.urlopen(
+            f"http://localhost:7777/settings/test/{changed_value}")
+        new_settings = the_settings()
+        expected_alue = True if changed_value == "on" else False
+        self.assertEqual(new_settings["test_mode"], expected_alue)
+
+        default = "off" if temp_settings["test_mode"] is False else "on"
+        response = urllib.request.urlopen(
+            f"http://localhost:7777/settings/test/{default}")
+
+        new_settings = the_settings()
+        self.assertEqual(new_settings["test_mode"], temp_settings["test_mode"])
+
 
 unittest.main(exit=False)
