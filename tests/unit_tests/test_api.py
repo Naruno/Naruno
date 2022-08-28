d0310db01892c0157e29407b29737f1a0a21e543
Restyled.io <commits@restyled.io>
2022-08-28 16:16:04 +0000

    Restyled by autopep8

--- tests/unit_tests/test_api.py
+++ tests/unit_tests/test_api.py
@@ -494,14 +494,18 @@ class Test_API(unittest.TestCase):
     def test_settings_test_on_off_page(self):
         temp_settings = the_settings()
         changed_value = "on" if temp_settings["test_mode"] is False else "off"
-        response = urllib.request.urlopen(f"http://localhost:7777/settings/test/{changed_value}")
+        response = urllib.request.urlopen(
+            f"http://localhost:7777/settings/test/{changed_value}")
         new_settings = the_settings()
         expected_alue = True if changed_value == "on" else False
         self.assertEqual(new_settings["test_mode"], expected_alue)
 
         default = "off" if temp_settings["test_mode"] is False else "on"
-        response = urllib.request.urlopen(f"http://localhost:7777/settings/test/{default}")
+        response = urllib.request.urlopen(
+            f"http://localhost:7777/settings/test/{default}")
 
         new_settings = the_settings()
         self.assertEqual(new_settings["test_mode"], temp_settings["test_mode"])
+
+
 unittest.main(exit=False)
