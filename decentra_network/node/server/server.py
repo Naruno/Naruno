814388cb7ea3cf6a3cba3e629e25cc008fe90384
Onur Atakan ULUSOY <atadogan06@gmail.com>
2022-08-30 04:57:05 +0300

    node: Added time to sending data for more security (#1058)
    * node: Added time to sending data for more security
    
    * fix
    
    * fix
    
    * Restyled by autopep8
    
    * Restyled by black
    
    * Restyled by isort
    
    * Restyled by reorder-python-imports
    
    * Restyled by yapf
    
    Co-authored-by: Restyled.io <commits@restyled.io>

--- decentra_network/node/server/server.py
+++ decentra_network/node/server/server.py
@@ -67,6 +67,7 @@ class server(Thread):
         custom_LOADING_BLOCKSHASH_PART_PATH=None,
         custom_CONNECTED_NODES_PATH=None,
         custom_PENDING_TRANSACTIONS_PATH=None,
+        time_control=None,
     ):
         Thread.__init__(self)
         self.running = True
@@ -122,6 +123,8 @@ class server(Thread):
 
         self.custom_variables = custom_variables
 
+        self.time_control = 10 if time_control is None else time_control
+
         if not test:
             self.__class__.Server = self
             self.start()
@@ -160,6 +163,7 @@ class server(Thread):
 
     def prepare_message(self, data):
         data["id"] = server.id
+        data["timestamp"] = str(time.time())
         sign = Ecdsa.sign(
             str(data),
             PrivateKey.fromPem(wallet_import(0, 1)),
@@ -212,6 +216,11 @@ class server(Thread):
         if "id" not in data:
             return False
         if "signature" not in data:
+            return False
+        if "timestamp" not in data:
+            return False
+        the_control = time.time() - float(data["timestamp"])
+        if the_control > self.time_control:
             return False
         # remove sign from data
         sign = data["signature"]
