ab00ea82adaf08ff54525f3657e0f69daa858d23
Onur Atakan ULUSOY <atadogan06@gmail.com>
2022-08-31 10:04:34 +0000

    accounts: Added caching to accounts

--- decentra_network/accounts/get_accounts.py
+++ decentra_network/accounts/get_accounts.py
@@ -10,6 +10,7 @@ import sqlite3
 
 from decentra_network.accounts.account import Account
 from decentra_network.config import TEMP_ACCOUNTS_PATH
+from decentra_network.lib.cache import Cache
 from decentra_network.lib.config_system import get_config
 
 
@@ -18,16 +19,24 @@ def GetAccounts(custom_TEMP_ACCOUNTS_PATH=None):
     Returns the accounts from TEMP_ACCOUNTS_PATH.
     """
 
+
+
     the_TEMP_ACCOUNTS_PATH = (TEMP_ACCOUNTS_PATH
                               if custom_TEMP_ACCOUNTS_PATH is None else
                               custom_TEMP_ACCOUNTS_PATH)
 
-    os.chdir(get_config()["main_folder"])
-    conn = sqlite3.connect(the_TEMP_ACCOUNTS_PATH, check_same_thread=False)
-    c = conn.cursor()
-    c.execute(
-        """CREATE TABLE IF NOT EXISTS account_list (address text, sequance_number integer, balance integer)"""
-    )
-    conn.commit()
+    the_cache = Cache.get(the_TEMP_ACCOUNTS_PATH)
 
-    return c
+    if the_cache is None:
+        os.chdir(get_config()["main_folder"])
+        conn = sqlite3.connect(the_TEMP_ACCOUNTS_PATH, check_same_thread=False)
+        c = conn.cursor()
+        c.execute(
+            """CREATE TABLE IF NOT EXISTS account_list (address text, sequance_number integer, balance integer)"""
+        )
+        conn.commit()
+
+        Cache.save(the_TEMP_ACCOUNTS_PATH, c)
+        return c
+    else:
+        return the_cache
