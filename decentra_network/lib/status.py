9de479725db9668b744c6c7eb130bc1232b75607
Onur Atakan ULUSOY <atadogan06@gmail.com>
2022-09-04 11:37:09 +0300

    blockchain: Added no_cache option for GetBlock (#1074)
    * blockchain: Added no_cache option for GetBlock
    
    * Restyled by autopep8
    
    * Restyled by black
    
    * Restyled by isort
    
    * Restyled by yapf
    
    * fix
    
    * Restyled by autopep8
    
    * Restyled by black
    
    * Restyled by isort
    
    * Restyled by yapf
    
    Co-authored-by: Restyled.io <commits@restyled.io>

--- decentra_network/lib/status.py
+++ decentra_network/lib/status.py
@@ -25,11 +25,13 @@ def Status(
     Returns the status of the network.
     """
 
-    first_block = (GetBlock(custom_TEMP_BLOCK_PATH=custom_TEMP_BLOCK_PATH)
+    first_block = (GetBlock(custom_TEMP_BLOCK_PATH=custom_TEMP_BLOCK_PATH,
+                            no_cache=True)
                    if custom_first_block is None else custom_first_block)
 
     time.sleep(25)
-    new_block = (GetBlock(custom_TEMP_BLOCK_PATH=custom_TEMP_BLOCK_PATH)
+    new_block = (GetBlock(custom_TEMP_BLOCK_PATH=custom_TEMP_BLOCK_PATH,
+                          no_cache=True)
                  if custom_new_block is None else custom_new_block)
 
     connections = (Unl.get_as_node_type(
