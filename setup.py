a5f6ab1a797217db5289ecfd36b9d384e3a81567
Onur Atakan ULUSOY <atadogan06@gmail.com>
2022-09-07 20:16:04 +0000

    Added version 0.29.0

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setup(
     packages     = ["decentra_network"],
 
     name         = "decentra_network",
-    version      = "0.28.0",
+    version      = "0.29.0",
     url          = "https://github.com/Decentra-Network/Decentra-Network",
     description  = "This is an open source decentralized application network. In this network, you can develop and publish decentralized applications.",
     keywords     = ["python", "cryptography", "blockchain", "p2p", "python3", "cryptocurrency", "kivy", "coin", "copilot", "fba", "dapps", "p2p-network", "kivymd", "blokzinciri", "decentra-network", "githubcopilot", "blokzincir"],
