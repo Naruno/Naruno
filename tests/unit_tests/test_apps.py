a31dc180b01fba5853a63779650e27ce4ae4d678
Onur Atakan ULUSOY <atadogan06@gmail.com>
2022-09-07 22:35:09 +0300

    apps: Added a test applications dedect and disable methods for real usages (#1078)

--- tests/unit_tests/test_apps.py
+++ tests/unit_tests/test_apps.py
@@ -6,15 +6,16 @@
 # file, You can obtain one at https://mozilla.org/MPL/2.0/.
 import os
 import sys
+
 sys.path.append(os.path.join(os.path.dirname(__file__), "..", ".."))
 import time
 import unittest
 
 from decentra_network.apps.apps_trigger import AppsTrigger
 from decentra_network.blockchain.block.block_main import Block
+from decentra_network.lib.clean_up import CleanUp_tests
 from decentra_network.lib.config_system import get_config
 from decentra_network.transactions.transaction import Transaction
-from decentra_network.lib.clean_up import CleanUp_tests
 
 
 class Test_apps(unittest.TestCase):
@@ -23,8 +24,23 @@ class Test_apps(unittest.TestCase):
     def setUpClass(cls):
         CleanUp_tests()
 
+    def test_AppsTrigger_App_not_test_app(self):
+        block = Block("onur")
+        block.sequance_number = 2
+        the_transaction = Transaction(1, 1, 1, 1, 1, 1, 1, 1)
+        the_transaction.transaction_time = time.time()
+        block.validating_list.append(the_transaction)
+        AppsTrigger(block)
+        time.sleep(2)
+        os.chdir(get_config()["main_folder"])
+        self.assertFalse(
+            os.path.isfile(
+                f"apps/testing_app/{block.validating_list[0].transaction_time}.tx"
+            ))
+
     def test_AppsTrigger_App(self):
         block = Block("onur")
+        block.sequance_number = 1
         the_transaction = Transaction(1, 1, 1, 1, 1, 1, 1, 1)
         the_transaction.transaction_time = time.time()
         block.validating_list.append(the_transaction)
