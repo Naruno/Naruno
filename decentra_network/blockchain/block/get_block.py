3ad835f821cd85629a91b97379c9d31be8010bc9
Restyled.io <commits@restyled.io>
2022-09-08 20:40:07 +0000

    Restyled by black

--- decentra_network/blockchain/block/get_block.py
+++ decentra_network/blockchain/block/get_block.py
@@ -19,8 +19,9 @@ def GetBlock(custom_TEMP_BLOCK_PATH=None):
     """
     Returns the block.
     """
-    the_TEMP_BLOCK_PATH = (TEMP_BLOCK_PATH if custom_TEMP_BLOCK_PATH is None
-                           else custom_TEMP_BLOCK_PATH)
+    the_TEMP_BLOCK_PATH = (
+        TEMP_BLOCK_PATH if custom_TEMP_BLOCK_PATH is None else custom_TEMP_BLOCK_PATH
+    )
 
     os.chdir(get_config()["main_folder"])
     with open(the_TEMP_BLOCK_PATH, "r") as block_file:
