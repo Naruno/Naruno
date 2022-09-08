4302e705acd2661dcb71460cbbfedfde535b4d9b
Onur Atakan ULUSOY <atadogan06@gmail.com>
2022-09-08 19:02:24 +0000

    accounts: Disabled caching for accounts

--- decentra_network/accounts/get_accounts.py
+++ decentra_network/accounts/get_accounts.py
@@ -4,13 +4,11 @@
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at https://mozilla.org/MPL/2.0/.
-import json
 import os
 import sqlite3
 
-from decentra_network.accounts.account import Account
+
 from decentra_network.config import TEMP_ACCOUNTS_PATH
-from decentra_network.lib.cache import Cache
 from decentra_network.lib.config_system import get_config
 
 
@@ -23,19 +21,12 @@ def GetAccounts(custom_TEMP_ACCOUNTS_PATH=None):
                               if custom_TEMP_ACCOUNTS_PATH is None else
                               custom_TEMP_ACCOUNTS_PATH)
 
-    the_cache = Cache.get(the_TEMP_ACCOUNTS_PATH)
-    the_cache_2 = Cache.get(f"{the_TEMP_ACCOUNTS_PATH}_conn")
-    if the_cache is None or the_cache_2 is None:
-        os.chdir(get_config()["main_folder"])
-        conn = sqlite3.connect(the_TEMP_ACCOUNTS_PATH, check_same_thread=False)
-        c = conn.cursor()
-        c.execute(
-            """CREATE TABLE IF NOT EXISTS account_list (address text, sequance_number integer, balance integer)"""
-        )
-        conn.commit()
 
-        Cache.save(the_TEMP_ACCOUNTS_PATH, c)
-        Cache.save(f"{the_TEMP_ACCOUNTS_PATH}_conn", conn)
-        return c
-    else:
-        return the_cache
+    os.chdir(get_config()["main_folder"])
+    conn = sqlite3.connect(the_TEMP_ACCOUNTS_PATH, check_same_thread=False)
+    c = conn.cursor()
+    c.execute(
+        """CREATE TABLE IF NOT EXISTS account_list (address text, sequance_number integer, balance integer)"""
+    )
+    return c
+
