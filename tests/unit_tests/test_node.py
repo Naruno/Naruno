9a5c71949455f22bb37be2f4dae5225bfdbd2cbe
Restyled.io <commits@restyled.io>
2022-09-11 15:15:02 +0000

    Restyled by reorder-python-imports

--- tests/unit_tests/test_node.py
+++ tests/unit_tests/test_node.py
@@ -7,8 +7,8 @@
 import os
 import sys
 
-from decentra_network.blockchain.block.blocks_hash import (GetBlockshash,
-                                                           GetBlockshash_part)
+from decentra_network.blockchain.block.blocks_hash import GetBlockshash
+from decentra_network.blockchain.block.blocks_hash import GetBlockshash_part
 
 sys.path.append(os.path.join(os.path.dirname(__file__), "..", ".."))
 import copy
