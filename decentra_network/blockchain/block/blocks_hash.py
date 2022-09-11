f01b7c5f7146c343fbcb868d38a3a5b256527e86
Restyled.io <commits@restyled.io>
2022-09-11 11:59:52 +0000

    Restyled by black

--- decentra_network/blockchain/block/blocks_hash.py
+++ decentra_network/blockchain/block/blocks_hash.py
@@ -22,21 +22,18 @@ def SaveBlockshash(the_blockshash, custom_TEMP_BLOCKSHASH_PATH=None):
     """
 
     os.chdir(get_config()["main_folder"])
-    the_TEMP_BLOCKSHASH_PATH = (TEMP_BLOCKSHASH_PATH
-                                if custom_TEMP_BLOCKSHASH_PATH is None else
-                                custom_TEMP_BLOCKSHASH_PATH)
+    the_TEMP_BLOCKSHASH_PATH = (
+        TEMP_BLOCKSHASH_PATH
+        if custom_TEMP_BLOCKSHASH_PATH is None
+        else custom_TEMP_BLOCKSHASH_PATH
+    )
 
     conn = sqlite3.connect(the_TEMP_BLOCKSHASH_PATH)
     c = conn.cursor()
-    c.execute(
-        """CREATE TABLE IF NOT EXISTS blockshash_list (hash text)"""
-    )
+    c.execute("""CREATE TABLE IF NOT EXISTS blockshash_list (hash text)""")
     if type(the_blockshash) == list:
         for i in the_blockshash:
-            c.execute(
-                """INSERT INTO blockshash_list VALUES (?)""",
-                (i,)
-            )
+            c.execute("""INSERT INTO blockshash_list VALUES (?)""", (i,))
     else:
         c.execute(
             """INSERT INTO blockshash_list VALUES (?)""",
@@ -52,11 +49,12 @@ def SaveBlockshash_part(the_blockshash, custom_TEMP_BLOCKSHASH_PART_PATH=None):
     """
 
     os.chdir(get_config()["main_folder"])
-    the_TEMP_BLOCKSHASH_PART_PATH = (TEMP_BLOCKSHASH_PART_PATH if
-                                     custom_TEMP_BLOCKSHASH_PART_PATH is None
-                                     else custom_TEMP_BLOCKSHASH_PART_PATH)
-    logger.info(
-        f"Saving blockshash part to disk ({the_TEMP_BLOCKSHASH_PART_PATH})")
+    the_TEMP_BLOCKSHASH_PART_PATH = (
+        TEMP_BLOCKSHASH_PART_PATH
+        if custom_TEMP_BLOCKSHASH_PART_PATH is None
+        else custom_TEMP_BLOCKSHASH_PART_PATH
+    )
+    logger.info(f"Saving blockshash part to disk ({the_TEMP_BLOCKSHASH_PART_PATH})")
     with open(the_TEMP_BLOCKSHASH_PART_PATH, "w") as block_file:
         json.dump(the_blockshash, block_file)
 
@@ -65,17 +63,17 @@ def GetBlockshash(custom_TEMP_BLOCKSHASH_PATH=None):
     """
     Returns the blockshash.
     """
-    the_TEMP_BLOCKSHASH_PATH = (TEMP_BLOCKSHASH_PATH
-                                if custom_TEMP_BLOCKSHASH_PATH is None else
-                                custom_TEMP_BLOCKSHASH_PATH)
+    the_TEMP_BLOCKSHASH_PATH = (
+        TEMP_BLOCKSHASH_PATH
+        if custom_TEMP_BLOCKSHASH_PATH is None
+        else custom_TEMP_BLOCKSHASH_PATH
+    )
 
     os.chdir(get_config()["main_folder"])
 
     conn = sqlite3.connect(the_TEMP_BLOCKSHASH_PATH, check_same_thread=False)
     c = conn.cursor()
-    c.execute(
-        """CREATE TABLE IF NOT EXISTS blockshash_list (hash text)"""
-    )
+    c.execute("""CREATE TABLE IF NOT EXISTS blockshash_list (hash text)""")
     c.execute("""SELECT * FROM blockshash_list""")
     result = c.fetchall()
 
@@ -89,9 +87,11 @@ def GetBlockshash_part(custom_TEMP_BLOCKSHASH_PART_PATH=None):
     """
     Returns the blockshash part.
     """
-    the_TEMP_BLOCKSHASH_PART_PATH = (TEMP_BLOCKSHASH_PART_PATH if
-                                     custom_TEMP_BLOCKSHASH_PART_PATH is None
-                                     else custom_TEMP_BLOCKSHASH_PART_PATH)
+    the_TEMP_BLOCKSHASH_PART_PATH = (
+        TEMP_BLOCKSHASH_PART_PATH
+        if custom_TEMP_BLOCKSHASH_PART_PATH is None
+        else custom_TEMP_BLOCKSHASH_PART_PATH
+    )
 
     os.chdir(get_config()["main_folder"])
     if not os.path.exists(the_TEMP_BLOCKSHASH_PART_PATH):
