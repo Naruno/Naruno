0585515e67ba0f1011a4fc4ef43f78e86494787b
Onur Atakan ULUSOY <atadogan06@gmail.com>
2022-09-12 15:39:18 +0000

    pakcages: Added decentra_network_tests package

---
+++ setups/tests/setup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at https://mozilla.org/MPL/2.0/.
+from setuptools import setup
+
+setup(
+    name="decentra_network_tests",
+    version="0.29.1",
+    description="""This is a tool for tests on Decentra Network""",
+    url="https://decentra-network.github.io/Decentra-Network/",
+    author="Decentra Network Developers",
+    author_email="atadogan06@gmail.com",
+    license="MPL-2.0",
+    install_requires="""
+requests==2.28.0
+pytest==7.1.2
+speed_calculator==0.4.1
+""",
+    python_requires=">=3.8",
+    zip_safe=False,
+)
