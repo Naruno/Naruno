166afeefa06c3fc0d50694be50ee7d9bb5db519e
Onur Atakan ULUSOY <atadogan06@gmail.com>
2022-09-14 21:26:28 +0300

    Update tabnavigation.py

--- decentra_network/gui_lib/libs/baseclass/tabnavigation.py
+++ decentra_network/gui_lib/libs/baseclass/tabnavigation.py
@@ -1,10 +1,3 @@
-#!/usr/bin/python3
-# -*- coding: utf-8 -*-
-#
-# This Source Code Form is subject to the terms of the Mozilla Public
-# License, v. 2.0. If a copy of the MPL was not distributed with this
-# file, You can obtain one at https://mozilla.org/MPL/2.0/.
-
 from kivy.animation import Animation
 from kivy.clock import Clock
 from kivy.core.window import Window
