6efa83b1324a6737f5e2663de26a6011688b1222
Onur Atakan ULUSOY <atadogan06@gmail.com>
2022-09-15 16:07:09 +0000

    api: Added new `/transactions/` based API's that seperated by sended or received and validated or not validated

--- decentra_network/transactions/my_transactions/get_my_transaction.py
+++ decentra_network/transactions/my_transactions/get_my_transaction.py
@@ -12,7 +12,7 @@ from decentra_network.lib.config_system import get_config
 from decentra_network.transactions.transaction import Transaction
 
 
-def GetMyTransaction(sended=None, validated=None) -> list:
+def GetMyTransaction(sended=None, validated=None, turn_json=False) -> list:
     """
     Returns the transaction db.
     """
@@ -38,5 +38,8 @@ def GetMyTransaction(sended=None, validated=None) -> list:
     
     if validated is not None:
         the_transactions = [tx for tx in the_transactions if tx[1] == validated]
-    
+
+    if turn_json:
+        the_transactions = {the_transactions.index(tx):{"transaction":tx[0].dump_json(), "validated":tx[1], "sended":tx[2]} for tx in the_transactions}
+
     return the_transactions
