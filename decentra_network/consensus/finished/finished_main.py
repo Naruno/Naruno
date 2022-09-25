48e8ffd3a5edb8dc8ecd10f942c137a72a5f1fe8
Restyled.io <commits@restyled.io>
2022-09-25 11:50:03 +0000

    Restyled by black

--- decentra_network/consensus/finished/finished_main.py
+++ decentra_network/consensus/finished/finished_main.py
@@ -9,26 +9,30 @@ import shutil
 
 from decentra_network.apps.apps_trigger import AppsTrigger
 from decentra_network.blockchain.block.block_main import Block
-from decentra_network.blockchain.block.blocks_hash import GetBlockshash, GetBlockshash_part
+from decentra_network.blockchain.block.blocks_hash import (
+    GetBlockshash,
+    GetBlockshash_part,
+)
 from decentra_network.blockchain.block.blocks_hash import SaveBlockshash
 from decentra_network.blockchain.block.blocks_hash import SaveBlockshash_part
 from decentra_network.blockchain.block.save_block import SaveBlock
-from decentra_network.blockchain.block.save_block_to_blockchain_db import \
-    SaveBlockstoBlockchainDB
+from decentra_network.blockchain.block.save_block_to_blockchain_db import (
+    SaveBlockstoBlockchainDB,
+)
 from decentra_network.config import BLOCKS_PATH
 from decentra_network.config import TEMP_ACCOUNTS_PATH
 from decentra_network.config import TEMP_BLOCK_PATH
 from decentra_network.config import TEMP_BLOCKSHASH_PART_PATH
 from decentra_network.config import TEMP_BLOCKSHASH_PATH
-from decentra_network.consensus.finished.transactions.transactions_main import \
-    transactions_main
+from decentra_network.consensus.finished.transactions.transactions_main import (
+    transactions_main,
+)
 from decentra_network.consensus.time.true_time.true_time_main import true_time
 from decentra_network.lib.log import get_logger
 from decentra_network.lib.mix.merkle_root import MerkleTree
 from decentra_network.lib.settings_system import save_settings
 from decentra_network.lib.settings_system import the_settings
-from decentra_network.transactions.pending_to_validating import \
-    PendingtoValidating
+from decentra_network.transactions.pending_to_validating import PendingtoValidating
 
 logger = get_logger("CONSENSUS")
 
@@ -43,25 +47,32 @@ def finished_main(
 ) -> None:
     if true_time(block):
         block.newly = False
-        logger.debug(
-            "Consensus proccess is complated, the block will be reset")
+        logger.debug("Consensus proccess is complated, the block will be reset")
 
-        the_BLOCKS_PATH = (BLOCKS_PATH if custom_BLOCKS_PATH is None else
-                           custom_BLOCKS_PATH)
-        the_TEMP_ACCOUNTS_PATH = (TEMP_ACCOUNTS_PATH
-                                  if custom_TEMP_ACCOUNTS_PATH is None else
-                                  custom_TEMP_ACCOUNTS_PATH)
-        the_TEMP_BLOCKSHASH_PATH = (TEMP_BLOCKSHASH_PATH
-                                    if custom_TEMP_BLOCKSHASH_PATH is None else
-                                    custom_TEMP_BLOCKSHASH_PATH)
+        the_BLOCKS_PATH = (
+            BLOCKS_PATH if custom_BLOCKS_PATH is None else custom_BLOCKS_PATH
+        )
+        the_TEMP_ACCOUNTS_PATH = (
+            TEMP_ACCOUNTS_PATH
+            if custom_TEMP_ACCOUNTS_PATH is None
+            else custom_TEMP_ACCOUNTS_PATH
+        )
+        the_TEMP_BLOCKSHASH_PATH = (
+            TEMP_BLOCKSHASH_PATH
+            if custom_TEMP_BLOCKSHASH_PATH is None
+            else custom_TEMP_BLOCKSHASH_PATH
+        )
         the_TEMP_BLOCKSHASH_PART_PATH = (
             TEMP_BLOCKSHASH_PART_PATH
-            if custom_TEMP_BLOCKSHASH_PART_PATH is None else
-            custom_TEMP_BLOCKSHASH_PART_PATH)
+            if custom_TEMP_BLOCKSHASH_PART_PATH is None
+            else custom_TEMP_BLOCKSHASH_PART_PATH
+        )
 
-        the_TEMP_BLOCK_PATH = (TEMP_BLOCK_PATH
-                               if custom_TEMP_BLOCK_PATH is None else
-                               custom_TEMP_BLOCK_PATH)
+        the_TEMP_BLOCK_PATH = (
+            TEMP_BLOCK_PATH
+            if custom_TEMP_BLOCK_PATH is None
+            else custom_TEMP_BLOCK_PATH
+        )
 
         reset_block = block.reset_the_block()
         settings = the_settings()
@@ -90,21 +101,25 @@ def finished_main(
             )
 
             the_blocks_hash = GetBlockshash(
-                custom_TEMP_BLOCKSHASH_PATH=the_TEMP_BLOCKSHASH_PATH)
+                custom_TEMP_BLOCKSHASH_PATH=the_TEMP_BLOCKSHASH_PATH
+            )
             if len(the_blocks_hash) == block.part_amount:
                 SaveBlockshash_part(
                     MerkleTree(the_blocks_hash).getRootHash(),
                     custom_TEMP_BLOCKSHASH_PART_PATH=the_TEMP_BLOCKSHASH_PART_PATH,
                 )
                 the_blockshas_part = GetBlockshash_part(
-                    custom_TEMP_BLOCKSHASH_PART_PATH=the_TEMP_BLOCKSHASH_PART_PATH)
-                block.part_amount_cache = MerkleTree(
-                    the_blockshas_part).getRootHash()
+                    custom_TEMP_BLOCKSHASH_PART_PATH=the_TEMP_BLOCKSHASH_PART_PATH
+                )
+                block.part_amount_cache = MerkleTree(the_blockshas_part).getRootHash()
                 if settings["save_blockshash"] == True:
                     shutil.copyfile(
                         the_TEMP_BLOCKSHASH_PATH,
-                        (the_BLOCKS_PATH + str(block.sequance_number) +
-                         ".blockshash_full.json"),
+                        (
+                            the_BLOCKS_PATH
+                            + str(block.sequance_number)
+                            + ".blockshash_full.json"
+                        ),
                     )
                     if not new_tx_from_us:
                         settings["save_blockshash"] = False
@@ -121,6 +136,5 @@ def finished_main(
         )
         return True
     else:
-        logger.debug(
-            "Consensus proccess is complated, waiting for the true time")
+        logger.debug("Consensus proccess is complated, waiting for the true time")
         return False
