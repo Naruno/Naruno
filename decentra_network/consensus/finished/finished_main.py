777dea0310d08813c26916674e0b8444035397f3
Restyled.io <commits@restyled.io>
2022-09-25 11:50:01 +0000

    Restyled by autopep8

--- decentra_network/consensus/finished/finished_main.py
+++ decentra_network/consensus/finished/finished_main.py
@@ -76,8 +76,7 @@ def finished_main(
                     custom_BLOCKS_PATH=the_BLOCKS_PATH,
                     custom_TEMP_ACCOUNTS_PATH=the_TEMP_ACCOUNTS_PATH,
                     custom_TEMP_BLOCKSHASH_PATH=the_TEMP_BLOCKSHASH_PATH,
-                    custom_TEMP_BLOCKSHASH_PART_PATH=
-                    the_TEMP_BLOCKSHASH_PART_PATH,
+                    custom_TEMP_BLOCKSHASH_PART_PATH=the_TEMP_BLOCKSHASH_PART_PATH,
                 )
                 new_tx_from_us = True
                 settings["save_blockshash"] = True
@@ -95,11 +94,12 @@ def finished_main(
             if len(the_blocks_hash) == block.part_amount:
                 SaveBlockshash_part(
                     MerkleTree(the_blocks_hash).getRootHash(),
-                    custom_TEMP_BLOCKSHASH_PART_PATH=
-                    the_TEMP_BLOCKSHASH_PART_PATH,
+                    custom_TEMP_BLOCKSHASH_PART_PATH=the_TEMP_BLOCKSHASH_PART_PATH,
                 )
-                the_blockshas_part = GetBlockshash_part(custom_TEMP_BLOCKSHASH_PART_PATH=the_TEMP_BLOCKSHASH_PART_PATH)
-                block.part_amount_cache = MerkleTree(the_blockshas_part).getRootHash()
+                the_blockshas_part = GetBlockshash_part(
+                    custom_TEMP_BLOCKSHASH_PART_PATH=the_TEMP_BLOCKSHASH_PART_PATH)
+                block.part_amount_cache = MerkleTree(
+                    the_blockshas_part).getRootHash()
                 if settings["save_blockshash"] == True:
                     shutil.copyfile(
                         the_TEMP_BLOCKSHASH_PATH,
