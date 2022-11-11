b22991e674fc7724344124b430e5872599bd6ac9
Ali eren <alierentabak0156@gmail.com>
2022-11-11 18:44:28 +0000

    transactions: Added logging to PendingtoValidating function

--- decentra_network/transactions/pending_to_validating.py
+++ decentra_network/transactions/pending_to_validating.py
@@ -13,14 +13,20 @@ def PendingtoValidating(block):
     Adds transactions to the verification list
     if there are suitable conditions.
     """
-
+    logger.info("Pending to validating transfer process is started")
+    logger.debug("currently tx amount", len(block.validating_list))
+    logger.debug("Validating list capacity", block.max_tx_number)    
     if len(block.validating_list) < block.max_tx_number:
         for tx in OrderbyFee(GetPending()):
             if len(block.validating_list) < block.max_tx_number:
+                logger.info(f"tx {tx.signature} is moved to validating list")
+
                 block.validating_list.append(tx)
                 DeletePending(tx)
-
-
+            else:
+                logger.info(f"TX {tx.signature} is can not moved to validating list")
+    else:
+        logger.info("List is full")
 def OrderbyFee(transactions: list):
     """
     Sorts transactions by fee.
