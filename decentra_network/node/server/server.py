01be4a0f7fd72511f33e150ec857586a929c3cdc
Onur Atakan ULUSOY <atadogan06@gmail.com>
2022-11-13 02:35:28 +0300

    fix

--- decentra_network/node/server/server.py
+++ decentra_network/node/server/server.py
@@ -158,7 +158,7 @@ class server(Thread):
                     logger.info(f"Added node: {client_id}")
                     self.clients.append(client(conn, addr, client_id, self))
                     self.save_connected_node(addr[0], addr[1], client_id)
-                else
+                else:
                     logger.info(f"This connection want dont accepted because its not unl: {client_id}")
             time.sleep(0.01)
 
