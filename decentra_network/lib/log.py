713f190438e6cceecb1e0b7e2bfea54c8716ca68
Onur Atakan ULUSOY <atadogan06@gmail.com>
2022-11-12 13:27:47 +0000

    lib: Fixed multiple times same logging

--- decentra_network/lib/log.py
+++ decentra_network/lib/log.py
@@ -13,26 +13,33 @@ from decentra_network.config import LOGS_PATH
 from decentra_network.lib.config_system import get_config
 from decentra_network.lib.settings_system import the_settings
 
+global_logger = []
 
 def get_logger(name):
     logger = logging.getLogger(name)
-    level = logging.DEBUG if the_settings()["debug_mode"] else logging.INFO
-    logger.setLevel(level)
-    # create console handler and set level to debug
-    ch = logging.StreamHandler()
-    ch.setLevel(level)
-    # create formatter
-    formatter = logging.Formatter(
-        "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
-    )
-    # add formatter to ch
-    ch.setFormatter(formatter)
-    # add ch to logger
-    logger.addHandler(ch)
-    # file
-    main_folder = get_config()["main_folder"]
-    fh = logging.FileHandler(os.path.join(main_folder, LOGS_PATH, f"{name}.log"))
-    fh.setLevel(level)
-    fh.setFormatter(formatter)
-    logger.addHandler(fh)
+
+    if not any(element == logger
+            for element in global_logger):
+
+        level = logging.DEBUG if the_settings()["debug_mode"] else logging.INFO
+        logger.setLevel(level)
+        # create console handler and set level to debug
+        ch = logging.StreamHandler()
+        ch.setLevel(level)
+        # create formatter
+        formatter = logging.Formatter(
+            "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+        )
+        # add formatter to ch
+        ch.setFormatter(formatter)
+        # add ch to logger
+        logger.addHandler(ch)
+        # file
+        main_folder = get_config()["main_folder"]
+        fh = logging.FileHandler(os.path.join(main_folder, LOGS_PATH, f"{name}.log"))
+        fh.setLevel(level)
+        fh.setFormatter(formatter)
+        logger.addHandler(fh)
+
+        global_logger.append(logger)
     return logger
