c0486d80e9f66002a624fa32728774b7b73f1518
B. Can Ergul <69393916+bcanergul06@users.noreply.github.com>
2022-11-14 02:06:36 +0300

    consensus: Added logging to time control of round 2 (#1253)
    * node: Adding logging to server.run function
    
    * fix
    
    * fix
    
    * fix
    
    * Restyled by autopep8
    
    * Restyled by black
    
    * Restyled by isort
    
    * Restyled by reorder-python-imports
    
    * Restyled by yapf
    
    * consensus: Fixed and cleaned round time calculator
    
    * consensus: Adding logging to time control of round 2
    
    * Update time_difference_main.py
    
    * Restyled by autopep8
    
    Co-authored-by: Onur Atakan ULUSOY <atadogan06@gmail.com>
    Co-authored-by: Restyled.io <commits@restyled.io>

--- decentra_network/consensus/rounds/round_2/checks/time/time_difference/time_difference_main.py
+++ decentra_network/consensus/rounds/round_2/checks/time/time_difference/time_difference_main.py
@@ -15,7 +15,13 @@ logger = get_logger("CONSENSUS_SECOND_ROUND")
 def time_difference_check(block: Block) -> bool:
 
     true_time = block.round_2_starting_time + block.round_2_time
-    if int(time.time()) >= true_time:
+    current_time = int(time.time())
+    logger.info("Round 2 time control started")
+    logger.debug(f"current_time: {current_time}")
+    logger.debug(f"true_time: {true_time}")
+    if current_time >= true_time:
+        logger.info("Time is true")
         return True
     else:
+        logger.info("Time is not true")
         return False
