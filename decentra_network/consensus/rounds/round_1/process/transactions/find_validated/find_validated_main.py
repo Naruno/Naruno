0dcaf75c298a2729672d3e14d313a9ed15537736
Restyled.io <commits@restyled.io>
2022-11-14 18:10:15 +0000

    Restyled by black

--- decentra_network/consensus/rounds/round_1/process/transactions/find_validated/find_validated_main.py
+++ decentra_network/consensus/rounds/round_1/process/transactions/find_validated/find_validated_main.py
@@ -15,24 +15,24 @@ from decentra_network.blockchain.block.blocks_hash import SaveBlockshash
 from decentra_network.blockchain.block.blocks_hash import SaveBlockshash_part
 from decentra_network.blockchain.block.hash.calculate_hash import CalculateHash
 from decentra_network.blockchain.block.save_block import SaveBlock
-from decentra_network.blockchain.candidate_block.candidate_block_main import \
-    candidate_block
-from decentra_network.consensus.rounds.round_1.checks.checks_main import \
-    round_check
+from decentra_network.blockchain.candidate_block.candidate_block_main import (
+    candidate_block,
+)
+from decentra_network.consensus.rounds.round_1.checks.checks_main import round_check
 from decentra_network.lib.log import get_logger
 from decentra_network.node.get_candidate_blocks import GetCandidateBlocks
 from decentra_network.node.server.server import server
 from decentra_network.node.unl import Unl
 from decentra_network.transactions.get_transaction import GetTransaction
-from decentra_network.transactions.process_the_transaction import \
-    ProccesstheTransaction
+from decentra_network.transactions.process_the_transaction import ProccesstheTransaction
 
 logger = get_logger("CONSENSUS_FIRST_ROUND")
 logger.info("Finding process of validating list is started.")
 
 
-def find_validated(block: Block, candidate_class: candidate_block,
-                   unl_nodes: dict) -> list:
+def find_validated(
+    block: Block, candidate_class: candidate_block, unl_nodes: dict
+) -> list:
     temp_validating_list = []
     logger.debug(f"First temp_validating_list: {temp_validating_list}")
     for candidate_block in candidate_class.candidate_blocks[:]:
@@ -54,20 +54,17 @@ def find_validated(block: Block, candidate_class: candidate_block,
             else:
                 tx_valid += 1
 
-            logger.debug(
-                f"Tx valid of {other_block_tx.signature} : {tx_valid}")
+            logger.debug(f"Tx valid of {other_block_tx.signature} : {tx_valid}")
             if tx_valid > (len(unl_nodes) / 2):
 
                 already_in_ok = False
                 for alrady_tx in temp_validating_list[:]:
 
                     if other_block_tx.signature == alrady_tx.signature:
-                        logger.warning(
-                            "The transaction is already in the list")
+                        logger.warning("The transaction is already in the list")
                         already_in_ok = True
                 if not already_in_ok:
-                    logger.debug(
-                        f"Transaction is valid ({other_block_tx.signature})")
+                    logger.debug(f"Transaction is valid ({other_block_tx.signature})")
                     temp_validating_list.append(other_block_tx)
     logger.debug(f"First temp_validating_list: {temp_validating_list}")
     return temp_validating_list
