20cb8bd9aa285a6120c18d2db0fcdcd119b07122
Restyled.io <commits@restyled.io>
2022-11-14 17:35:31 +0000

    Restyled by autopep8

--- decentra_network/consensus/rounds/round_1/process/transactions/checks/duplicated.py
+++ decentra_network/consensus/rounds/round_1/process/transactions/checks/duplicated.py
@@ -21,7 +21,8 @@ def Remove_Duplicates(block: Block):
     for tx in block.validating_list:
         if not any(tx.signature == tx2.signature
                    for tx2 in new_validating_list):
-            logger.info(f"tx: {tx} will be removed because its added more than one")
+            logger.info(
+                f"tx: {tx} will be removed because its added more than one")
             new_validating_list.append(tx)
     block.validating_list = new_validating_list
     logger.debug(f"End block.validatin_list: {block.validating_list}")
