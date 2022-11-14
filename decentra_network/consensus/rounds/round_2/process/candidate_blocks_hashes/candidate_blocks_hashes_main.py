0fb3b068a19e7f45d0d130e2cfe4f9075bf5e705
Restyled.io <commits@restyled.io>
2022-11-14 18:19:25 +0000

    Restyled by yapf

--- decentra_network/consensus/rounds/round_2/process/candidate_blocks_hashes/candidate_blocks_hashes_main.py
+++ decentra_network/consensus/rounds/round_2/process/candidate_blocks_hashes/candidate_blocks_hashes_main.py
@@ -18,9 +18,9 @@ from decentra_network.lib.log import get_logger
 logger = get_logger("CONSENSUS_SECOND_ROUND")
 
 
-def process_candidate_blocks_hashes(
-    block: Block, candidate_class: candidate_block, unl_nodes: dict
-) -> dict:
+def process_candidate_blocks_hashes(block: Block,
+                                    candidate_class: candidate_block,
+                                    unl_nodes: dict) -> dict:
     logger.info("Control process of candidate block hashes is started.")
     for candidate_block_hash in candidate_class.candidate_block_hashes[:]:
         logger.debug(f"Candidate block hash {candidate_block_hash}")
@@ -28,18 +28,18 @@ def process_candidate_blocks_hashes(
         tx_valid = 1
 
         for other_block in candidate_class.candidate_block_hashes[:]:
-            if (
-                candidate_block_hash != other_block
-                and candidate_block_hash["hash"] == other_block["hash"]
-            ):
+            if (candidate_block_hash != other_block
+                    and candidate_block_hash["hash"] == other_block["hash"]):
                 tx_valid += 1
                 logger.debug(
                     f"candidate_block_hash: {candidate_block_hash} validated from {other_block}"
                 )
         logger.debug(f"Hash valid of  {candidate_block_hash} : {tx_valid}")
-        logger.info(f"candidate_block_hash: {candidate_block_hash} is validated.")
+        logger.info(
+            f"candidate_block_hash: {candidate_block_hash} is validated.")
         if tx_valid >= ((len(unl_nodes) * 80) / 100):
-            logger.info(f"candidate_block_hash: {candidate_block_hash} is validated.")
+            logger.info(
+                f"candidate_block_hash: {candidate_block_hash} is validated.")
             return candidate_block_hash
     logger.warning("All candidate_block_hashes can not be validated.")
     return {"hash": False}
