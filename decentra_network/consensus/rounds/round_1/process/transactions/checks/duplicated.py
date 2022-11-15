7c96307d703bb3702c45151b8c42081aa0b7c02c
Ali eren <alierentabak0156@gmail.com>
2022-11-15 09:16:51 +0300

    consensus: Added logging to remove duplicates of round 1 (#1275)
    * consensus: Added logging to remove duplicates of round 1
    
    * Update duplicated.py
    
    * Restyled by autopep8
    
    * Restyled by black
    
    * Restyled by yapf
    
    Co-authored-by: Onur Atakan ULUSOY <atadogan06@gmail.com>
    Co-authored-by: Restyled.io <commits@restyled.io>

--- decentra_network/consensus/rounds/round_1/process/transactions/checks/duplicated.py
+++ decentra_network/consensus/rounds/round_1/process/transactions/checks/duplicated.py
@@ -14,11 +14,16 @@ def Remove_Duplicates(block: Block):
     """
     Remove duplicate transactions
     """
+    logger.info("Removing dublicated transaction is started")
+    logger.debug(f"First block.validatin_list: {block.validating_list}")
+
     new_validating_list = []
     for tx in block.validating_list:
         if not any(tx.signature == tx2.signature
                    for tx2 in new_validating_list):
+            logger.info(
+                f"tx: {tx} will be removed because its added more than one")
             new_validating_list.append(tx)
     block.validating_list = new_validating_list
-
+    logger.debug(f"End block.validatin_list: {block.validating_list}")
     return block
