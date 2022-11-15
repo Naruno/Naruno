9a011f3af2533b8a276f97cb795b78dbaa8b43ef
Onur Atakan ULUSOY <atadogan06@gmail.com>
2022-11-15 22:36:09 +0300

    consensus: Added logging to candidate block control of round 1

--- decentra_network/consensus/rounds/round_1/checks/candidate_blocks/candidate_blocks_main.py
+++ decentra_network/consensus/rounds/round_1/checks/candidate_blocks/candidate_blocks_main.py
@@ -13,7 +13,14 @@ logger = get_logger("CONSENSUS_FIRST_ROUND")
 
 def candidate_blocks_check(candidate_class: candidate_block,
                            unl_nodes: dict) -> bool:
+    logger.info("Candidate block control started for round 2")
+    logger.debug(
+        f"len(candidate_class.candidate_blocks): {len(candidate_class.candidate_blocks)}"
+    )
+    logger.debug(f"len(unl_nodes): {len(unl_nodes)}")
     if len(candidate_class.candidate_blocks) >= ((len(unl_nodes) * 80) / 100):
+        logger.info("Candidate block number is True")  
         return True
     else:
+        logger.info("Candidate block number is not True")
         return False
