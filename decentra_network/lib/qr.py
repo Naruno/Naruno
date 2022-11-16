6ae0cee54bed5aae4cf084acef9460a53d9dec05
Restyled.io <commits@restyled.io>
2022-11-16 23:01:24 +0000

    Restyled by autopep8

--- decentra_network/lib/qr.py
+++ decentra_network/lib/qr.py
@@ -21,23 +21,21 @@ def qr(data):
     logger.info("Qr code generator is started")
     logger.debug(f"data: {data}")
 
-
-
     main_folder = get_config()["main_folder"]
     qr = qrcode.QRCode(error_correction=qrcode.constants.ERROR_CORRECT_H)
-    
+
     qr.add_data(data)
-    qr.make(fit = True)
+    qr.make(fit=True)
 
     icon = f"{main_folder}/gui_lib/images/logo_w_bc.png"
     logger.debug(icon)
 
     qr_img = qr.make_image(
-        image_factory=StyledPilImage, 
+        image_factory=StyledPilImage,
         embeded_image_path=icon,
         color_mask=SolidFillColorMask(front_color=(94, 194, 149))
-        )
-    
+    )
+
     location = f"{main_folder}/{QR_CORE_PATH}{data}.png"
     logger.info(f"location: {location}")
     qr_img.save(location)
