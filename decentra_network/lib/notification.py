571557d7197977586bbfc4e4b8dad85ba3201285
Onur Atakan ULUSOY <atadogan06@gmail.com>
2022-11-17 01:51:46 +0300

    fix

--- decentra_network/lib/notification.py
+++ decentra_network/lib/notification.py
@@ -4,20 +4,29 @@
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at https://mozilla.org/MPL/2.0/.
-import os
+
 from plyer import notification as plyer_notification
 from decentra_network.lib.config_system import get_config
 from decentra_network.lib.log import get_logger
 
 
+from plyer.utils import platform
+
 logger = get_logger("LIB")
 
 def notification(title, message):
     logger.info("Notification system is started")
+    app_name = "Decentra Network"
+    timeout = 10
+    logger.debug(f"app_name: {app_name}")
+    logger.debug(f"timeout: {timeout}")
     logger.debug(f"title: {title}")
     logger.debug(f"message: {message}")
-    os.chdir(get_config()["main_folder"])
-    icon = "/gui_lib/images/logo.ico"
-    logger.debug(f"icon: {icon}")
-    plyer_notification.notify(title=title, message=message, app_icon=icon)
+    main_folder = get_config()["main_folder"]
+    if platform == "win":
+        icon = f"{main_folder}/gui_lib/images/logo_win.ico"
+    else:
+        icon = f"{main_folder}/gui_lib/images/logo.png"
 
+    logger.debug(f"icon: {icon}")
+    plyer_notification.notify(title=title, message=message, app_icon=icon, app_name=app_name, timeout=timeout)
