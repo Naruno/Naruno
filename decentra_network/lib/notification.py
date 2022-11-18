ff3a0c802a2f6aeb618b4fa90d131130d50c321d
Onur Atakan ULUSOY <atadogan06@gmail.com>
2022-11-18 19:33:41 +0300

    lib: Added try except ImportError to notification function

--- decentra_network/lib/notification.py
+++ decentra_network/lib/notification.py
@@ -4,8 +4,7 @@
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at https://mozilla.org/MPL/2.0/.
-from plyer import notification as plyer_notification
-from plyer.utils import platform
+
 
 from decentra_network.lib.config_system import get_config
 from decentra_network.lib.log import get_logger
@@ -14,22 +13,27 @@ logger = get_logger("LIB")
 
 
 def notification(title, message):
-    logger.info("Notification system is started")
-    app_name = "Decentra Network"
-    timeout = 10
-    logger.debug(f"app_name: {app_name}")
-    logger.debug(f"timeout: {timeout}")
-    logger.debug(f"title: {title}")
-    logger.debug(f"message: {message}")
-    main_folder = get_config()["main_folder"]
-    if platform == "win":
-        icon = f"{main_folder}/gui_lib/images/logo_win.ico"
-    else:
-        icon = f"{main_folder}/gui_lib/images/logo.png"
+    try:
+        from plyer import notification as plyer_notification
+        from plyer.utils import platform        
+        logger.info("Notification system is started")
+        app_name = "Decentra Network"
+        timeout = 10
+        logger.debug(f"app_name: {app_name}")
+        logger.debug(f"timeout: {timeout}")
+        logger.debug(f"title: {title}")
+        logger.debug(f"message: {message}")
+        main_folder = get_config()["main_folder"]
+        if platform == "win":
+            icon = f"{main_folder}/gui_lib/images/logo_win.ico"
+        else:
+            icon = f"{main_folder}/gui_lib/images/logo.png"
 
-    logger.debug(f"icon: {icon}")
-    plyer_notification.notify(title=title,
-                              message=message,
-                              app_icon=icon,
-                              app_name=app_name,
-                              timeout=timeout)
+        logger.debug(f"icon: {icon}")
+        plyer_notification.notify(title=title,
+                                  message=message,
+                                  app_icon=icon,
+                                  app_name=app_name,
+                                  timeout=timeout)
+    except ImportError:
+        logger.info("Passing notification system (no plyer)")
