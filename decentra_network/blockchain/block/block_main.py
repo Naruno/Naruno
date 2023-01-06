5a9cd2752c7b5efc59874b49b8ff958787347093
Onur Atakan ULUSOY <atadogan06@gmail.com>
2023-01-07 02:01:00 +0300

    Revert "blockchain: Removed candidate block resetting (#1387)"
    This reverts commit f0c32fb354ef372bea4a0c91343ba279c6efedad.

--- decentra_network/blockchain/block/block_main.py
+++ decentra_network/blockchain/block/block_main.py
@@ -102,6 +102,13 @@ class Block:
         self.validated = False
         self.validated_time = None
 
+        # Resetting the node candidate blocks.
+        nodes = (Unl.get_as_node_type(Unl.get_unl_nodes())
+                 if custom_nodes is None else custom_nodes)
+        for node in nodes:
+            node.candidate_block = None
+            node.candidate_block_hash = None
+
         if len(self.validating_list) >= (self.max_tx_number / 2):
             block2 = copy.copy(self)
             # Resetting and setting the new elements.
