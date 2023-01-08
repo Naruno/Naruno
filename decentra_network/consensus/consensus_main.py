d411888dd80f89146c62b0fe89b6e02085f68011
Onur Atakan ULUSOY <atadogan06@gmail.com>
2023-01-08 14:59:26 +0300

    Update consensus_main.py

--- decentra_network/consensus/consensus_main.py
+++ decentra_network/consensus/consensus_main.py
@@ -60,15 +60,14 @@ def consensus_trigger(
     custom_TEMP_ACCOUNTS_PATH: str = None,
     custom_TEMP_BLOCKSHASH_PATH: str = None,
     custom_TEMP_BLOCKSHASH_PART_PATH: str = None,        
-    ):  
-        if not block.round_1:
-            custom_server.send_my_block(
-                block) if custom_server is not None else server.Server.send_my_block(
-                    block)
-        elif not block.round_2:
-            logger.debug("Our block hash is sending to the unl nodes")
-            the_server = server.Server if custom_server is None else custom_server
-            the_server.send_my_block_hash(block)
+    ):
+        custom_server.send_my_block(
+            block) if custom_server is not None else server.Server.send_my_block(
+                block)
+
+        logger.debug("Our block hash is sending to the unl nodes")
+        the_server = server.Server if custom_server is None else custom_server
+        the_server.send_my_block_hash(block)
 
 
     if block.validated:
