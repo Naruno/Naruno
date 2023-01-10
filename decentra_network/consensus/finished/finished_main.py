fdd0b03dcc0fb26252b4fce5837205967d9de92a
Onur Atakan ULUSOY <atadogan06@gmail.com>
2023-01-10 09:03:46 +0000

    fix

--- decentra_network/consensus/finished/finished_main.py
+++ decentra_network/consensus/finished/finished_main.py
@@ -112,7 +112,7 @@ def finished_main(
             if len(the_blocks_hash) == block.part_amount:
                 block.sync_empty_blocks()
                 block.empty_block_number += block.gap_block_number
-                difference = (block.start_time +  (block.hard_block_number * block.block.block_time)) - int(time.time())
+                difference = (block.start_time +  (block.hard_block_number * block.block_time)) - int(time.time())
                 time.sleep(difference)
                 block.sync = True
                 SaveBlockshash_part(
