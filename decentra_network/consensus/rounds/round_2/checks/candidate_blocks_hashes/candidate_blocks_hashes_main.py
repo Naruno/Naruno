e61dd68e60b8677ac97eba41c44475b532813a59
Onur Atakan ULUSOY <atadogan06@gmail.com>
2023-01-10 06:24:29 +0300

    Update candidate_blocks_hashes_main.py

--- decentra_network/consensus/rounds/round_2/checks/candidate_blocks_hashes/candidate_blocks_hashes_main.py
+++ decentra_network/consensus/rounds/round_2/checks/candidate_blocks_hashes/candidate_blocks_hashes_main.py
@@ -18,8 +18,8 @@ def candidate_blocks_hashes_check(candidate_class: candidate_block,
         f"len(candidate_class.candidate_block_hashes): {len(candidate_class.candidate_block_hashes)}"
     )
     logger.debug(f"len(unl_nodes): {len(unl_nodes)}")
-    if len(candidate_class.candidate_block_hashes) > ((
-        (len(unl_nodes) + 1) * 50) / 100):
+    if len(candidate_class.candidate_block_hashes) >= ((
+        (len(unl_nodes) + 1) * 80) / 100):
         logger.info("Candidate block hash number is True")
         return True
     else:
