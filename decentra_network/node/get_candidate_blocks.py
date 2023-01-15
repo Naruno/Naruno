7fb767657ba583126497aa5f580f1a24e8fa5eb8
Onur Atakan ULUSOY <atadogan06@gmail.com>
2023-01-15 15:54:40 +0000

    fix

--- decentra_network/node/get_candidate_blocks.py
+++ decentra_network/node/get_candidate_blocks.py
@@ -27,19 +27,19 @@ def GetCandidateBlocks(custom_nodes_list=None, block: Block = None):
     for node in nodes:
         if node.candidate_block is not None:
 
-            if node.candidate_block["sequance_number"] == block.sequance_number:
+            if node.candidate_block["sequence_number"] == block.sequance_number:
                 the_candidate_blocks.append(node.candidate_block)
             else:
                 for i in node.candidate_block_history:
-                    if i["sequance_number"] == block.sequance_number:
+                    if i["sequence_number"] == block.sequance_number:
                         the_candidate_blocks.append(i)
         if node.candidate_block_hash is not None:
 
-            if node.candidate_block_hash["sequance_number"] == block.sequance_number:
+            if node.candidate_block_hash["sequence_number"] == block.sequance_number:
                 the_candidate_block_hashes.append(node.candidate_block_hash)
             else:
                 for i in node.candidate_block_hash_history:
-                    if i["sequance_number"] == block.sequance_number:
+                    if i["sequence_number"] == block.sequance_number:
                         the_candidate_block_hashes.append(i)
 
     if block is not None:
@@ -56,6 +56,7 @@ def GetCandidateBlocks(custom_nodes_list=None, block: Block = None):
             "action": "myblock",
             "transaction": new_list,
             "signature": a_time,
+            "sequance_number": block.sequance_number,
         })
 
         the_candidate_block_hashes.append({
@@ -63,6 +64,7 @@ def GetCandidateBlocks(custom_nodes_list=None, block: Block = None):
             "hash": block.hash,
             "previous_hash": block.previous_hash,
             "signature": a_time,
+            "sequance_number": block.sequance_number,
         })
 
     not_none_the_candidate_blocks = []
