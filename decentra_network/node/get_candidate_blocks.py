3bc66c22d7f61bfec442f41400772f648f86cdc0
Onur Atakan ULUSOY <atadogan06@gmail.com>
2023-01-16 06:26:05 +0000

    fix

--- decentra_network/node/get_candidate_blocks.py
+++ decentra_network/node/get_candidate_blocks.py
@@ -26,21 +26,25 @@ def GetCandidateBlocks(custom_nodes_list=None, block: Block = None):
 
     for node in nodes:
         if node.candidate_block is not None:
-
-            if int(node.candidate_block["sequence_number"]) == block.sequance_number:
-                the_candidate_blocks.append(node.candidate_block)
+            if node.candidate_block is None:
+                continue
             else:
-                for i in node.candidate_block_history:
-                    if i["sequence_number"] == block.sequance_number:
-                        the_candidate_blocks.append(i)
+                if int(node.candidate_block["sequence_number"]) == block.sequance_number:
+                    the_candidate_blocks.append(node.candidate_block)
+                else:
+                    for i in node.candidate_block_history:
+                        if i["sequence_number"] == block.sequance_number:
+                            the_candidate_blocks.append(i)
         if node.candidate_block_hash is not None:
-
-            if int(node.candidate_block_hash["sequence_number"]) == block.sequance_number:
-                the_candidate_block_hashes.append(node.candidate_block_hash)
+            if node.candidate_block_hash is None:
+                continue
             else:
-                for i in node.candidate_block_hash_history:
-                    if i["sequence_number"] == block.sequance_number:
-                        the_candidate_block_hashes.append(i)
+                if int(node.candidate_block_hash["sequence_number"]) == block.sequance_number:
+                    the_candidate_block_hashes.append(node.candidate_block_hash)
+                else:
+                    for i in node.candidate_block_hash_history:
+                        if i["sequence_number"] == block.sequance_number:
+                            the_candidate_block_hashes.append(i)
 
     if block is not None:
         new_list = []
