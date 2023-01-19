2242d2e0d0ab23dc4c4992e39dbd37a4a80d1512
Restyled.io <commits@restyled.io>
2023-01-19 11:25:26 +0000

    Restyled by autopep8

--- decentra_network/gui/main.py
+++ decentra_network/gui/main.py
@@ -71,7 +71,8 @@ class GUI(MDApp):
         """
 
         self.theme_cls.primary_palette = "Green"
-        FONT_PATH = os.path.join(os.environ['DECENTRA_ROOT'],"gui_lib", "fonts")
+        FONT_PATH = os.path.join(
+            os.environ['DECENTRA_ROOT'], "gui_lib", "fonts")
 
         self.theme_cls.font_styles.update({
             "H1": [os.path.join(FONT_PATH, "Poppins-Light"), 96, False, -1.5],
@@ -100,8 +101,7 @@ def arguments():
     """
 
     parser = argparse.ArgumentParser(
-        description=
-        "This is an open source decentralized application network. In this network, you can develop and publish decentralized applications. Use the menu (-m) or GUI to gain full control and use the node, operation, etc."
+        description="This is an open source decentralized application network. In this network, you can develop and publish decentralized applications. Use the menu (-m) or GUI to gain full control and use the node, operation, etc."
     )
 
     parser.add_argument(
