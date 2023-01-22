8eb538101f66a90654c865957c80dfd1aa3c0e89
Onur Atakan ULUSOY <atadogan06@gmail.com>
2023-01-22 14:20:24 +0300

    gui: Added ability to sign and verify data (#1429)
    * gui: Added ability to sign and verify data
    
    * Restyled by autopep8
    
    * Restyled by black
    
    * Restyled by isort
    
    * Restyled by yapf
    
    Co-authored-by: Restyled.io <commits@restyled.io>

--- decentra_network/gui_lib/libs/baseclass/operations_screen.py
+++ decentra_network/gui_lib/libs/baseclass/operations_screen.py
@@ -15,6 +15,8 @@ from decentra_network.blockchain.block.save_block import SaveBlock
 from decentra_network.config import MY_TRANSACTION_EXPORT_PATH
 from decentra_network.lib.export import export_the_transactions
 from decentra_network.lib.settings_system import the_settings
+from decentra_network.lib.sign import sign
+from decentra_network.lib.verify import verify
 from decentra_network.transactions.my_transactions.get_my_transaction import \
     GetMyTransaction
 from decentra_network.transactions.my_transactions.save_to_my_transaction import \
@@ -31,9 +33,19 @@ class Send_Coin_Box(MDGridLayout):
     cols = 2
 
 
+class Sign_Box(MDGridLayout):
+    cols = 2
+
+
+class Verify_Box(MDGridLayout):
+    cols = 2
+
+
 class OperationBox(MDGridLayout):
     cols = 2
     send_coin_dialog = None
+    sign_dialog = None
+    verify_dialog = None
     export_transaction_csv_dialog = None
     FONT_PATH = f"{os.environ['DECENTRA_ROOT']}/gui_lib/fonts/"
 
@@ -62,9 +74,79 @@ class OperationBox(MDGridLayout):
 
         self.send_coin_dialog.open()
 
+    def show_sign_dialog(self):
+        if not self.sign_dialog:
+            self.sign_dialog = SweetAlert(
+                title="Sign Data",
+                type="custom",
+                auto_dismiss=False,
+                content_cls=Sign_Box(),
+                buttons=[
+                    MDFlatButton(
+                        text="CANCEL",
+                        on_press=self.dismiss_sign_dialog,
+                        font_size="18sp",
+                        font_name=f"{self.FONT_PATH}Poppins-Bold",
+                    ),
+                    MDFlatButton(
+                        text="OK",
+                        on_press=self.sign_the_data,
+                        font_size="18sp",
+                        font_name=f"{self.FONT_PATH}Poppins-Bold",
+                    ),
+                ],
+            )
+
+        self.sign_dialog.open()
+
+    def show_verify_dialog(self):
+        if not self.verify_dialog:
+            self.verify_dialog = SweetAlert(
+                title="Verify Signed Data",
+                type="custom",
+                auto_dismiss=False,
+                content_cls=Verify_Box(),
+                buttons=[
+                    MDFlatButton(
+                        text="CANCEL",
+                        on_press=self.dismiss_verify_dialog,
+                        font_size="18sp",
+                        font_name=f"{self.FONT_PATH}Poppins-Bold",
+                    ),
+                    MDFlatButton(
+                        text="OK",
+                        on_press=self.verify_the_data,
+                        font_size="18sp",
+                        font_name=f"{self.FONT_PATH}Poppins-Bold",
+                    ),
+                ],
+            )
+
+        self.verify_dialog.open()
+
     def get_send_coin_dialog_text(self):
         text_list = []
         for obj in self.send_coin_dialog.content_cls.children:
+            for sub_obj in obj.children:
+                text_list.append(sub_obj.text)
+
+                sub_obj.text = ""
+
+        return text_list
+
+    def get_sign_dialog_text(self):
+        text_list = []
+        for obj in self.sign_dialog.content_cls.children:
+            for sub_obj in obj.children:
+                text_list.append(sub_obj.text)
+
+                sub_obj.text = ""
+
+        return text_list
+
+    def get_verify_dialog_text(self):
+        text_list = []
+        for obj in self.verify_dialog.content_cls.children:
             for sub_obj in obj.children:
                 text_list.append(sub_obj.text)
 
@@ -114,10 +196,58 @@ class OperationBox(MDGridLayout):
 
         self.send_coin_dialog.dismiss()
 
+    def sign_the_data(self, widget):
+
+        text_list = self.get_sign_dialog_text()
+
+        data = text_list[1]
+        path = sign(data, text_list[0])
+
+        Clipboard.copy(path)
+
+        SweetAlert().fire(
+            f"Signed data file created in {path}, The file has been copied to your clipboard.",
+            type="success",
+        )
+
+        del text_list
+
+        self.sign_dialog.dismiss()
+
+    def verify_the_data(self, widget):
+
+        text_list = self.get_verify_dialog_text()
+
+        path = text_list[0]
+        result = verify(path)
+
+        if result == True:
+            SweetAlert().fire(
+                "Data is verified",
+                type="success",
+            )
+        else:
+            SweetAlert().fire(
+                "Data is not verified",
+                type="failure",
+            )
+
+        self.verify_dialog.dismiss()
+
     def dismiss_send_coin_dialog(self, widget):
         self.get_send_coin_dialog_text()
 
         self.send_coin_dialog.dismiss()
+
+    def dismiss_sign_dialog(self, widget):
+        self.get_sign_dialog_text()
+
+        self.sign_dialog.dismiss()
+
+    def dismiss_verify_dialog(self, widget):
+        self.get_verify_dialog_text()
+
+        self.verify_dialog.dismiss()
 
     def send_coin(self):
         try:
@@ -129,6 +259,12 @@ class OperationBox(MDGridLayout):
             )
             return False
         self.show_send_coin_dialog()
+
+    def sign(self):
+        self.show_sign_dialog()
+
+    def verify(self):
+        self.show_verify_dialog()
 
     def export_transaction_csv(self):
         if export_the_transactions():
