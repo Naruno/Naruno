fa1ab81108223a0bda69af2c14528bf04f91144d
Restyled.io <commits@restyled.io>
2023-01-22 10:38:02 +0000

    Restyled by autopep8

--- decentra_network/cli/main.py
+++ decentra_network/cli/main.py
@@ -59,8 +59,7 @@ def show_menu():
     print(
         banner_maker(
             sc_name="Decentra Network",
-            description=
-            "This is an open source decentralized application network. In this network, you can develop and publish decentralized applications.",
+            description="This is an open source decentralized application network. In this network, you can develop and publish decentralized applications.",
             author="Decentra Network Developers",
         ))
 
@@ -241,11 +240,8 @@ def menu():
         if choices_input == "verify":
             print(verify(input("Please write the signed data path: ")))
 
-
-
         input("Press enter to continue (screen will be cleaned)...")
         os.system("cls" if os.name == "nt" else "clear")
-
 
         if choices_input == "0":
             exit()
@@ -257,8 +253,7 @@ def arguments():
     """
 
     parser = argparse.ArgumentParser(
-        description=
-        "This is an open source decentralized application network. In this network, you can develop and publish decentralized applications. Use the menu (-m) or GUI to gain full control and use the node, operation, etc."
+        description="This is an open source decentralized application network. In this network, you can develop and publish decentralized applications. Use the menu (-m) or GUI to gain full control and use the node, operation, etc."
     )
 
     parser.add_argument("-pw",
