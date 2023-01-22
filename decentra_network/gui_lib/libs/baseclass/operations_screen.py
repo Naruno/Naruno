ee708ccb6872eca92068d3246500618663eef160
Restyled.io <commits@restyled.io>
2023-01-22 11:19:26 +0000

    Restyled by yapf

--- decentra_network/gui_lib/libs/baseclass/operations_screen.py
+++ decentra_network/gui_lib/libs/baseclass/operations_screen.py
@@ -163,10 +163,8 @@ class OperationBox(MDGridLayout):
         data = text_list[1]
 
         if float(amount) >= the_block.minumum_transfer_amount:
-            if (
-                wallet_import(int(the_settings()["wallet"]), 2)
-                == sha256(text_list[0].encode("utf-8")).hexdigest()
-            ):
+            if (wallet_import(int(the_settings()["wallet"]), 2) == sha256(
+                    text_list[0].encode("utf-8")).hexdigest()):
                 block = the_block
                 send_tx = send(
                     text_list[0],
@@ -289,16 +287,16 @@ class OperationBox(MDGridLayout):
         if len(transactions) != 0:
             bottom_sheet_menu = MDListBottomSheet(radius=25, radius_from="top")
             data = {
-                tx[
-                    0
-                ]: f"{tx[0].toUser} | {str(tx[0].amount)} | {str(tx[0].transaction_fee)} | {str(tx[1])}"
+                tx[0]:
+                f"{tx[0].toUser} | {str(tx[0].amount)} | {str(tx[0].transaction_fee)} | {str(tx[1])}"
                 for tx in transactions
             }
 
             for item in data.items():
                 bottom_sheet_menu.add_item(
                     item[1],
-                    lambda x, y=item[0]: self.callback_for_transaction_history_items(y),
+                    lambda x, y=item[0]: self.
+                    callback_for_transaction_history_items(y),
                 )
             bottom_sheet_menu.open()
         else:
