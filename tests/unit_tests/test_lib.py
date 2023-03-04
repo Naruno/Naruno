3a9e88325145ef5afa0e3e69fb54d0c505bb5c1b
Onur Atakan ULUSOY <atadogan06@gmail.com>
2023-03-04 03:48:35 +0300

    lib: Added publisher mode setting (#1469)
    * lib: Added publisher mode setting
    
    * Restyled by autopep8
    
    * Restyled by black
    
    * Restyled by isort
    
    * Restyled by yapf
    
    ---------
    
    Co-authored-by: Restyled.io <commits@restyled.io>

--- tests/unit_tests/test_lib.py
+++ tests/unit_tests/test_lib.py
@@ -9,49 +9,43 @@ import os
 import sys
 import time
 
-
-
 sys.path.append(os.path.join(os.path.dirname(__file__), "..", ".."))
 import unittest
 from unittest import mock
 
 from naruno.blockchain.block.block_main import Block
-from naruno.config import (
-    CONNECTED_NODES_PATH, LOADING_ACCOUNTS_PATH, LOADING_BLOCK_PATH,
-    LOADING_BLOCKSHASH_PART_PATH, LOADING_BLOCKSHASH_PATH,
-    MY_TRANSACTION_EXPORT_PATH, PENDING_TRANSACTIONS_PATH, TEMP_ACCOUNTS_PATH,
-    TEMP_BLOCK_PATH, TEMP_BLOCKSHASH_PART_PATH, TEMP_BLOCKSHASH_PATH,
-    UNL_NODES_PATH)
+from naruno.config import (CONNECTED_NODES_PATH, LOADING_ACCOUNTS_PATH,
+                           LOADING_BLOCK_PATH, LOADING_BLOCKSHASH_PART_PATH,
+                           LOADING_BLOCKSHASH_PATH, MY_TRANSACTION_EXPORT_PATH,
+                           PENDING_TRANSACTIONS_PATH, TEMP_ACCOUNTS_PATH,
+                           TEMP_BLOCK_PATH, TEMP_BLOCKSHASH_PART_PATH,
+                           TEMP_BLOCKSHASH_PATH, UNL_NODES_PATH)
 from naruno.lib.backup.naruno_export import naruno_export
 from naruno.lib.backup.naruno_import import naruno_import
 from naruno.lib.clean_up import CleanUp_tests
 from naruno.lib.config_system import get_config
 from naruno.lib.export import export_the_transactions
 from naruno.lib.mix.mixlib import (banner_maker, ended_text_centered,
-                                             menu_maker, menu_seperator,
-                                             menu_space, menu_title,
-                                             printcentertext, question_maker,
-                                             quit_menu_maker,
-                                             starting_text_centered)
+                                   menu_maker, menu_seperator, menu_space,
+                                   menu_title, printcentertext, question_maker,
+                                   quit_menu_maker, starting_text_centered)
 from naruno.lib.notification import notification
 from naruno.lib.performance_analyzers.heartbeat_db import \
     heartbeat_generic_db_analyzer
 from naruno.lib.perpetualtimer import perpetualTimer
 from naruno.lib.safety import safety_check
-from naruno.lib.settings_system import (d_mode_settings,
-                                                  dark_mode_settings,
-                                                  mt_settings, save_settings,
-                                                  t_mode_settings,
-                                                  the_settings)
+from naruno.lib.settings_system import (d_mode_settings, dark_mode_settings,
+                                        mt_settings, publisher_mode_settings,
+                                        save_settings, t_mode_settings,
+                                        the_settings)
+from naruno.lib.sign import sign
 from naruno.lib.status import Status
+from naruno.lib.verify import verify
 from naruno.node.server.server import server
 from naruno.node.unl import Unl
 from naruno.transactions.transaction import Transaction
-
-from naruno.lib.sign import sign
-from naruno.lib.verify import verify
-
 from naruno.wallet.wallet_import import Address, wallet_import
+
 
 def perpetual_time_test():
     os.chdir(get_config()["main_folder"])
@@ -627,8 +621,18 @@ class Test_Lib(unittest.TestCase):
 
         dark_mode_settings(temp_settings["dark_mode"])
 
-    def test_perpetualTimer_0(self):
+    def test_publisher_mode_settings(self):
+        temp_settings = the_settings()
+        changed_value = True if temp_settings[
+            "publisher_mode"] is False else False
+        publisher_mode_settings(changed_value)
+        new_settings = the_settings()
 
+        self.assertEqual(new_settings["publisher_mode"], changed_value)
+
+        publisher_mode_settings(temp_settings["publisher_mode"])
+
+    def test_perpetualTimer_0(self):
         the_timer = perpetualTimer(
             0,
             perpetual_time_test,
@@ -638,7 +642,6 @@ class Test_Lib(unittest.TestCase):
         the_timer.cancel()
 
     def test_perpetualTimer(self):
-
         the_timer = perpetualTimer(
             1,
             perpetual_time_test,
@@ -667,7 +670,6 @@ class Test_Lib(unittest.TestCase):
         notification("test", "trest", raise_plyer=True)
 
     def test_notification_mt_True(self):
-
         temp_settings = the_settings()
         changed_value = True
         mt_settings(changed_value)
@@ -677,7 +679,6 @@ class Test_Lib(unittest.TestCase):
         mt_settings(temp_settings["mute_notifications"])
 
     def test_notification_mt_False(self):
-
         temp_settings = the_settings()
         changed_value = False
         mt_settings(changed_value)
@@ -701,7 +702,6 @@ class Test_Lib(unittest.TestCase):
         naruno_import(backup)
         self.assertNotEqual(the_settings()["debug_mode"], changed_value)
 
-
     def test_sign_verify(self):
         signed = sign("Onur & Ali Eren", "123")
         result = verify(signed)
@@ -719,5 +719,6 @@ class Test_Lib(unittest.TestCase):
     def test_sign_verify_false_pass(self):
         signed = sign("Onur & Ali Eren", str(time.time()))
         self.assertEqual(signed, "None")
+
 
 unittest.main(exit=False)
