46aff716e748c949286144676e156a40c6a919e3
Onur Atakan ULUSOY <atadogan06@gmail.com>
2023-03-04 21:37:31 +0300

    blockchain: Added logging to hash calculation

--- naruno/blockchain/block/hash/calculate_hash.py
+++ naruno/blockchain/block/hash/calculate_hash.py
@@ -8,21 +8,27 @@ from naruno.blockchain.block.hash.accounts_hash import AccountsHash
 from naruno.blockchain.block.hash.blocks_hash import BlocksHash
 from naruno.blockchain.block.hash.tx_hash import TransactionsHash
 from naruno.lib.mix.merkle_root import MerkleTree
+from naruno.lib.log import get_logger
 
+logger = get_logger("BLOCKCHAIN")
 
 def CalculateHash(block, part_of_blocks_hash, the_blocks_hash, the_accounts):
     """
     Calculates and returns the hash of the block.
     """
+    logger.info(f"Calculating the hash of the block#{block.sequence_number}")
 
     # Transaction Hash
     tx_hash = TransactionsHash(block)
+    logger.debug(f"tx_hash: {tx_hash}")
 
     # Blocks Hash
     blocks_hash = BlocksHash(block, part_of_blocks_hash, the_blocks_hash)
+    logger.debug(f"blocks_hash: {blocks_hash}")
 
     # Account
     accounts_hash = AccountsHash(block, the_accounts)
+    logger.debug(f"accounts_hash: {accounts_hash}")
 
     # Other Elements
     main_list = [
@@ -53,6 +59,9 @@ def CalculateHash(block, part_of_blocks_hash, the_blocks_hash, the_accounts):
         str(block.fee_address),
     ]
 
+    logger.debug(f"main_list: {main_list}")
+
     the_hash = MerkleTree(main_list).getRootHash()
+    logger.debug(f"the_hash: {the_hash}")
 
     return the_hash
