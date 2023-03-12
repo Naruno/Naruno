2246dc2e2b67fa7982d55645216179367fe145de
Restyled.io <commits@restyled.io>
2023-03-12 01:09:20 +0000

    Restyled by autopep8

--- naruno/blockchain/block/hash/accounts_hash.py
+++ naruno/blockchain/block/hash/accounts_hash.py
@@ -12,7 +12,6 @@ def AccountsHash(block, the_accounts):
     Calculates and returns the hash of the accounts.
     """
 
-
     the_accounts.execute("SELECT * FROM account_list")
     the_list = the_accounts.fetchall()
 
