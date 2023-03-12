5f965ff3e841f4897ca6b6c97c9139f0c37f2b03
Onur Atakan ULUSOY <atadogan06@gmail.com>
2023-03-12 20:02:56 +0300

    blockchain: Fixed accounts hash function

--- naruno/blockchain/block/hash/accounts_hash.py
+++ naruno/blockchain/block/hash/accounts_hash.py
@@ -11,11 +11,17 @@ def AccountsHash(block, the_accounts):
     """
     Calculates and returns the hash of the accounts.
     """
+    the_list = []
+    if isinstance(the_accounts, list):
+        the_list = the_accounts
+        the_list = [account.get_hash()
+                               for account in the_list]
+    else:
+        the_accounts.execute("SELECT * FROM account_list")
+        the_list = the_accounts.fetchall()
+        the_list = [str(account[0])+str(account[1])+str(account[2])
+                               for account in the_list]
 
-    the_accounts.execute("SELECT * FROM account_list")
-    the_list = the_accounts.fetchall()
-
-    account_list = MerkleTree([str(account[0])+str(account[1])+str(account[2])
-                               for account in the_list]).getRootHash()
+    account_list = MerkleTree(the_list).getRootHash()
 
     return account_list
