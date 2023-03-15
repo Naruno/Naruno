f15441a5c7b7be981906ad0b0b0b91ec03051e90
Restyled.io <commits@restyled.io>
2023-03-15 07:50:17 +0000

    Restyled by yapf

--- naruno/transactions/my_transactions/save_to_my_transaction.py
+++ naruno/transactions/my_transactions/save_to_my_transaction.py
@@ -35,9 +35,8 @@ def SavetoMyTransaction(
         The list of the my transactions.
     """
 
-    currently_list = (
-        GetMyTransaction() if custom_currently_list is None else custom_currently_list
-    )
+    currently_list = (GetMyTransaction() if custom_currently_list is None else
+                      custom_currently_list)
     if not tx.signature == "NARUNO":
         new = True
 
@@ -48,12 +47,14 @@ def SavetoMyTransaction(
         if new:
             if not sended and validated:
                 notification(
-                    "Incoming TX", f"{tx.data}:{tx.amount} from {Address(tx.fromUser)}"
-                )
+                    "Incoming TX",
+                    f"{tx.data}:{tx.amount} from {Address(tx.fromUser)}")
             elif sended and not validated:
-                notification("Sended TX", f"{tx.data}:{tx.amount} to {tx.toUser}")
+                notification("Sended TX",
+                             f"{tx.data}:{tx.amount} to {tx.toUser}")
             elif sended and validated:
-                notification("Validated TX", f"{tx.data}:{tx.amount} to {tx.toUser}")
+                notification("Validated TX",
+                             f"{tx.data}:{tx.amount} to {tx.toUser}")
 
             tx_list = [tx, validated, sended]
             currently_list.append(tx_list)
