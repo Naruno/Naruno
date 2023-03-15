fa60367a729a62e88e432e7b9563ecdb5909154d
Restyled.io <commits@restyled.io>
2023-03-15 17:46:15 +0000

    Restyled by black

--- naruno/apps/remote_app.py
+++ naruno/apps/remote_app.py
@@ -16,12 +16,11 @@ from naruno.lib.config_system import get_config
 from naruno.lib.log import get_logger
 from naruno.lib.settings_system import baklava_settings
 from naruno.lib.settings_system import the_settings
-from naruno.transactions.my_transactions.save_to_my_transaction import \
-    SavetoMyTransaction
-from naruno.transactions.my_transactions.sended_transaction import \
-    SendedTransaction
-from naruno.transactions.my_transactions.validate_transaction import \
-    ValidateTransaction
+from naruno.transactions.my_transactions.save_to_my_transaction import (
+    SavetoMyTransaction,
+)
+from naruno.transactions.my_transactions.sended_transaction import SendedTransaction
+from naruno.transactions.my_transactions.validate_transaction import ValidateTransaction
 from naruno.transactions.transaction import Transaction
 from naruno.wallet.wallet_import import wallet_import
 
@@ -29,7 +28,6 @@ logger = get_logger("REMOTE_APP")
 
 
 class Integration:
-
     def __init__(
         self,
         app_name,
@@ -46,8 +44,9 @@ class Integration:
         :param password: The password of the wallet
         """
         self.app_name = app_name
-        self.cache_name = sha256(
-            self.app_name.encode()).hexdigest() + wallet_import(-1, 3)
+        self.cache_name = sha256(self.app_name.encode()).hexdigest() + wallet_import(
+            -1, 3
+        )
         self.host = host
         self.port = port
         self.password = password
@@ -76,8 +75,7 @@ class Integration:
         if not os.path.exists(f"db/remote_app_cache/{self.cache_name}.cache"):
             self.cache = []
             self.save_cache()
-        with open(f"db/remote_app_cache/{self.cache_name}.cache",
-                  "r") as cache:
+        with open(f"db/remote_app_cache/{self.cache_name}.cache", "r") as cache:
             self.cache = json.load(cache)
 
         self.backward_support_cache()
@@ -95,8 +93,7 @@ class Integration:
             return
         self.backward_support_cache()
         os.chdir(get_config()["main_folder"])
-        with open(f"db/remote_app_cache/{self.cache_name}.cache",
-                  "w") as cache:
+        with open(f"db/remote_app_cache/{self.cache_name}.cache", "w") as cache:
             json.dump(self.cache, cache)
 
     def delete_cache(self):
@@ -134,16 +131,15 @@ class Integration:
             "to_user": to_user,
             "data": data,
         }
-        response = self.prepare_request("/send/",
-                                        type="post",
-                                        data=request_body)
+        response = self.prepare_request("/send/", type="post", data=request_body)
 
         if "false" in response.text:
             logger.error("Error sending message")
             return False
         else:
             logger.info(
-                f"Message sent: app_name:{self.app_name} action:{action} data: {data} to: {to_user}")
+                f"Message sent: app_name:{self.app_name} action:{action} data: {data} to: {to_user}"
+            )
             return True
 
     def get_(self):
@@ -155,53 +151,61 @@ class Integration:
         transactions_sended_not_validated = {}
 
         if self.sended:
-            response = self.prepare_request("/transactions/sended/validated",
-                                            type="get")
+            response = self.prepare_request(
+                "/transactions/sended/validated", type="get"
+            )
             transactions_sended = response.json()
 
         if self.sended_not_validated:
             response = self.prepare_request(
-                "/transactions/sended/not_validated", type="get")
+                "/transactions/sended/not_validated", type="get"
+            )
             transactions_sended_not_validated = response.json()
 
         new_dict = {}
 
         for transaction in transactions:
-            if transactions[transaction]["transaction"][
-                    "signature"] in self.cache:
+            if transactions[transaction]["transaction"]["signature"] in self.cache:
                 continue
             else:
                 new_dict[transaction] = transactions[transaction]
-                the_tx = Transaction.load_json(
-                    transactions[transaction]["transaction"])
+                the_tx = Transaction.load_json(transactions[transaction]["transaction"])
                 SavetoMyTransaction(the_tx)
                 ValidateTransaction(the_tx)
-                self.cache.append(
-                    transactions[transaction]["transaction"]["signature"])
+                self.cache.append(transactions[transaction]["transaction"]["signature"])
 
         for transaction in transactions_sended:
-            if (transactions_sended[transaction]["transaction"]["signature"]
-                    in self.cache):
+            if (
+                transactions_sended[transaction]["transaction"]["signature"]
+                in self.cache
+            ):
                 continue
             else:
                 new_dict[transaction] = transactions_sended[transaction]
                 the_tx = Transaction.load_json(
-                    transactions_sended[transaction]["transaction"])
+                    transactions_sended[transaction]["transaction"]
+                )
                 SavetoMyTransaction(the_tx)
                 ValidateTransaction(the_tx)
-                self.cache.append(transactions_sended[transaction]
-                                  ["transaction"]["signature"])
+                self.cache.append(
+                    transactions_sended[transaction]["transaction"]["signature"]
+                )
 
         for transaction in transactions_sended_not_validated:
-            if (transactions_sended_not_validated[transaction]["transaction"]
-                    ["signature"] in self.cache):
+            if (
+                transactions_sended_not_validated[transaction]["transaction"][
+                    "signature"
+                ]
+                in self.cache
+            ):
                 continue
             else:
-                new_dict[transaction] = transactions_sended_not_validated[
-                    transaction]
+                new_dict[transaction] = transactions_sended_not_validated[transaction]
                 self.cache.append(
-                    transactions_sended_not_validated[transaction]
-                    ["transaction"]["signature"])
+                    transactions_sended_not_validated[transaction]["transaction"][
+                        "signature"
+                    ]
+                )
 
         self.save_cache()
 
@@ -211,10 +215,13 @@ class Integration:
             if not new_dict[transaction]["transaction"]["data"] == "NP":
                 with contextlib.suppress(json.decoder.JSONDecodeError):
                     new_dict[transaction]["transaction"]["data"] = json.loads(
-                        new_dict[transaction]["transaction"]["data"])
+                        new_dict[transaction]["transaction"]["data"]
+                    )
 
-                    if (self.app_name in new_dict[transaction]["transaction"]
-                            ["data"]["action"]):
+                    if (
+                        self.app_name
+                        in new_dict[transaction]["transaction"]["data"]["action"]
+                    ):
                         last_list.append(new_dict[transaction]["transaction"])
 
         result = []
