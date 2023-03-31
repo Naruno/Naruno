32e25e6fe830502a7d9fb4bf971cea47a8bfaae1
Restyled.io <commits@restyled.io>
2023-03-31 07:23:14 +0000

    Restyled by isort

--- naruno/node/get_candidate_blocks.py
+++ naruno/node/get_candidate_blocks.py
@@ -5,8 +5,10 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at https://mozilla.org/MPL/2.0/.
 import time
+
 from naruno.blockchain.block.block_main import Block
-from naruno.blockchain.candidate_block.candidate_block_main import candidate_block
+from naruno.blockchain.candidate_block.candidate_block_main import \
+    candidate_block
 from naruno.node.unl import Unl
 
 
