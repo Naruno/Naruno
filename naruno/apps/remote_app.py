58ecd3414092726235810844f690a17fb9f76527
Onur Atakan ULUSOY <atadogan06@gmail.com>
2023-04-08 10:35:38 +0000

    fix

--- naruno/apps/remote_app.py
+++ naruno/apps/remote_app.py
@@ -148,7 +148,7 @@ class Integration:
 
         self.check_thread = None
         if self.total_check:
-            self.check_thread = perpetualTimer(copy.copy(self.wait_amount), selfchecker)
+            self.check_thread = perpetualTimer(copy.copy(self.wait_amount), self.checker)
             self.wait_amount = 0
             self.check_thread.start()
 
