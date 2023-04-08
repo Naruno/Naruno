32f005bab62d4a03e7e62141894c78ec4b65d6b0
Restyled.io <commits@restyled.io>
2023-04-08 04:18:03 +0000

    Restyled by autopep8

--- naruno/apps/remote_app.py
+++ naruno/apps/remote_app.py
@@ -315,8 +315,7 @@ class Integration:
             for each_data in splitted_data:
                 self.send(
                     action=action,
-                    app_data=
-                    f"split-{2+splitted_data.index(each_data)}-{split_random}{each_data}",
+                    app_data=f"split-{2+splitted_data.index(each_data)}-{split_random}{each_data}",
                     to_user=to_user,
                     force=force,
                     retrysecond=retrysecond,
@@ -376,7 +375,7 @@ class Integration:
 
         new_txs = self.get(get_all=True, disable_caches=True)
 
-        for sended_tx in self.sended_txs[:self.max_tx_number/2]:
+        for sended_tx in self.sended_txs[:self.max_tx_number / 2]:
             in_get = False
             with contextlib.suppress(ValueError):
                 self.sended_txs.remove(sended_tx)
@@ -463,7 +462,7 @@ class Integration:
         for transaction in transactions_sended:
             if self.sended:
                 if (transactions_sended[transaction]["transaction"]
-                    ["signature"] in self.cache) and not get_all:
+                        ["signature"] in self.cache) and not get_all:
                     continue
                 else:
                     if transactions_sended[transaction]["transaction"][
@@ -474,7 +473,7 @@ class Integration:
                             transactions_sended[transaction]["transaction"])
 
                         if (not transactions_sended[transaction]["transaction"]
-                            ["data"] == "NP"):
+                                ["data"] == "NP"):
                             with contextlib.suppress(
                                     json.decoder.JSONDecodeError):
                                 transactions_sended[transaction][
@@ -518,7 +517,7 @@ class Integration:
                             transaction] = transactions_sended_not_validated[
                                 transaction]
                         if (not transactions_sended_not_validated[transaction]
-                            ["transaction"]["data"] == "NP"):
+                                ["transaction"]["data"] == "NP"):
                             with contextlib.suppress(
                                     json.decoder.JSONDecodeError):
                                 transactions_sended_not_validated[transaction][
@@ -557,7 +556,7 @@ class Integration:
             with contextlib.suppress(TypeError):
                 if not new_dict[transaction]["transaction"]["data"] == "NP":
                     if (self.app_name in new_dict[transaction]["transaction"]
-                        ["data"]["action"]):
+                            ["data"]["action"]):
                         last_list.append(new_dict[transaction]["transaction"])
 
         splits = []
