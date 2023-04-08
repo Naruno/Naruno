9e47954c694505dc73b9bca624756201d8ad3d95
Onur Atakan ULUSOY <yatadogan06@gmail.com>
2023-04-08 23:41:22 +0000

    test

--- naruno/config.py
+++ naruno/config.py
@@ -5,6 +5,7 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
+
 CONFIG_PATH = "db/config.json"
 SETTING_PATH = "db/settings.json"
 
