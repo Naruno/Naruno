97ecd981d399be022a7acbe72cf2f5ab33e5d30a
Restyled.io <commits@restyled.io>
2023-04-10 13:59:51 +0000

    Restyled by black

--- tests/stability_tests/local/test_naruno_local_10_3.py
+++ tests/stability_tests/local/test_naruno_local_10_3.py
@@ -25,58 +25,54 @@ temp_environment.start()
 
 
 class Test_Naruno_Local(unittest.TestCase):
-
     def test_multiple_transaction_with_time_difference(self):
         """
         Send coin to 2.wallet from 1.wallet
         """
 
         wallet_2_json = json.loads(
-            urllib.request.urlopen(
-                "http://localhost:8101/wallet/create/123").read().decode())
-        wallet_2_address = (wallet_2_json[0].replace("0) ", "").replace(
-            " - CURRENTLY USED\n", ""))
+            urllib.request.urlopen("http://localhost:8101/wallet/create/123")
+            .read()
+            .decode()
+        )
+        wallet_2_address = (
+            wallet_2_json[0].replace("0) ", "").replace(" - CURRENTLY USED\n", "")
+        )
 
         requests.post(
             "http://localhost:8000/send/",
-            {
-                "to_user": wallet_2_address,
-                "amount": 5000,
-                "password": "123"
-            },
+            {"to_user": wallet_2_address, "amount": 5000, "password": "123"},
         )
         time.sleep(50)
         balance_wallet_1 = json.loads(
-            urllib.request.urlopen(
-                "http://localhost:8101/wallet/balance").read().decode())
-        self.assertEqual(balance_wallet_1, 4000.0,
-                         "A problem in same network one transaction -1.")
+            urllib.request.urlopen("http://localhost:8101/wallet/balance")
+            .read()
+            .decode()
+        )
+        self.assertEqual(
+            balance_wallet_1, 4000.0, "A problem in same network one transaction -1."
+        )
 
         time.sleep(100)
 
         requests.post(
             "http://localhost:8000/send/",
-            {
-                "to_user": wallet_2_address,
-                "amount": 500,
-                "password": "123"
-            },
+            {"to_user": wallet_2_address, "amount": 500, "password": "123"},
         )
         time.sleep(50)
         requests.post(
             "http://localhost:8000/send/",
-            {
-                "to_user": wallet_2_address,
-                "amount": 5000,
-                "password": "123"
-            },
+            {"to_user": wallet_2_address, "amount": 5000, "password": "123"},
         )
         time.sleep(50)
         balance_wallet_1 = json.loads(
-            urllib.request.urlopen(
-                "http://localhost:8101/wallet/balance").read().decode())
-        self.assertEqual(balance_wallet_1, 9500.0,
-                         "A problem in same network one transaction -3.")
+            urllib.request.urlopen("http://localhost:8101/wallet/balance")
+            .read()
+            .decode()
+        )
+        self.assertEqual(
+            balance_wallet_1, 9500.0, "A problem in same network one transaction -3."
+        )
 
 
 unittest.main(exit=False)
