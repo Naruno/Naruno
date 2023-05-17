c11b0351efb363c93666f944b0eacf78e1455742
onuratakan <atadogan06@gmail.com>
2023-05-17 22:13:27 +0000

    licence: Added licence to naruno import and export libraries

--- naruno/lib/backup/naruno_import.py
+++ naruno/lib/backup/naruno_import.py
@@ -1,3 +1,10 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
 import copy
 import os
 import shutil
