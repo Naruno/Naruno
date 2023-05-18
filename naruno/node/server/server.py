3778c0cbf3a74a1c5f29a81a90e492ad20800abc
Restyled.io <commits@restyled.io>
2023-05-18 16:16:59 +0000

    Restyled by isort

--- naruno/node/server/server.py
+++ naruno/node/server/server.py
@@ -19,16 +19,11 @@ from naruno.blockchain.block.block_main import Block
 from naruno.blockchain.block.change_transaction_fee import ChangeTransactionFee
 from naruno.blockchain.block.get_block import GetBlock
 from naruno.blockchain.block.save_block import SaveBlock
-from naruno.config import CONNECTED_NODES_PATH
-from naruno.config import LOADING_ACCOUNTS_PATH
-from naruno.config import LOADING_BLOCK_PATH
-from naruno.config import LOADING_BLOCKSHASH_PART_PATH
-from naruno.config import LOADING_BLOCKSHASH_PATH
-from naruno.config import PENDING_TRANSACTIONS_PATH
-from naruno.config import TEMP_ACCOUNTS_PATH
-from naruno.config import TEMP_BLOCK_PATH
-from naruno.config import TEMP_BLOCKSHASH_PART_PATH
-from naruno.config import TEMP_BLOCKSHASH_PATH
+from naruno.config import (CONNECTED_NODES_PATH, LOADING_ACCOUNTS_PATH,
+                           LOADING_BLOCK_PATH, LOADING_BLOCKSHASH_PART_PATH,
+                           LOADING_BLOCKSHASH_PATH, PENDING_TRANSACTIONS_PATH,
+                           TEMP_ACCOUNTS_PATH, TEMP_BLOCK_PATH,
+                           TEMP_BLOCKSHASH_PART_PATH, TEMP_BLOCKSHASH_PATH)
 from naruno.lib.config_system import get_config
 from naruno.lib.log import get_logger
 from naruno.node.client.client import client
