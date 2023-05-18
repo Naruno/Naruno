fabffc01cb9b8124d497ed24213e55be8d3bb52e
Onur Atakan ULUSOY <atadogan06@gmail.com>
2023-05-19 01:14:24 +0300

    node: All loggings are redesigned (#1609)
    * node: All loggings are redesigned
    
    * Fix
    
    * Restyled by autopep8
    
    * Restyled by black
    
    * Restyled by isort
    
    * Restyled by reorder-python-imports
    
    * Restyled by yapf
    
    * Fix
    
    * Restyled by black
    
    * Restyled by isort
    
    * Restyled by reorder-python-imports
    
    * Restyled by yapf
    
    * Fix
    
    * Update server.py
    
    * Update client.py
    
    ---------
    
    Co-authored-by: Restyled.io <commits@restyled.io>

--- naruno/node/client/client.py
+++ naruno/node/client/client.py
@@ -8,12 +8,11 @@ import contextlib
 import json
 import socket
 import time
+from hashlib import md5
 from threading import Thread
 
 from naruno.blockchain.block.block_main import Block
 from naruno.lib.log import get_logger
-
-logger = get_logger("NODE")
 
 a_block = Block("onur")
 buffer_size = 6525 + int(
@@ -34,6 +33,15 @@ class client(Thread):
         self.candidate_block_history = []
         self.candidate_block_hash_history = []
 
+        self.logger = get_logger(
+            f"NODE_{self.server.host}_{self.server.port}_SOCK_{self.host}_{self.port}"
+        )
+
+        self.logger.info(f"Connection established with {self.id}")
+
+        self.logger.debug("Test mode: " + str(test))
+        self.logger.debug("Buffer size: " + str(buffer_size))
+
         self.running = True
         if not test:
             self.start()
@@ -43,9 +51,6 @@ class client(Thread):
         while self.running:
             with contextlib.suppress(socket.timeout):
                 data = self.socket.recv(buffer_size)
-                logger.debug(
-                    f"NODE:{self.server.host}:{self.server.port} SOCK:{self.host}:{self.port} Received data"
-                )
 
                 if not data:
                     break
@@ -55,8 +60,14 @@ class client(Thread):
                     data = json.loads(data)
                 with contextlib.suppress(Exception):
                     del data["buffer"]
-                with contextlib.suppress(Exception):
-                    self.server.get_message(self, data)
+                try:
+                    hash_of_data = md5(str(data).encode()).hexdigest()[:6]
+                    self.logger.debug(
+                        f"Received data ({hash_of_data}): {data}")
+                    self.server.get_message(self, data, hash_of_data)
+                except Exception as e:
+                    self.logger.error(f"Error while processing data: {e}")
+                    self.logger.error(f"Data: {data}")
 
             time.sleep(0.01)
 
