7210b6c049c0093b5f38dbbe16213c6cf2c51c34
Restyled.io <commits@restyled.io>
2023-06-16 11:10:29 +0000

    Restyled by autopep8

--- naruno/gui/main.py
+++ naruno/gui/main.py
@@ -54,7 +54,6 @@ ScreenManager:
 logger = get_logger("GUI")
 
 
-
 class GUI(MDApp):
     """
     An MDApp based  GUI class.
@@ -74,7 +73,6 @@ class GUI(MDApp):
         """
         Window.borderless = True
         value = the_settings()["dark_mode"]
-
 
         for i in self.theme_cls.colors["Yellow"]:
             self.theme_cls.colors["Yellow"][i] = "#5C6BC0"
@@ -165,8 +163,7 @@ def arguments():
     """
 
     parser = argparse.ArgumentParser(
-        description=
-        "Naruno is a lightning-fast, secure, and scalable blockchain that is able to create transaction proofs and verification via raw data and timestamp. We remove the archive nodes and lazy web3 integrations. With Naruno everyone can get the proof (5-10MB) of their transactions via their nodes and after everyone can use in another node for verification the raw data and timestamp. Also you can integrate your web3 applications with 4 code lines (just python for now) via our remote app system. Use the menu (-m) or GUI to gain full control and use the node, operation, etc."
+        description="Naruno is a lightning-fast, secure, and scalable blockchain that is able to create transaction proofs and verification via raw data and timestamp. We remove the archive nodes and lazy web3 integrations. With Naruno everyone can get the proof (5-10MB) of their transactions via their nodes and after everyone can use in another node for verification the raw data and timestamp. Also you can integrate your web3 applications with 4 code lines (just python for now) via our remote app system. Use the menu (-m) or GUI to gain full control and use the node, operation, etc."
     )
 
     parser.add_argument(
