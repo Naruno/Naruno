c251786efd629152311e56b4ec78461b07076d6d
Restyled.io <commits@restyled.io>
2023-06-16 01:25:39 +0000

    Restyled by black

--- naruno/gui/popup.py
+++ naruno/gui/popup.py
@@ -15,21 +15,27 @@ import naruno.gui.the_naruno_gui_app
 the_popups = []
 
 
-def popup(title=None,
-          text=None,
-          image=None,
-          height_image=None,
-          thirdly_title=None,
-          target=None,
-          inputs=None,
-          type="custom"):
+def popup(
+    title=None,
+    text=None,
+    image=None,
+    height_image=None,
+    thirdly_title=None,
+    target=None,
+    inputs=None,
+    type="custom",
+):
     for apopup in the_popups:
-        if (apopup.title == title and apopup.text == text
-                and apopup.image == image
-                and apopup.height_image == height_image
-                and apopup.thirdly_title == thirdly_title
-                and apopup.target == target and apopup.inputs == inputs
-                and apopup.type == type):
+        if (
+            apopup.title == title
+            and apopup.text == text
+            and apopup.image == image
+            and apopup.height_image == height_image
+            and apopup.thirdly_title == thirdly_title
+            and apopup.target == target
+            and apopup.inputs == inputs
+            and apopup.type == type
+        ):
             return popup
     return popup_class(
         title=title,
@@ -44,7 +50,6 @@ def popup(title=None,
 
 
 class popup_class:
-
     def __init__(
         self,
         title=None,
@@ -85,12 +90,16 @@ class popup_class:
 
     def dismiss(self, widget=None):
         for apopup in the_popups:
-            if (apopup.title == self.title and apopup.text == self.text
-                    and apopup.image == self.image
-                    and apopup.height_image == self.height_image
-                    and apopup.thirdly_title == self.thirdly_title
-                    and apopup.target == self.target and apopup.inputs == self.inputs
-                    and apopup.type == type):
+            if (
+                apopup.title == self.title
+                and apopup.text == self.text
+                and apopup.image == self.image
+                and apopup.height_image == self.height_image
+                and apopup.thirdly_title == self.thirdly_title
+                and apopup.target == self.target
+                and apopup.inputs == self.inputs
+                and apopup.type == type
+            ):
                 apopup.dialog.dismiss()
                 the_popups.remove(apopup)
         self.dialog.dismiss()
@@ -132,8 +141,7 @@ class popup_class:
                             font_size="18sp",
                             on_press=self.dismiss,
                             font_name=os.path.join(
-                                naruno.gui.the_naruno_gui_app.the_naruno_gui.
-                                FONT_PATH,
+                                naruno.gui.the_naruno_gui_app.the_naruno_gui.FONT_PATH,
                                 "Poppins-Bold",
                             ),
                         ),
@@ -141,8 +149,7 @@ class popup_class:
                             text="OK",
                             font_size="18sp",
                             font_name=os.path.join(
-                                naruno.gui.the_naruno_gui_app.the_naruno_gui.
-                                FONT_PATH,
+                                naruno.gui.the_naruno_gui_app.the_naruno_gui.FONT_PATH,
                                 "Poppins-Bold",
                             ),
                             on_press=self.director,
@@ -154,12 +161,13 @@ class popup_class:
                     content = i[0]
                     is_pass = i[1]
                     self.dialog.content_cls.add_widget(
-                        MDTextField(hint_text=content,
-                                    mode="fill",
-                                    password=is_pass))
+                        MDTextField(hint_text=content, mode="fill", password=is_pass)
+                    )
             elif self.type != "question":
                 the_type = None if self.type == "qr" else self.type
-                self.dialog = SweetAlert(auto_dismiss=False, )
+                self.dialog = SweetAlert(
+                    auto_dismiss=False,
+                )
                 self.dialog.fire(
                     self.title,
                     self.text,
@@ -169,7 +177,9 @@ class popup_class:
                     type=the_type,
                 )
             else:
-                self.dialog = SweetAlert(auto_dismiss=False, )
+                self.dialog = SweetAlert(
+                    auto_dismiss=False,
+                )
                 self.dialog.fire(
                     title=self.title,
                     text=self.text,
@@ -180,8 +190,7 @@ class popup_class:
                             font_size="18sp",
                             on_press=self.dismiss,
                             font_name=os.path.join(
-                                naruno.gui.the_naruno_gui_app.the_naruno_gui.
-                                FONT_PATH,
+                                naruno.gui.the_naruno_gui_app.the_naruno_gui.FONT_PATH,
                                 "Poppins-Bold",
                             ),
                         ),
@@ -189,8 +198,7 @@ class popup_class:
                             text="YES",
                             font_size="18sp",
                             font_name=os.path.join(
-                                naruno.gui.the_naruno_gui_app.the_naruno_gui.
-                                FONT_PATH,
+                                naruno.gui.the_naruno_gui_app.the_naruno_gui.FONT_PATH,
                                 "Poppins-Bold",
                             ),
                             on_press=self.director_without_input,
