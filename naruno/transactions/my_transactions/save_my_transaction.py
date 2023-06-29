b24544183e30cdabb2081270e3856dbe4f08464c
Restyled.io <commits@restyled.io>
2023-06-29 12:48:24 +0000

    Restyled by black

--- naruno/transactions/my_transactions/save_my_transaction.py
+++ naruno/transactions/my_transactions/save_my_transaction.py
@@ -14,8 +14,7 @@ from naruno.lib.config_system import get_config
 
 from naruno.lib.kot import KOT
 
-mytransactions_db = KOT("mytransactions", folder=get_config()[
-                        "main_folder"] + "/db")
+mytransactions_db = KOT("mytransactions", folder=get_config()["main_folder"] + "/db")
 
 
 def SaveMyTransaction(transaction_list, clear=False):
@@ -33,8 +32,8 @@ def SaveMyTransaction(transaction_list, clear=False):
                 name = "empty".encode("utf-8")
 
             entry_name_list.append(
-                os.path.join(MY_TRANSACTION_PATH,
-                             sha256(name).hexdigest()))
+                os.path.join(MY_TRANSACTION_PATH, sha256(name).hexdigest())
+            )
 
         new_dict = {
             tx[0].signature: {
@@ -47,7 +46,7 @@ def SaveMyTransaction(transaction_list, clear=False):
 
         if clear:
             for entry in mytransactions_db.get_all():
-                if (not entry.endswith("validated") and not entry.endswith("sended")):
+                if not entry.endswith("validated") and not entry.endswith("sended"):
                     if entry not in str(entry_name_list):
                         mytransactions_db.delete(entry)
                         mytransactions_db.delete(entry + "validated")
@@ -59,23 +58,24 @@ def SaveMyTransaction(transaction_list, clear=False):
             name = copy.copy(tx.encode("utf-8"))
             if tx == b"":
                 name = "empty".encode("utf-8")
-            mytransactions_db.set(sha256(name).hexdigest(),
-                                  transaction_list[tx]["tx"])
+            mytransactions_db.set(sha256(name).hexdigest(), transaction_list[tx]["tx"])
 
             if transaction_list[tx]["validated"]:
-                mytransactions_db.set(
-                    sha256(name).hexdigest() + "validated", True)
+                mytransactions_db.set(sha256(name).hexdigest() + "validated", True)
             if transaction_list[tx]["sended"]:
-                mytransactions_db.set(
-                    sha256(name).hexdigest() + "sended", True)
+                mytransactions_db.set(sha256(name).hexdigest() + "sended", True)
 
     elif type(transaction_list) is dict and transaction_list != {}:
-        mytransactions_db.set(sha256(
-            transaction_list[0].signature).hexdigest(), transaction_list[0].dump_json())
+        mytransactions_db.set(
+            sha256(transaction_list[0].signature).hexdigest(),
+            transaction_list[0].dump_json(),
+        )
 
         if transaction_list[1]:
             mytransactions_db.set(
-                sha256(transaction_list[0].signature + "validated").hexdigest(), True)
+                sha256(transaction_list[0].signature + "validated").hexdigest(), True
+            )
         if transaction_list[2]:
             mytransactions_db.set(
-                sha256(transaction_list[0].signature + "sended").hexdigest(), True)
+                sha256(transaction_list[0].signature + "sended").hexdigest(), True
+            )
