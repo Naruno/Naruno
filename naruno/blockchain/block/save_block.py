b522109963737c412bd0a12f8e833c50357e7efd
onuratakan <atadogan06@gmail.com>
2023-07-02 17:13:49 +0000

    fix

--- naruno/blockchain/block/save_block.py
+++ naruno/blockchain/block/save_block.py
@@ -202,7 +202,6 @@ def SaveBlock(
         SaveAccounts(
             accounts_list,
             custom_TEMP_ACCOUNTS_PATH=custom_TEMP_ACCOUNTS_PATH,
-            reset=True,
         )
         SaveBlockshash(
             block.previous_hash,
