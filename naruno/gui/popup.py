2c258b5c652cc1f08406bc978ddc541de3d1f05a
onuratakan <atadogan06@gmail.com>
2023-07-06 18:10:35 +0000

    try

--- naruno/gui/popup.py
+++ naruno/gui/popup.py
@@ -9,6 +9,7 @@ import os
 from kivymd.uix.button import MDFlatButton
 from kivymd.uix.textfield import MDTextField
 from kivymd_extensions.sweetalert import SweetAlert
+from kivymd.uix.gridlayout import MDGridLayout
 
 import naruno.gui.the_naruno_gui_app
 
@@ -81,6 +82,11 @@ class popup:
     def create(self):
         if self.dialog is None:
             if self.type == "custom":
+                custom_content_cls = MDGridLayout(
+                        adaptive_height=True,
+                        padding=("24dp", "68dp", "24dp", "24dp"),
+                        spacing="24dp",
+                    )                
                 self.dialog = SweetAlert(
                     title=self.title,
                     type=self.type,
@@ -109,6 +115,7 @@ class popup:
                             on_press=self.director,
                         ),
                     ],
+                    content_cls=custom_content_cls,
                 )
 
                 for i in self.inputs:
