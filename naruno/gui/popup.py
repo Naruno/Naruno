2e11bfbd1e623ca7fef901e81c7ac1f5d796bd6e
Restyled.io <commits@restyled.io>
2023-07-06 19:12:49 +0000

    Restyled by yapf

--- naruno/gui/popup.py
+++ naruno/gui/popup.py
@@ -15,6 +15,7 @@ import naruno.gui.the_naruno_gui_app
 
 
 class popup:
+
     def __init__(
         self,
         title=None,
@@ -96,7 +97,8 @@ class popup:
                             font_size="18sp",
                             on_press=self.dismiss,
                             font_name=os.path.join(
-                                naruno.gui.the_naruno_gui_app.the_naruno_gui.FONT_PATH,
+                                naruno.gui.the_naruno_gui_app.the_naruno_gui.
+                                FONT_PATH,
                                 "Poppins-Bold",
                             ),
                         ),
@@ -104,7 +106,8 @@ class popup:
                             text="OK",
                             font_size="18sp",
                             font_name=os.path.join(
-                                naruno.gui.the_naruno_gui_app.the_naruno_gui.FONT_PATH,
+                                naruno.gui.the_naruno_gui_app.the_naruno_gui.
+                                FONT_PATH,
                                 "Poppins-Bold",
                             ),
                             on_press=self.director,
@@ -117,8 +120,9 @@ class popup:
                     content = i[0]
                     is_pass = i[1]
                     self.dialog.content_cls.add_widget(
-                        MDTextField(hint_text=content, mode="fill", password=is_pass)
-                    )
+                        MDTextField(hint_text=content,
+                                    mode="fill",
+                                    password=is_pass))
             elif self.type != "question":
                 the_type = None if self.type == "qr" else self.type
                 self.dialog = SweetAlert()
@@ -142,7 +146,8 @@ class popup:
                             font_size="18sp",
                             on_press=self.dismiss,
                             font_name=os.path.join(
-                                naruno.gui.the_naruno_gui_app.the_naruno_gui.FONT_PATH,
+                                naruno.gui.the_naruno_gui_app.the_naruno_gui.
+                                FONT_PATH,
                                 "Poppins-Bold",
                             ),
                         ),
@@ -150,7 +155,8 @@ class popup:
                             text="YES",
                             font_size="18sp",
                             font_name=os.path.join(
-                                naruno.gui.the_naruno_gui_app.the_naruno_gui.FONT_PATH,
+                                naruno.gui.the_naruno_gui_app.the_naruno_gui.
+                                FONT_PATH,
                                 "Poppins-Bold",
                             ),
                             on_press=self.director_without_input,
