87f71b442ad35fde5efe751e647102ef942c35f6
Restyled.io <commits@restyled.io>
2023-07-10 23:26:06 +0000

    Restyled by yapf

--- naruno/consensus/rounds/round_2/checks/candidate_blocks_hashes/candidate_blocks_hashes_main.py
+++ naruno/consensus/rounds/round_2/checks/candidate_blocks_hashes/candidate_blocks_hashes_main.py
@@ -11,17 +11,15 @@ from naruno.lib.log import get_logger
 logger = get_logger("CONSENSUS_SECOND_ROUND")
 
 
-def candidate_blocks_hashes_check(
-    candidate_class: candidate_block, unl_nodes: dict
-) -> bool:
+def candidate_blocks_hashes_check(candidate_class: candidate_block,
+                                  unl_nodes: dict) -> bool:
     logger.info("Candidate block hash control started for round 2")
     logger.debug(
         f"len(candidate_class.candidate_block_hashes): {len(candidate_class.candidate_block_hashes)}"
     )
     logger.debug(f"len(unl_nodes): {len(unl_nodes)}")
-    if len(candidate_class.candidate_block_hashes) > (
-        ((len(unl_nodes) + 1) * 80) / 100
-    ):
+    if len(candidate_class.candidate_block_hashes) > ((
+        (len(unl_nodes) + 1) * 80) / 100):
         logger.info("Candidate block hash number is True")
         return True
     else:
