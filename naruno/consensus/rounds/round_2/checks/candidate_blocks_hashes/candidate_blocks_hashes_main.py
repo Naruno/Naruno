14cf2b7dd5615e64c010b31f5a5e0bdb2351cd63
Restyled.io <commits@restyled.io>
2023-07-10 23:26:00 +0000

    Restyled by black

--- naruno/consensus/rounds/round_2/checks/candidate_blocks_hashes/candidate_blocks_hashes_main.py
+++ naruno/consensus/rounds/round_2/checks/candidate_blocks_hashes/candidate_blocks_hashes_main.py
@@ -4,22 +4,23 @@
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at https://mozilla.org/MPL/2.0/.
-from naruno.blockchain.candidate_block.candidate_block_main import \
-    candidate_block
+from naruno.blockchain.candidate_block.candidate_block_main import candidate_block
 from naruno.lib.log import get_logger
 
 logger = get_logger("CONSENSUS_SECOND_ROUND")
 
 
-def candidate_blocks_hashes_check(candidate_class: candidate_block,
-                                  unl_nodes: dict) -> bool:
+def candidate_blocks_hashes_check(
+    candidate_class: candidate_block, unl_nodes: dict
+) -> bool:
     logger.info("Candidate block hash control started for round 2")
     logger.debug(
         f"len(candidate_class.candidate_block_hashes): {len(candidate_class.candidate_block_hashes)}"
     )
     logger.debug(f"len(unl_nodes): {len(unl_nodes)}")
-    if len(candidate_class.candidate_block_hashes) > ((
-            (len(unl_nodes) + 1) * 80) / 100):
+    if len(candidate_class.candidate_block_hashes) > (
+        ((len(unl_nodes) + 1) * 80) / 100
+    ):
         logger.info("Candidate block hash number is True")
         return True
     else:
--- naruno/consensus/rounds/round_2/process/candidate_blocks_hashes/candidate_blocks_hashes_main.py
+++ naruno/consensus/rounds/round_2/process/candidate_blocks_hashes/candidate_blocks_hashes_main.py
@@ -11,16 +11,15 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at https://mozilla.org/MPL/2.0/.
 from naruno.blockchain.block.block_main import Block
-from naruno.blockchain.candidate_block.candidate_block_main import \
-    candidate_block
+from naruno.blockchain.candidate_block.candidate_block_main import candidate_block
 from naruno.lib.log import get_logger
 
 logger = get_logger("CONSENSUS_SECOND_ROUND")
 
 
-def process_candidate_blocks_hashes(block: Block,
-                                    candidate_class: candidate_block,
-                                    unl_nodes: dict) -> dict:
+def process_candidate_blocks_hashes(
+    block: Block, candidate_class: candidate_block, unl_nodes: dict
+) -> dict:
     logger.info("Control process of candidate block hashes is started.")
 
     current_hash = {"signature": "A", "hash": False, "previous_hash": False}
@@ -32,35 +31,44 @@ def process_candidate_blocks_hashes(block: Block,
         tx_valid = 1
 
         for other_block in candidate_class.candidate_block_hashes[:]:
-            if (candidate_block_hash != other_block
-                    and candidate_block_hash["hash"] == other_block["hash"]):
+            if (
+                candidate_block_hash != other_block
+                and candidate_block_hash["hash"] == other_block["hash"]
+            ):
                 tx_valid += 1
 
         logger.debug(f"Hash valid of  {candidate_block_hash} : {tx_valid}")
 
         if tx_valid > (((len(unl_nodes) + 1) * 80) / 100):
-            logger.info(
-                f"candidate_block_hash: {candidate_block_hash} is validated.")
+            logger.info(f"candidate_block_hash: {candidate_block_hash} is validated.")
             current_hash = candidate_block_hash
 
     for candidate_block_hash in candidate_class.candidate_block_hashes[:]:
-        logger.debug(
-            f"Candidate block hash previous_hash {candidate_block_hash}")
+        logger.debug(f"Candidate block hash previous_hash {candidate_block_hash}")
 
         tx_valid = 1
 
         for other_block in candidate_class.candidate_block_hashes[:]:
-            if (candidate_block_hash != other_block
-                    and candidate_block_hash["previous_hash"] == other_block["previous_hash"]):
+            if (
+                candidate_block_hash != other_block
+                and candidate_block_hash["previous_hash"]
+                == other_block["previous_hash"]
+            ):
                 tx_valid += 1
         logger.debug(
-            f"Hash valid of previous_hash  {candidate_block_hash} : {tx_valid}")
+            f"Hash valid of previous_hash  {candidate_block_hash} : {tx_valid}"
+        )
         if tx_valid > (((len(unl_nodes) + 1) * 80) / 100):
             logger.info(
-                f"candidate_block_hash previous_hash: {candidate_block_hash} is validated.")
+                f"candidate_block_hash previous_hash: {candidate_block_hash} is validated."
+            )
             previous_hash = candidate_block_hash
 
-    if current_hash != {"signature": "A", "hash": False, "previous_hash": False} or previous_hash != {"signature": "A", "hash": False, "previous_hash": False}:
+    if current_hash != {
+        "signature": "A",
+        "hash": False,
+        "previous_hash": False,
+    } or previous_hash != {"signature": "A", "hash": False, "previous_hash": False}:
         if current_hash["signature"] == "self":
             for other in candidate_class.candidate_block_hashes[:]:
                 if current_hash != other and current_hash["hash"] == other["hash"]:
@@ -69,7 +77,10 @@ def process_candidate_blocks_hashes(block: Block,
 
         if previous_hash["signature"] == "self":
             for other in candidate_class.candidate_block_hashes[:]:
-                if previous_hash != other and previous_hash["previous_hash"] == other["previous_hash"]:
+                if (
+                    previous_hash != other
+                    and previous_hash["previous_hash"] == other["previous_hash"]
+                ):
                     previous_hash = other
                     break
         return {"hash": current_hash, "previous_hash": previous_hash}
