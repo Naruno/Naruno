ecf8f13a49da85e6fdf29fe21f2bb66129c995fd
Onur Atakan ULUSOY <atadogan06@gmail.com>
2023-07-11 01:51:36 +0300

    Fix

--- naruno/consensus/rounds/round_1/process/transactions/find_validated/find_validated_main.py
+++ naruno/consensus/rounds/round_1/process/transactions/find_validated/find_validated_main.py
@@ -35,7 +35,7 @@ def find_validated(block: Block, candidate_class: candidate_block,
 
             logger.debug(
                 f"Tx valid of {other_block_tx.signature} : {tx_valid}")
-            if tx_valid > (len(unl_nodes) + 1) * 80) / 100):
+            if tx_valid > (((len(unl_nodes) + 1) * 80) / 100):
                 already_in_ok = False
                 for alrady_tx in temp_validating_list[:]:
                     if other_block_tx.signature == alrady_tx.signature:
