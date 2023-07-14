d9061fbd7bac944feb4f46ab8b45d611babb34d8
Onur Atakan ULUSOY <atadogan06@gmail.com>
2023-07-14 19:12:14 +0300

    Fix

--- naruno/node/get_candidate_blocks.py
+++ naruno/node/get_candidate_blocks.py
@@ -30,7 +30,7 @@ def self_candidates(block: Block):
                 will_add_candidate_block_hash = naruno.node.get_candidate_blocks.our_candidates[1]
             
 
-            if (the_block.sequence_number < block.sequence_number) or block.sequence_number == 0:
+            if (the_block.sequence_number < block.sequence_number) or block.sequence_number == 0 or (the_block.sequence_number == block.sequence_number and the_block.empty_block_number < block.empty_block_number):
                 will_add_candidate_block = {
                         "action": "myblock",
                         "transaction": new_list,
