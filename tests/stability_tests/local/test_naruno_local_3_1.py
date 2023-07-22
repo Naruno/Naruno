da63d2b9dfffbfe8a35fbafe5e7e4eee2db37ad2
onuratakan <atadogan06@gmail.com>
2023-07-22 10:21:56 +0000

    Fix

--- tests/stability_tests/local/test_naruno_local_3_1.py
+++ tests/stability_tests/local/test_naruno_local_3_1.py
@@ -17,7 +17,7 @@ import requests
 
 from auto_builders.local import Naruno_Local
 
-temp_environment = Naruno_Local(2, 1, test_mode=True)
+temp_environment = Naruno_Local(3, 1, test_mode=True)
 temp_environment.delete()
 temp_environment.install()
 temp_environment.run()
