88f6951c5faff01eea82a4f2a07edb30776c1c05
onuratakan <atadogan06@gmail.com>
2023-07-24 19:43:14 +0000

    Fix

--- naruno/consensus/finished/transactions/transactions_main.py
+++ naruno/consensus/finished/transactions/transactions_main.py
@@ -23,6 +23,45 @@ from naruno.wallet.wallet_import import wallet_import_all
 logger = get_logger("CONSENSUS")
 
 
+def threaded_transactions_main(block: Block, commanders) -> list:
+    """
+    This function is responsible for the transactions of the block.
+    Parameters:
+        block: The block that is going to be validated.
+    Returns:
+        list: The list of the transactions that are going to be validated.
+    """
+    new_my_transactions_list = None
+    my_address = wallet_import_all(3)
+    my_public_key = wallet_import_all(0)
+    custom_currently_list = GetMyTransaction()
+    for tx in block.validating_list:
+        if tx.toUser in my_address:
+            new_my_transactions_list = True
+            SavetoMyTransaction(
+                tx,
+                validated=True,
+                custom_currently_list=custom_currently_list)
+        elif tx.fromUser in my_public_key:
+            new_my_transactions_list = True
+            ValidateTransaction(
+                tx, custom_currently_list=custom_currently_list)
+            SendedTransaction(
+                tx, custom_currently_list=custom_currently_list)
+        elif tx.fromUser in commanders:
+            new_my_transactions_list = True
+            SavetoMyTransaction(
+                tx,
+                validated=True,
+                custom_currently_list=custom_currently_list)
+        else:
+            if the_settings()["publisher_mode"]:
+                SavetoMyTransaction(
+                    tx,
+                    validated=True,
+                    custom_currently_list=custom_currently_list)
+    return new_my_transactions_list    
+
 
 def transactions_main(block: Block) -> list:
     """
@@ -32,29 +71,18 @@ def transactions_main(block: Block) -> list:
     Returns:
         list: The list of the transactions that are going to be validated.
     """
-    new_my_transactions_list = False
+    new_my_transactions_list = None
     my_address = wallet_import_all(3)
     my_public_key = wallet_import_all(0)
-    custom_currently_list = GetMyTransaction()
     commanders = GetCommander()
     for tx in block.validating_list:
         if tx.toUser in my_address:
             new_my_transactions_list = True
-            threading.Thread(target=SavetoMyTransaction, args=(
-                tx,
-                ), kwargs={"validated":True,"custom_currently_list":custom_currently_list}).start()
         elif tx.fromUser in my_public_key:
             new_my_transactions_list = True
-            threading.Thread(target=ValidateTransaction, args=(
-                tx), kwargs={"custom_currently_list":custom_currently_list}).start()
-            threading.Thread(target=SendedTransaction, args=(
-                tx), kwargs={"custom_currently_list":custom_currently_list}).start()
         elif tx.fromUser in commanders:
             new_my_transactions_list = True
-            threading.Thread(target=SavetoMyTransaction, args=(tx,), kwargs={"validated":True,"custom_currently_list":custom_currently_list}).start()
-        else:
-            if the_settings()["publisher_mode"]:
-                threading.Thread(target=SavetoMyTransaction, args=(
-                    tx,
-                    ), kwargs={"validated":True,"custom_currently_list":custom_currently_list}).start()
-    return True
+
+    threading.Thread(target=threaded_transactions_main, args=(block, commanders)).start()
+
+    return new_my_transactions_list
