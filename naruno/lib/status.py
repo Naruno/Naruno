1306d69cee766a3f84391bcced0cb604128f7cc2
onuratakan <atadogan06@gmail.com>
2023-07-25 19:44:39 +0000

    Fix

--- naruno/lib/status.py
+++ naruno/lib/status.py
@@ -43,7 +43,7 @@ def Status(
         first_block = copy.copy((GetBlock(custom_TEMP_BLOCK_PATH=custom_TEMP_BLOCK_PATH)
                        if custom_first_block is None else custom_first_block))
 
-        the_wait_time = wait_time if wait_time is not None else first_block.block_time*1.5
+        the_wait_time = wait_time if wait_time is not None else first_block.block_time*3
         time.sleep(the_wait_time)
         new_block = (GetBlock(custom_TEMP_BLOCK_PATH=custom_TEMP_BLOCK_PATH)
                      if custom_new_block is None else custom_new_block)
