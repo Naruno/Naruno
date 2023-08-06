42aaf5f22ee4d4fbd01a072b0384d7006bc86a7e
onuratakan <atadogan06@gmail.com>
2023-08-06 14:00:47 +0000

    Fix

--- naruno/apps/remote_app.py
+++ naruno/apps/remote_app.py
@@ -464,7 +464,7 @@ class Integration:
                 return False
             else:
                 logger.debug(response.text)
-                logger.debug(type(esponse.text))                
+                logger.debug(type(response.text))                
                 logger.info(
                     f"Message sent: app_name:{self.app_name} action:{action} data: {data} to: {to_user}"
                 )
