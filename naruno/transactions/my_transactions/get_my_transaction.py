4d1ea4da8557927b0ff9800d5c1536f3f61b4ff4
Restyled.io <commits@restyled.io>
2023-08-14 10:40:13 +0000

    Restyled by black

--- naruno/transactions/my_transactions/get_my_transaction.py
+++ naruno/transactions/my_transactions/get_my_transaction.py
@@ -19,8 +19,7 @@ from naruno.lib.settings_system import the_settings
 from naruno.transactions.transaction import Transaction
 import naruno
 
-mytransactions_db = KOT("mytransactions",
-                        folder=get_config()["main_folder"] + "/db")
+mytransactions_db = KOT("mytransactions", folder=get_config()["main_folder"] + "/db")
 
 mytransactions_db_ram = {}
 
@@ -35,8 +34,10 @@ def check_from_network():
     if the_settings()["baklava"]:
         # export validated transactions
         response = (
-            urlopen("http://test_net.1.naruno.org:8000/transactions/received"
-                    ).read().decode("utf-8"))
+            urlopen("http://test_net.1.naruno.org:8000/transactions/received")
+            .read()
+            .decode("utf-8")
+        )
         response = json.loads(response)
         for transaction in response:
             if response[transaction]["validated"]:
@@ -78,32 +79,50 @@ def GetMyTransaction(sended=None, validated=None, turn_json=False) -> list:
 
     the_transactions = []
 
-    if len(naruno.transactions.my_transactions.get_my_transaction.mytransactions_db_ram) != mytransactions_db.get_count():
-        naruno.transactions.my_transactions.get_my_transaction.mytransactions_db_ram = {}
+    if (
+        len(
+            naruno.transactions.my_transactions.get_my_transaction.mytransactions_db_ram
+        )
+        != mytransactions_db.get_count()
+    ):
+        naruno.transactions.my_transactions.get_my_transaction.mytransactions_db_ram = (
+            {}
+        )
 
-    all_records = (mytransactions_db.get_all()
-                   if naruno.transactions.my_transactions.get_my_transaction.mytransactions_db_ram == {} else naruno.transactions.my_transactions.get_my_transaction.mytransactions_db_ram)
+    all_records = (
+        mytransactions_db.get_all()
+        if naruno.transactions.my_transactions.get_my_transaction.mytransactions_db_ram
+        == {}
+        else naruno.transactions.my_transactions.get_my_transaction.mytransactions_db_ram
+    )
     for entry in copy.copy(all_records):
         if not entry.endswith("validated") and not entry.endswith("sended"):
             try:
                 the_transactions_json = all_records[entry]
                 the_tx = Transaction.load_json(the_transactions_json)
-                each_validated = (False if mytransactions_db.get(entry +
-                                                                 "validated")
-                                  == None else True)
+                each_validated = (
+                    False
+                    if mytransactions_db.get(entry + "validated") == None
+                    else True
+                )
 
-                if (the_transactions_json["signature"]
-                        in network_validated_source and not each_validated):
+                if (
+                    the_transactions_json["signature"] in network_validated_source
+                    and not each_validated
+                ):
                     each_validated = True
                     network_validated.append(the_tx)
 
-                each_sended = (False if mytransactions_db.get(entry + "sended")
-                               == None else True)
-                the_transactions.append([
-                    the_tx,
-                    each_validated,
-                    each_sended,
-                ])
+                each_sended = (
+                    False if mytransactions_db.get(entry + "sended") == None else True
+                )
+                the_transactions.append(
+                    [
+                        the_tx,
+                        each_validated,
+                        each_sended,
+                    ]
+                )
             except json.decoder.JSONDecodeError:
                 mytransactions_db.delete(entry)
                 mytransactions_db.delete(entry + "validated")
@@ -113,20 +132,19 @@ def GetMyTransaction(sended=None, validated=None, turn_json=False) -> list:
         the_transactions = [tx for tx in the_transactions if tx[2] == sended]
 
     if validated is not None:
-        the_transactions = [
-            tx for tx in the_transactions if tx[1] == validated
-        ]
+        the_transactions = [tx for tx in the_transactions if tx[1] == validated]
 
     # sort
     the_transactions.sort(key=lambda x: x[0].signature)
 
-    from naruno.transactions.my_transactions.validate_transaction import \
-        ValidateTransaction
+    from naruno.transactions.my_transactions.validate_transaction import (
+        ValidateTransaction,
+    )
 
     for i in network_validated:
-        ValidateTransaction(i,
-                            custom_currently_list=the_transactions,
-                            force_notify=True)
+        ValidateTransaction(
+            i, custom_currently_list=the_transactions, force_notify=True
+        )
 
     if turn_json:
         the_transactions = {
